966ba273578cd3d82acd53f405272c48aedfe96b
Jason Feingold <jfeingold@newrelic.com>
2020-07-08 14:29:13 -0700

    Updated default config batch size =700 and removed logging messages

--- src/Agent/NewRelic/Agent/Core/Aggregators/SpanEventAggregatorInfiniteTracing.cs
+++ src/Agent/NewRelic/Agent/Core/Aggregators/SpanEventAggregatorInfiniteTracing.cs
@@ -14,7 +14,6 @@ using NewRelic.Core.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace NewRelic.Agent.Core.Aggregators
