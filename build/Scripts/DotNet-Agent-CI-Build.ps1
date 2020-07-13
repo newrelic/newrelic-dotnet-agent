cc40c8cc5529b1e72782c36d612baf0e487f7384
Lynice Spangler <20267975+lspangler584@users.noreply.github.com>
2020-07-13 16:29:27 -0700

    change build script sln location

--- build/Scripts/DotNet-Agent-CI-Build.ps1
+++ build/Scripts/DotNet-Agent-CI-Build.ps1
@@ -4,9 +4,9 @@ $ErrorActionPreference = "Stop"
 # NuGet Restore #
 ###############
 
-$nugetPath = (Resolve-Path ".\Build\Tools\nuget.exe").Path
+$nugetPath = (Resolve-Path ".\build\Tools\nuget.exe").Path
 #$applicationsFull = @("Agent\FullAgent.sln", "IntegrationTests\IntegrationTests.sln", "IntegrationTests\UnboundedIntegrationTests.sln")
-$applicationsFull = @("Agent\FullAgent.sln")
+$applicationsFull = @("src\Agent\FullAgent.sln")
 
 Write-Host "Restoring NuGet packages"
 foreach ($application in $applicationsFull) {
@@ -19,7 +19,7 @@ foreach ($application in $applicationsFull) {
 
 $msBuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 
-$applicationsFull = [Ordered]@{"Agent\FullAgent.sln" = "Configuration=Release;Platform=x86;AllowUnsafeBlocks=true";
+$applicationsFull = [Ordered]@{"src\Agent\FullAgent.sln" = "Configuration=Release;Platform=x86;AllowUnsafeBlocks=true";
 #    "IntegrationTests\IntegrationTests.sln"          = "Configuration=Release;DeployOnBuild=true;PublishProfile=LocalDeploy";
 #    "IntegrationTests\UnboundedIntegrationTests.sln" = "Configuration=Release;DeployOnBuild=true;PublishProfile=LocalDeploy"
 }
@@ -34,7 +34,7 @@ foreach ($applicationFull in $applicationsFull.Keys) {
         exit $LastExitCode
     }
 
-    if ($applicationFull -eq "Agent\FullAgent.sln") {
+    if ($applicationFull -eq "src\Agent\FullAgent.sln") {
         Write-Host "-- Executing '. $msBuildPath /m /p:$($applicationsFull.Item($applicationFull).Replace("x86", "x64")) $applicationFull'"
         . $msBuildPath /m /p:$($applicationsFull.Item($applicationFull).Replace("x86", "x64")) $applicationFull
         
