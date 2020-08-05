1426a56f054eb12a55d0bf7a8103096cfa0f4da8
Lynice Spangler <20267975+lspangler584@users.noreply.github.com>
2020-08-05 10:20:12 -0700

    removes/readds OpenRastaWebApplication ' '

--- tests/Agent/IntegrationTests/Applications/OpenRastaWebApplication/Default.aspx.cs
+++ tests/Agent/IntegrationTests/Applications/OpenRastaWebApplication/Default.aspx.cs
@@ -2,8 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace OpenRastaSite
+namespace OpenRastaWebApplication
 {
     public partial class Default : System.Web.UI.Page
     {
--- tests/Agent/IntegrationTests/Applications/OpenRastaWebApplication/Global.asax.cs
+++ tests/Agent/IntegrationTests/Applications/OpenRastaWebApplication/Global.asax.cs
@@ -2,11 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
 
-namespace OpenRastaSite
+namespace OpenRastaWebApplication
 {
     public class Global : System.Web.HttpApplication
     {
+
         protected void Application_Start(object sender, EventArgs e)
         {
 
