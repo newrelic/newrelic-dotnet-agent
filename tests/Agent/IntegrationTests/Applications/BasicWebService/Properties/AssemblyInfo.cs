ecad95ef5b5f56a1b478ecde6c5c96557be03620
lspangler584 <20267975+lspangler584@users.noreply.github.com>
2020-08-11 16:14:05 -0700

    adds StyleCop to IntegrationTests

--- tests/Agent/IntegrationTests/Applications/BasicWebService/Properties/AssemblyInfo.cs
+++ tests/Agent/IntegrationTests/Applications/BasicWebService/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
