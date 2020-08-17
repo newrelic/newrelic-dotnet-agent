439fc7fe12023e1e6cb32545924314a7fc0c56b9
lspangler584 <20267975+lspangler584@users.noreply.github.com>
2020-08-17 09:38:54 -0700

    move stylecop props to separate file

--- src/Agent/NewRelic/Profiler/ConfigurationTest/ConfigurationTestTemplates.h
+++ src/Agent/NewRelic/Profiler/ConfigurationTest/ConfigurationTestTemplates.h
@@ -1,6 +1,5 @@
-/*
-* Copyright 2020 New Relic Corporation. All rights reserved.
-* SPDX-License-Identifier: Apache-2.0
-*/
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #pragma once
 #include "../LoggingTest/LoggingTestTemplates.h"
--- src/Agent/NewRelic/Profiler/ConfigurationTest/InstrumentationConfigurationTest.cpp
+++ src/Agent/NewRelic/Profiler/ConfigurationTest/InstrumentationConfigurationTest.cpp
@@ -1,7 +1,6 @@
-/*
-* Copyright 2020 New Relic Corporation. All rights reserved.
-* SPDX-License-Identifier: Apache-2.0
-*/
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "ConfigurationTestTemplates.h"
