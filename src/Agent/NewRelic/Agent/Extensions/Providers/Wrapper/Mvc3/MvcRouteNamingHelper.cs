8608df6c8bc295c1f4def67e60a3f9453de44f24
lspangler584 <20267975+lspangler584@users.noreply.github.com>
2020-08-26 13:20:27 -0700

    alternate way to get "action"

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/Mvc3/MvcRouteNamingHelper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/Mvc3/MvcRouteNamingHelper.cs
@@ -1,8 +1,9 @@
 // Copyright 2020 New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Collections.Generic;
-using System.Linq;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading;
 using System.Web.Routing;
 using NewRelic.Agent.Extensions.Providers.Wrapper;
 using NewRelic.SystemExtensions;
@@ -27,9 +28,16 @@ namespace NewRelic.Providers.Wrapper.Mvc3
             if (actionName != null)
                 return actionName;
 
-            var directRouteMatches = routeData.Values.GetValueOrDefault("MS_DirectRouteMatches") as IEnumerable<RouteData> ?? Enumerable.Empty<RouteData>();
-            routeData = directRouteMatches.FirstOrDefault() ?? routeData;
-            actionName = routeData.Values.GetValueOrDefault("action") as string;
+            //var directRouteMatches = routeData.Values.GetValueOrDefault("MS_DirectRouteMatches") as IEnumerable<RouteData> ?? Enumerable.Empty<RouteData>();
+            //routeData = directRouteMatches.FirstOrDefault() ?? routeData;
+            //actionName = routeData.Values.GetValueOrDefault("action") as string;
+
+            //var a = routeData.Values;
+            //var b = a["MS_DirectRouteMatches"];
+            //var c = b[0];
+            //actionName = c.Values["action"];
+          
+            actionName = routeData?.Values?["MS_DirectRouteMatches"]?[0]?.Values?["action"];
 
             return actionName ?? "Unknown Action";
         }
