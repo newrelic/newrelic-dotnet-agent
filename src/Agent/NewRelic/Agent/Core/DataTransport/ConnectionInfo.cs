9b78df4e4b1216349f426b2e68034600e2988a77
hsinghkalsi <harkamal.singh@gmail.com>
2020-09-03 12:39:22 -0400

    Adding support for region aware collector URL

--- src/Agent/NewRelic/Agent/Core/DataTransport/ConnectionInfo.cs
+++ src/Agent/NewRelic/Agent/Core/DataTransport/ConnectionInfo.cs
@@ -25,7 +25,7 @@ namespace NewRelic.Agent.Core.DataTransport
 
         public ConnectionInfo(IConfiguration configuration)
         {
-            Host = configuration.CollectorHost;
+            Host = GetCollectorHost(configuration);
             Port = configuration.CollectorPort;
             HttpProtocol = configuration.CollectorHttpProtocol;
             ProxyHost = configuration.ProxyHost;
@@ -40,7 +40,7 @@ namespace NewRelic.Agent.Core.DataTransport
 
         public ConnectionInfo(IConfiguration configuration, string redirectHost)
         {
-            Host = redirectHost ?? configuration.CollectorHost;
+            Host = redirectHost ?? GetCollectorHost(configuration);
             Port = configuration.CollectorPort;
             HttpProtocol = configuration.CollectorHttpProtocol;
             ProxyHost = configuration.ProxyHost;
@@ -53,6 +53,33 @@ namespace NewRelic.Agent.Core.DataTransport
             Proxy = GetWebProxy(ProxyHost, ProxyUriPath, ProxyPort, ProxyUsername, ProxyPassword, ProxyDomain);
         }
 
+        private static string GetCollectorHost(IConfiguration configuration)
+        {
+            const string defaultCollectorUrl = "collector.newrelic.com";
+            const string regionAwareDefaultCollectorUrl = "collector.nr-data.net";
+            const char domainSeparator = '.';
+            const char regionSeparator = 'x';
+
+            if (string.IsNullOrEmpty(configuration.CollectorHost))
+            {
+                return defaultCollectorUrl;
+            }
+
+            if (configuration.AgentLicenseKey != null)
+            {
+                var match = accountRegionRegex.Match(configuration.AgentLicenseKey);
+
+                if (match.Success)
+                {
+                    var regionSegment = match.Value.TrimEnd(regionSeparator);
+                    var collectorUrlRegionStartPosition = regionAwareDefaultCollectorUrl.IndexOf(domainSeparator) + 1;
+                    var regionAwareCollectorUrl = regionAwareDefaultCollectorUrl.Insert(collectorUrlRegionStartPosition, regionSegment + domainSeparator);
+                    return regionAwareCollectorUrl;
+                }
+            }
+
+            return defaultCollectorUrl;
+        }
 
         private static WebProxy GetWebProxy(string proxyHost, string proxyUriPath, int proxyPort, string proxyUsername, string proxyPassword, string proxyDomain)
         {
