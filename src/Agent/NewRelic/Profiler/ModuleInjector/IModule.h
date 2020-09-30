d494fd2a9b2892004f9eee5c39ffb6a82662d5df
Jason Feingold <jfeingold@newrelic.com>
2020-09-30 14:08:34 -0700

    Updated Profiler to inject mscorlib assembly reference into netstandard modules without mscorlib in .net framework apps

--- src/Agent/NewRelic/Profiler/ModuleInjector/IModule.h
+++ src/Agent/NewRelic/Profiler/ModuleInjector/IModule.h
@@ -5,6 +5,7 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <atlcomcli.h>
 #include "../Common/Macros.h"
 #include "../Sicily/codegen/ITokenizer.h"
 
@@ -17,7 +18,18 @@ namespace NewRelic { namespace Profiler { namespace ModuleInjector
         virtual void InjectPlatformInvoke(const std::wstring& methodName, const std::wstring& className, const std::wstring& moduleName, const ByteVector& signature) = 0;
         virtual void InjectStaticSecuritySafeMethod(const std::wstring& methodName, const std::wstring& className, const ByteVector& signature) = 0;
         virtual void InjectMscorlibSecuritySafeMethodReference(const std::wstring& methodName, const std::wstring& className, const ByteVector& signature) = 0;
-        
+
+        virtual bool GetHasRefMscorlib() = 0;
+        virtual bool GetHasRefSysRuntime() = 0;
+        virtual bool GetHasRefNetStandard() = 0;
+
+        virtual void SetMscorlibAssemblyRef(mdAssembly assemblyRefToken) = 0;
+
+        virtual bool GetIsThisTheMscorlibAssembly() = 0;
+        virtual bool GetIsThisTheNetStandardAssembly() = 0;
+
+        virtual CComPtr<IMetaDataAssemblyEmit> GetMetaDataAssemblyEmit() = 0;
+
         virtual sicily::codegen::ITokenizerPtr GetTokenizer() = 0;
     };
 
