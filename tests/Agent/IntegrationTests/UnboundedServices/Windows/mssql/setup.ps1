72865853f986a0d9417f3fde4159714d08b0e260
lspangler584 <20267975+lspangler584@users.noreply.github.com>
2020-10-06 18:49:00 -0700

    NR db comes alive, 1 mssql test passes'

--- tests/Agent/IntegrationTests/UnboundedServices/Windows/mssql/setup.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "setup.ps1"
-# Starts a loop to check if SQL is ready before running setup script.
-DO
-{
-    sqlcmd -S localhost -U sa -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h -1
-    Start-Sleep 1s
-} WHILE ($LASTEXITCODE -ne 0)
-
-.\sqlcmd -S localhost -U sa -i \restore.sql
