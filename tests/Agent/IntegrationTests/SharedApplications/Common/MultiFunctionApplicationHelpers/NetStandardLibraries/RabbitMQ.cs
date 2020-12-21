299bbd36b4c27e5c5e448ec3848d44917c92d1d2
Alex Hemsath <ahemsath@newrelic.com>
2020-12-21 14:06:41 -0800

    Add SharedApplications to unbounded solution

---
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/RabbitMQ.cs
@@ -0,0 +1,13 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiFunctionApplicationHelpers.NetStandardLibraries
+{
+    class RabbitMQ
+    {
+    }
+}
