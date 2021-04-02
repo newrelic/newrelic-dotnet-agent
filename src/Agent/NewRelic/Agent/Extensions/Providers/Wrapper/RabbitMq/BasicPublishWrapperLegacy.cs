1f196040de877ce81838c39c259dbf266fe9d085
Vu Tran <vtran@newrelic.com>
2021-04-01 17:22:35 -0700

    Removes hard coded instrumentation points in rabbit wrappers.

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/RabbitMq/BasicPublishWrapperLegacy.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/RabbitMq/BasicPublishWrapperLegacy.cs
@@ -8,19 +8,15 @@ namespace NewRelic.Providers.Wrapper.RabbitMq
 {
     public class BasicPublishWrapperLegacy : IWrapper
     {
+        private const string WrapperName = "BasicPublishWrapperLegacy";
+
         private const int BasicPropertiesIndex = 4;
 
         public bool IsTransactionRequired => true;
 
         public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
         {
-            var method = methodInfo.Method;
-            var canWrap = method.MatchesAny(assemblyName: "RabbitMQ.Client", typeName: "RabbitMQ.Client.Framing.Impl.Model",
-                methodSignatures: new[]
-                {
-                    new MethodSignature("_Private_BasicPublish","System.String,System.String,System.Boolean,System.Boolean,RabbitMQ.Client.IBasicProperties,System.Byte[]"), // 3.5.X
-				});
-            return new CanWrapResponse(canWrap);
+            return new CanWrapResponse(WrapperName.Equals(methodInfo.RequestedWrapperName));
         }
 
         public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
