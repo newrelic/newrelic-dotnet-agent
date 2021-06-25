e9f8d4bd49926b0f32dabc128c291fdc14b6494c
Josh Coleman <jcoleman@newrelic.com>
2021-06-25 14:20:19 -0700

    Disable new routing rule that broke an upgraded .netcore21 mvc app

--- tests/Agent/IntegrationTests/Applications/AspNetCoreMvcAsyncApplication/Startup.cs
+++ tests/Agent/IntegrationTests/Applications/AspNetCoreMvcAsyncApplication/Startup.cs
@@ -23,7 +23,7 @@ namespace AspNetCoreMvcAsyncApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(x => x.EnableEndpointRouting = false);
+            services.AddMvc(x => { x.EnableEndpointRouting = false; x.SuppressAsyncSuffixInActionNames = false; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
