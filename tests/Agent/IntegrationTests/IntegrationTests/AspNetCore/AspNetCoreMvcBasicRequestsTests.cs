1203c2ff29b3614cac89617e556f7a7bc9e04169
Basil Abusamrah <tehbio@gmail.com>
2021-07-07 13:14:43 -0600

    Added request method capturing to AspNetCore + test assertion.

--- tests/Agent/IntegrationTests/IntegrationTests/AspNetCore/AspNetCoreMvcBasicRequestsTests.cs
+++ tests/Agent/IntegrationTests/IntegrationTests/AspNetCore/AspNetCoreMvcBasicRequestsTests.cs
@@ -72,6 +72,7 @@ namespace NewRelic.Agent.IntegrationTests.AspNetCore
 
             Assert.NotNull(transactionSample);
             Assertions.TransactionTraceSegmentsExist(expectedTransactionTraceSegments, transactionSample);
+            Assertions.TransactionTraceHasAttributes(new Dictionary<string, string>{ {"request.method", "GET" } }, TransactionTraceAttributeType.Agent, transactionSample);
 
             var getTransactionEvent = _fixture.AgentLog.TryGetTransactionEvent("WebTransaction/MVC/Home/Index");
 
