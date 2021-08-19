acdffca0fdb9b5a07057ff9e5f7277a8da17fb68
Josh Coleman <jcoleman@newrelic.com>
2021-08-18 19:11:08 -0700

    Remove only customer facing obsolete methods

--- src/Agent/NewRelic.Api.Agent/NoOpTransaction.cs
+++ src/Agent/NewRelic.Api.Agent/NoOpTransaction.cs
@@ -8,17 +8,7 @@ namespace NewRelic.Api.Agent
 {
     internal class NoOpTransaction : ITransaction
     {
-        private static IDistributedTracePayload _noOpDistributedTracePayload = new NoOpDistributedTracePayload();
         private static ISpan _noOpSpan = new NoOpSpan();
-
-        public void AcceptDistributedTracePayload(string payload, TransportType transportType = TransportType.Unknown)
-        {
-        }
-
-        public IDistributedTracePayload CreateDistributedTracePayload()
-        {
-            return _noOpDistributedTracePayload;
-        }
 
         public ITransaction AddCustomAttribute(string key, object value)
         {
