bedc46392eabefed2e89dca98dfe75202393f51f
Basil Abusamrah <tehbio@gmail.com>
2021-08-27 14:54:02 -0600

    Added new span_event_harvest_config from server, and updated to use it.

---
+++ src/Agent/NewRelic/Agent/Core/Configuration/SingleEventHarvestConfig.cs
@@ -0,0 +1,20 @@
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace NewRelic.Agent.Core.Configuration
+{
+    public class SingleEventHarvestConfig
+    {
+        [JsonProperty("report_period_ms")]
+        public int? ReportPeriodMs { get; set; }
+
+        [JsonProperty("harvest_limit")]
+        public int HarvestLimit { get; set; }
+
+        public TimeSpan? HarvestCycle => ReportPeriodMs.HasValue ? TimeSpan.FromMilliseconds(ReportPeriodMs.Value) : (TimeSpan?)null;
+    }
+}
