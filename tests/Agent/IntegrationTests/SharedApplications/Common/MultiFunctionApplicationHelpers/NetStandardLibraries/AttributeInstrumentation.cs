b2984e5abb9a8a4b9fd4fe389c0adc0f34cee3a7
Alex Hemsath <ahemsath@newrelic.com>
2021-10-18 14:27:31 -0700

    Unify framework and core attribute instrumentation tests

--- tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/AttributeInstrumentation.cs
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/AttributeInstrumentation.cs
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 
+using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using NewRelic.Agent.IntegrationTests.Shared.ReflectionHelpers;
 using NewRelic.Api.Agent;
 
@@ -11,20 +13,97 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Internal
     [Library]
     internal static class AttributeInstrumentation
     {
+        /// <summary>
+        /// Synchronous method creating a Web transaction
+        /// </summary>
+        [LibraryMethod]
+        [Transaction(Web = true)]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        public static void MakeWebTransaction()
+        {
+            DoSomeWork();
+        }
+
+        /// <summary>
+        /// Synchronous method creating a Web transaction and utilizing SetTransactionUri
+        /// </summary>
+        [LibraryMethod]
+        [Transaction(Web = true)]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        public static void MakeWebTransactionWithCustomUri()
+        {
+            var uri = new Uri("http://foo.bar.net/fizz/buzz");
+            NewRelic.Api.Agent.NewRelic.SetTransactionUri(uri);
+            DoSomeWork();
+        }
+
+        /// <summary>
+        /// Synchronous method creating an Other transaction
+        /// </summary>
         [LibraryMethod]
         [Transaction]
         [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-        public static void DoSomething()
+        public static void MakeOtherTransaction()
         {
-            DoSomethingInside();
+            DoSomeWork();
         }
 
+        /// <summary>
+        /// Async method creating an Other transaction
+        /// </summary>
+        /// <returns></returns>
+        [LibraryMethod]
+        [Transaction]
         [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-        [Trace]
-        private static void DoSomethingInside()
+        public static async Task MakeOtherTransactionAsync()
         {
-            System.Threading.Thread.Sleep(2000);
+            await DoSomeWorkAsync();
         }
+
+        /// <summary>
+        /// Synchronous method that calls async methods.
+        /// This use case illustrates that this scenario is possible, but it is
+        /// recommended that transactions involving async code start with an 
+        /// async entry point to avoid potential pitfalls.
+        /// </summary>
+        [LibraryMethod]
+        [Transaction]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        public static void MakeOtherTransactionThenCallAsyncMethod()
+        {
+            DoSomeWorkAsync().Wait();
+        }
+
+        [LibraryMethod]
+        [Transaction]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        public static void MakeOtherTransactionWithCallToNetStandardMethod()
+        {
+            var myObj = new NetStandardTestLibrary.MyClass();
+            myObj.MyMethodToBeInstrumented();
+        }
+
+        [Trace]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        private static void DoSomeWork()
+        {
+        }
+
+        [Trace]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        private static async Task<string> DoSomeWorkAsync()
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+            return await DoSomeMoreWorkAsync();
+        }
+
+        [Trace]
+        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        private static async Task<string> DoSomeMoreWorkAsync()
+        {
+            return await Task.FromResult("Some work.");
+        }
+
 
     }
 }
--- tests/Agent/IntegrationTests/SharedApplications/Common/NetStandardTestLibrary/MyClass.cs
+++ tests/Agent/IntegrationTests/SharedApplications/Common/NetStandardTestLibrary/MyClass.cs
@@ -4,7 +4,7 @@
 
 using System.Runtime.CompilerServices;
 
-namespace NetStandardClassLibrary
+namespace NetStandardTestLibrary
 {
     public class MyClass
     {
