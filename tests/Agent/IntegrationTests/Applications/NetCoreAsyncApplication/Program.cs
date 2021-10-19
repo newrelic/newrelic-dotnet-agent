4202ebf5b14580ba41e88c131be17fd86281e9f3
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2021-10-19 11:06:21 -0700

    Use named pipes for test app ipc (#762)
    * Using named pipes for test application process control
    
    * Get test using AspNetCoreMvcBasicRequestsApplication to pass on Windows
    
    * Refactor test app lifecycle management into helper library
    
    * Refactor lifecycle management for AspNetCore3BasicWebApiApp
    
    * Refactored several apps to use shared lifecycle management code
    
    * oops, missed a spot
    
    * Refactor SerilogSumologic app
    
    * Move net core attribute instrumentation tests to use ConsoleMF app
    
    * Remove NetCoreAttributeInstrumentationFixture
    
    * Unify framework and core attribute instrumentation tests

--- tests/Agent/IntegrationTests/Applications/NetCoreAsyncApplication/Program.cs
+++ tests/Agent/IntegrationTests/Applications/NetCoreAsyncApplication/Program.cs
@@ -10,33 +10,17 @@ using System.Reflection;
 using System.Threading;
 using Microsoft.CodeAnalysis;
 using System.Threading.Tasks;
+using ApplicationLifecycle;
 
 namespace NetCoreAsyncApplication
 {
     class Program
     {
-        private const string DefaultPort = "5001";
-
         private static string _port;
-
-        private static string _applicationName;
 
         static void Main(string[] args)
         {
-            var commandLine = string.Join(" ", args);
-
-            _applicationName = Path.GetFileNameWithoutExtension(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath) + ".exe";
-
-            Console.WriteLine($"[{_applicationName}] Joined args: {commandLine}");
-
-            var result = CommandLineParser.SplitCommandLineIntoArguments(commandLine, true).ToList();
-
-            var argPort = result.FirstOrDefault()?.Split('=')[1];
-            _port = argPort ?? DefaultPort;
-
-            Console.WriteLine($"[{_applicationName}] Received port: {argPort} | Using port: {_port}");
-
-            var eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "app_server_wait_for_all_request_done_" + _port);
+            _port = AppLifecycleManager.GetPortFromArgs(args);
 
             var cancellationTokenSource = new CancellationTokenSource();
             cancellationTokenSource.CancelAfter(TimeSpan.FromMinutes(5));
@@ -44,10 +28,9 @@ namespace NetCoreAsyncApplication
             var doWorkTask = Task.Run(async () => await DoWorkAsync(), cancellationTokenSource.Token);
             doWorkTask.Wait(cancellationTokenSource.Token);
 
-            //Wait to create the Pid file until the work is done so that the agent has a chance to attach and create log directory and log files.
-            CreatePidFile();
+            AppLifecycleManager.CreatePidFile();
 
-            eventWaitHandle.WaitOne(TimeSpan.FromMinutes(5));
+            AppLifecycleManager.WaitForTestCompletion(_port);
         }
 
         private static async Task DoWorkAsync()
@@ -59,15 +42,5 @@ namespace NetCoreAsyncApplication
             await asyncUseCases.CpuBoundTasksAsync();
         }
 
-        private static void CreatePidFile()
-        {
-            var pid = Process.GetCurrentProcess().Id;
-            var applicationDirectory =
-                Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath),
-                    _applicationName);
-            var pidFilePath = applicationDirectory + ".pid";
-            var file = File.CreateText(pidFilePath);
-            file.WriteLine(pid);
-        }
     }
 }
---
+++ tests/Agent/IntegrationTests/IntegrationTests/CustomInstrumentation/AttributeInstrumentationTests.cs
@@ -0,0 +1,139 @@
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+using MultiFunctionApplicationHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using NewRelic.Agent.IntegrationTestHelpers;
+using NewRelic.Testing.Assertions;
+using Xunit;
+using Xunit.Abstractions;
+using NewRelic.Agent.IntegrationTestHelpers.Models;
+
+namespace NewRelic.Agent.IntegrationTests.CustomInstrumentation
+{
+    [NetFrameworkTest]
+    public class AttributeInstrumentationTestsFW461 : AttributeInstrumentationTests<ConsoleDynamicMethodFixtureFW461>
+    {
+        public AttributeInstrumentationTestsFW461(ConsoleDynamicMethodFixtureFW461 fixture, ITestOutputHelper output)
+            : base(fixture, output)
+        {
+        }
+    }
+
+    [NetCoreTest]
+    public class AttributeInstrumentationTestsCore31 : AttributeInstrumentationTests<ConsoleDynamicMethodFixtureCore31>
+    {
+        public AttributeInstrumentationTestsCore31(ConsoleDynamicMethodFixtureCore31 fixture, ITestOutputHelper output)
+            : base(fixture, output)
+        {
+        }
+    }
+
+    public abstract class AttributeInstrumentationTests<TFixture> : NewRelicIntegrationTest<TFixture> where TFixture : ConsoleDynamicMethodFixture
+    {
+        private const string LibraryClassName = "MultiFunctionApplicationHelpers.NetStandardLibraries.Internal.AttributeInstrumentation";
+
+        private readonly string[] TestCommands = new string[]
+            {
+                // Web transactions (i.e. [Transaction(Web = true)]
+                "MakeWebTransaction",
+                "MakeWebTransactionWithCustomUri",
+                // Other transactions (i.e. [Transaction] or [Transaction(Web = false)])
+                "MakeOtherTransaction",
+                "MakeOtherTransactionAsync",
+                "MakeOtherTransactionThenCallAsyncMethod",
+                "MakeOtherTransactionWithCallToNetStandardMethod"
+            };
+
+
+        protected readonly TFixture Fixture;
+
+        public AttributeInstrumentationTests(TFixture fixture, ITestOutputHelper output) : base(fixture)
+        {
+            Fixture = fixture;
+            Fixture.TestLogger = output;
+
+            foreach (var testCommand in TestCommands)
+            {
+                Fixture.AddCommand($"AttributeInstrumentation {testCommand}");
+            }
+
+            Fixture.Actions
+            (
+                setupConfiguration: () =>
+                {
+                    var configPath = fixture.DestinationNewRelicConfigFilePath;
+                    var configModifier = new NewRelicConfigModifier(configPath);
+                    configModifier.ForceTransactionTraces();
+                }
+            );
+
+            Fixture.Initialize();
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var expectedMetrics = new List<Assertions.ExpectedMetric>
+            {
+                new Assertions.ExpectedMetric {metricName = $"WebTransaction", callCount = 2},
+                new Assertions.ExpectedMetric {metricName = $"OtherTransaction/all", callCount = 4},
+
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWork", callCount = 3},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWorkAsync", callCount = 2},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeMoreWorkAsync", callCount = 2},
+
+                new Assertions.ExpectedMetric {metricName = $"WebTransaction/Custom/{LibraryClassName}/MakeWebTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeWebTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeWebTransaction", metricScope = $"WebTransaction/Custom/{LibraryClassName}/MakeWebTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWork", metricScope = $"WebTransaction/Custom/{LibraryClassName}/MakeWebTransaction", callCount = 1},
+
+                new Assertions.ExpectedMetric {metricName = $"WebTransaction/Uri/fizz/buzz", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeWebTransactionWithCustomUri", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeWebTransactionWithCustomUri", metricScope = "WebTransaction/Uri/fizz/buzz", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWork", metricScope = "WebTransaction/Uri/fizz/buzz", callCount = 1},
+
+                new Assertions.ExpectedMetric {metricName = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransaction", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransaction", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWork", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransaction", callCount = 1},
+
+                new Assertions.ExpectedMetric {metricName = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWorkAsync", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeMoreWorkAsync", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionThenCallAsyncMethod", callCount = 1},
+
+                new Assertions.ExpectedMetric {metricName = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionAsync", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionAsync", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionAsync", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionAsync", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeWorkAsync", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionAsync", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/DoSomeMoreWorkAsync", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionAsync", callCount = 1},
+
+                new Assertions.ExpectedMetric {metricName = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionWithCallToNetStandardMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionWithCallToNetStandardMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/{LibraryClassName}/MakeOtherTransactionWithCallToNetStandardMethod", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionWithCallToNetStandardMethod", callCount = 1},
+                new Assertions.ExpectedMetric {metricName = $"DotNet/NetStandardTestLibrary.MyClass/MyMethodToBeInstrumented", metricScope = $"OtherTransaction/Custom/{LibraryClassName}/MakeOtherTransactionWithCallToNetStandardMethod", callCount = 1},
+            };
+
+            var expectedTransactionEventAgentAttributes = new Dictionary<string, string>
+            {
+                { "request.uri", "/fizz/buzz" }
+            };
+
+
+            var metrics = Fixture.AgentLog.GetMetrics().ToList();
+            var transactionEvent = Fixture.AgentLog.GetTransactionEvents()
+                .Where(e => e.IntrinsicAttributes["name"].ToString() == "WebTransaction/Uri/fizz/buzz")
+                .FirstOrDefault();
+
+
+            NrAssert.Multiple(
+                () => Assertions.MetricsExist(expectedMetrics, metrics),
+                () => Assertions.TransactionEventHasAttributes(expectedTransactionEventAgentAttributes, TransactionEventAttributeType.Agent, transactionEvent)
+            );
+        }
+    }
+}
