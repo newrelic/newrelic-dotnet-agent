fe7a9364b71c9650ace196ccaaa9ee9d86cac640
Vu Tran <56414817+vuqtran88@users.noreply.github.com>
2021-12-03 19:37:28 +0000

    Adds NServiceBus 6/7 message receive instrumentation. (#830)
    * Adds NServiceBus 6/7 message receive instrumentation.
    Refactoring.
    
    * Update ReceiveMessageWrapper.cs
    
    Removes comment which resulted from copy/paste.

---
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/NServiceBus/NServiceBusHelpers.cs
@@ -0,0 +1,125 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using NewRelic.Agent.Api;
+using NewRelic.Agent.Extensions.Providers.Wrapper;
+using NewRelic.Reflection;
+
+namespace NewRelic.Providers.Wrapper.NServiceBus
+{
+    public class NServiceBusHelpers
+    {
+        private static Func<object, Dictionary<string, string>> _getHeadersFunc;
+        private static Func<object, object> _getIncomingLogicalMessageFunc;
+
+        private static Func<object, object> _getMessageFromIncomingLogicalMessageContextFunc;
+        private static Func<object, Dictionary<string, string>> _getHeadersFromIncomingLogicalMessageContextFunc;
+
+
+        private static Func<object, Type> _getMessageTypeFunc;
+
+        public static object GetMessageFromIncomingLogicalMessageContext(object incomingLogicalMessageContext)
+        {
+            var getLogicalMessageContextFunc = _getMessageFromIncomingLogicalMessageContextFunc ??= VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(incomingLogicalMessageContext.GetType(), "Message");
+            return getLogicalMessageContextFunc(incomingLogicalMessageContext);
+        }
+
+        public static Dictionary<string, string> GetHeadersFromIncomingLogicalMessageContext(object incomingLogicalMessageContext)
+        {
+            var getHeadersFromIncomingLogicalMessageContextFunc = _getHeadersFromIncomingLogicalMessageContextFunc ??= VisibilityBypasser.Instance.GeneratePropertyAccessor<Dictionary<string, string>>(incomingLogicalMessageContext.GetType(), "Headers");
+            return getHeadersFromIncomingLogicalMessageContextFunc(incomingLogicalMessageContext);
+        }
+
+        public static object GetIncomingLogicalMessage(object incomingContext)
+        {
+            var getLogicalMessageFunc = _getIncomingLogicalMessageFunc ??= VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(incomingContext.GetType(), "IncomingLogicalMessage");
+            return getLogicalMessageFunc(incomingContext);
+        }
+
+        public static Dictionary<string, string> GetHeaders(object logicalMessage)
+        {
+            var getHeadersFunc = _getHeadersFunc ??= VisibilityBypasser.Instance.GeneratePropertyAccessor<Dictionary<string, string>>(logicalMessage.GetType(), "Headers");
+            return getHeadersFunc(logicalMessage);
+        }
+
+        public static void SetHeaders(object logicalMessage, Dictionary<string, string> headers)
+        {
+            // Unlike the GetHeaders function, we can't cache this action.  It is only valid for the specific logicalMessage object instance provided.
+            var action = VisibilityBypasser.Instance.GeneratePropertySetter<Dictionary<string, string>>(logicalMessage, "Headers");
+
+            action(headers);
+        }
+
+        /// <summary>
+        /// Returns a metric name based on the type of message. The source/destination queue isn't always known (depending on the circumstances) and in some cases isn't even relevant. The message type is always known and is always relevant.
+        /// </summary>
+        /// <param name="logicalMessage"></param>
+        /// <returns></returns>
+        public static string TryGetQueueName(object logicalMessage)
+        {
+            var getMessageTypeFunc = _getMessageTypeFunc ??= VisibilityBypasser.Instance.GeneratePropertyAccessor<Type>(logicalMessage.GetType(), "MessageType");
+
+            var messageType = getMessageTypeFunc(logicalMessage);
+
+            if (messageType == null)
+            {
+                return null;
+            }
+
+            return messageType.FullName;
+        }
+
+        public static void ProcessHeaders(Dictionary<string, string> headers, IAgent agent)
+        {
+            agent.CurrentTransaction.AcceptDistributedTraceHeaders(headers, GetHeaderValue, TransportType.HTTP);
+        }
+
+        public static void CreateOutboundHeaders(IAgent agent, object logicalMessage)
+        {
+            // not sure why we just bail if the headers are null, we might not want to keep this behavior
+            var headers = GetHeaders(logicalMessage);
+            if (headers == null)
+            {
+                return;
+            }
+
+            var setHeaders = new Action<object, string, string>((carrier, key, value) =>
+            {
+                var headers = GetHeaders(carrier);
+
+                if (headers == null)
+                {
+                    headers = new Dictionary<string, string>();
+                    SetHeaders(carrier, headers);
+                }
+                else if (headers is IReadOnlyDictionary<string, object>)
+                {
+                    headers = new Dictionary<string, string>(headers);
+                    SetHeaders(carrier, headers);
+                }
+
+                headers[key] = value;
+            });
+
+            agent.CurrentTransaction.InsertDistributedTraceHeaders(logicalMessage, setHeaders);
+        }
+
+        private static IEnumerable<string> GetHeaderValue(Dictionary<string, string> carrier, string key)
+        {
+            if (carrier != null)
+            {
+                foreach (var item in carrier)
+                {
+                    if (item.Key.Equals(key, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return new string[] { item.Value };
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/NServiceBus/SendMessageWrapper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/NServiceBus/SendMessageWrapper.cs
@@ -2,11 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using NewRelic.Agent.Api;
 using NewRelic.Agent.Extensions.Providers.Wrapper;
 using NewRelic.SystemExtensions;
-using NServiceBus.Unicast.Messages;
 
 namespace NewRelic.Providers.Wrapper.NServiceBus
 {
@@ -17,54 +16,33 @@ namespace NewRelic.Providers.Wrapper.NServiceBus
     {
         public bool IsTransactionRequired => true;
 
+        private const string WrapperName = "SendMessageWrapper";
+        private const string BrokerVendorName = "NServiceBus";
+
+        private const string LogicalMessageType = "NServiceBus.Unicast.Messages.LogicalMessage";
+        private const int LogicalMessageIndex = 1;
+
+
         public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
         {
-            var method = methodInfo.Method;
-            var canWrap = method.MatchesAny(assemblyName: "NServiceBus.Core", typeName: "NServiceBus.Unicast.UnicastBus", methodName: "SendMessage", parameterSignature: "NServiceBus.Unicast.SendOptions,NServiceBus.Unicast.Messages.LogicalMessage");
-            return new CanWrapResponse(canWrap);
+            return new CanWrapResponse(WrapperName.Equals(methodInfo.RequestedWrapperName));
         }
 
         public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
         {
-            var logicalMessage = instrumentedMethodCall.MethodCall.MethodArguments.ExtractNotNullAs<LogicalMessage>(1);
+            var logicalMessage = instrumentedMethodCall.MethodCall.MethodArguments.ExtractNotNullAs<object>(LogicalMessageIndex);
 
-            const string brokerVendorName = "NServiceBus";
-            var queueName = TryGetQueueName(logicalMessage);
-            var segment = transaction.StartMessageBrokerSegment(instrumentedMethodCall.MethodCall, MessageBrokerDestinationType.Queue, MessageBrokerAction.Produce, brokerVendorName, queueName);
-
-            CreateOutboundHeaders(agent, logicalMessage);
-            return Delegates.GetDelegateFor(segment);
-        }
-
-        private static void CreateOutboundHeaders(IAgent agent, LogicalMessage logicalMessage)
-        {
-            if (logicalMessage.Headers == null)
-                return;
-
-            var setHeaders = new Action<LogicalMessage, string, string>((carrier, key, value) =>
+            //If the NServiceBus version doesn't provide the LogicalMessage parameter we just bail.
+            if (logicalMessage.GetType().FullName != LogicalMessageType)
             {
-                if (carrier.Headers.ContainsKey(key))
-                {
-                    carrier.Headers.Remove(key);
-                }
+                return Delegates.NoOp;
+            }
 
-                carrier.Headers.Add(key, value);
-            });
+            var queueName = NServiceBusHelpers.TryGetQueueName(logicalMessage);
+            var segment = transaction.StartMessageBrokerSegment(instrumentedMethodCall.MethodCall, MessageBrokerDestinationType.Queue, MessageBrokerAction.Produce, BrokerVendorName, queueName);
 
-            agent.CurrentTransaction.InsertDistributedTraceHeaders(logicalMessage, setHeaders);
-        }
-
-        /// <summary>
-        /// Returns a metric name based on the type of message. The source/destination queue isn't always known (depending on the circumstances) and in some cases isn't even relevant. The message type is always known and is always relevant.
-        /// </summary>
-        /// <param name="logicalMessage"></param>
-        /// <returns></returns>
-        private static string TryGetQueueName(LogicalMessage logicalMessage)
-        {
-            if (logicalMessage.MessageType == null)
-                return null;
-
-            return logicalMessage.MessageType.FullName;
+            NServiceBusHelpers.CreateOutboundHeaders(agent, logicalMessage);
+            return Delegates.GetDelegateFor(segment);
         }
     }
 }
