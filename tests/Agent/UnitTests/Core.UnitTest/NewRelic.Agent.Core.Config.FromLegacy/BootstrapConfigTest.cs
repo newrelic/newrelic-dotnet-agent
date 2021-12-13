ecc0bb8d7e273d9ee2851133679d82a5f37330a9
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2021-12-13 14:54:47 -0800

    Guard against agent config file schema validation errors preventing agent startup; unify framework/core behavior (#835)
    * Just use the config xsd on disk for both agents
    
    * Fix unit test failure, refactor
    
    * Unit tests reference configuration.xsd from disk instead of from embedded resource
    
    * Add unit test to cover missing schema file case
    
    * Remove embedded resource from core DLL

--- tests/Agent/UnitTests/Core.UnitTest/NewRelic.Agent.Core.Config.FromLegacy/BootstrapConfigTest.cs
+++ tests/Agent/UnitTests/Core.UnitTest/NewRelic.Agent.Core.Config.FromLegacy/BootstrapConfigTest.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace NewRelic.Agent.Core.Config
@@ -12,17 +13,49 @@ namespace NewRelic.Agent.Core.Config
         [Test]
         public void TestInvalidServiceAttribute()
         {
+            var bogusConfigXml = "<configuration xmlns=\"urn:newrelic-config\">" +
+                                 "<service bogus=\"true\" licenseKey=\"dude\"/>" +
+                                 "<application><name>My App</name></application>" +
+                                 "</configuration>";
+
+            var xsdFile = Path.Combine(TestContext.CurrentContext.TestDirectory, "Configuration.xsd");
+            Func<string> configSchemaSource = () => File.ReadAllText(xsdFile);
+
             using (var logging = new TestUtilities.Logging())
             {
                 // this should load with an error in the event log
-                ConfigurationLoader.InitializeFromXml(
-                    "<configuration xmlns=\"urn:newrelic-config\">" +
-                    "<service bogus=\"true\" licenseKey=\"dude\"/>" +
-                    "<application><name>My App</name></application>" +
-                    "</configuration>");
+                ConfigurationLoader.InitializeFromXml(bogusConfigXml, configSchemaSource);
+
                 var errorMessage = Type.GetType("Mono.Runtime") == null ?
                         "An error occurred parsing newrelic.config - The 'bogus' attribute is not declared." :
                         "An error occurred parsing newrelic.config - XmlSchema error: Attribute declaration was not found for bogus";
+                Assert.IsTrue(logging.HasMessageThatContains(errorMessage));
+            }
+        }
+
+        [Test]
+        public void TestMissingOrEmptyConfigXsd()
+        {
+            // The config XML is irrelvant to this test, so just copied this from the other test
+            var bogusConfigXml = "<configuration xmlns=\"urn:newrelic-config\">" +
+                                 "<service bogus=\"true\" licenseKey=\"dude\"/>" +
+                                 "<application><name>My App</name></application>" +
+                                 "</configuration>";
+
+            // Have the config schema source yield an empty string, as would happen if the
+            // actual newrelic.xsd on disk was missing or empty
+            Func<string> configSchemaSource = () => string.Empty;
+
+            using (var logging = new TestUtilities.Logging())
+            {
+                // this should load with an error in the event log
+                ConfigurationLoader.InitializeFromXml(bogusConfigXml, configSchemaSource);
+
+                // While this error message is somewhat cryptic, in an actual agent run it would be
+                // preceeded by a warning message regarding failure to read the schema file contents from disk
+                var errorMessage = Type.GetType("Mono.Runtime") == null ?
+                        "An error occurred parsing newrelic.config - Root element is missing." :
+                        "An error occurred parsing newrelic.config - Root element is missing.";
                 Assert.IsTrue(logging.HasMessageThatContains(errorMessage));
             }
         }
