709a707581004a61dbdbbaeea1a6be14fe2d45e0
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2021-12-17 15:19:55 -0800

    Update Linux package build/test and smoke test docker containers (#859)
    * Updating linux package build and test assets to use net6.0.  work in progress
    
    * Make sure that we're installing the correct architecture agent - this is working now
    
    * Update smoke test to net6.0
    
    * Remove stray file that got checked in, and update gitignore
    
    * Remove troubleshooting change

--- build/Linux/test/scripts/mvc_test.sh
+++ build/Linux/test/scripts/mvc_test.sh
@@ -9,7 +9,7 @@ install_agent
 verify_no_logs
 
 CORECLR_ENABLE_PROFILING=0 dotnet build
-dotnet bin/Debug/netcoreapp2.1/mvc.dll &
+dotnet bin/Debug/net6.0/mvc.dll &
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:5000); do
     printf '.'
