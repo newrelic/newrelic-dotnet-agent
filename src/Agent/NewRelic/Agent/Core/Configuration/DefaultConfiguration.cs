3c0f20c6c7d887b342c68812306b44772d57a6ad
Jacob Affinito <jaffinito@newrelic.com>
2022-02-18 08:59:04 -0800

    Swapped env and server preference

--- src/Agent/NewRelic/Agent/Core/Configuration/DefaultConfiguration.cs
+++ src/Agent/NewRelic/Agent/Core/Configuration/DefaultConfiguration.cs
@@ -1808,9 +1808,9 @@ namespace NewRelic.Agent.Core.Configuration
         {
             get
             {
-                return ServerOverrides(_serverConfiguration.EventHarvestConfig?.LogEventHarvestLimit(),
-                    EnvironmentOverrides(_localConfiguration.applicationLogging.forwarding.maxSamplesStored, "NEW_RELIC_APPLICATION_LOGGING_MAX_SAMPLES_STORED")
-                    ?? _localConfiguration.applicationLogging.forwarding.maxSamplesStored);
+                return (int)EnvironmentOverrides(
+                    ServerOverrides(_serverConfiguration.EventHarvestConfig?.LogEventHarvestLimit(), _localConfiguration.applicationLogging.forwarding.maxSamplesStored),
+                    "NEW_RELIC_APPLICATION_LOGGING_MAX_SAMPLES_STORED");
             }
         }
 
