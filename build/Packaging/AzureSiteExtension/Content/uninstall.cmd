83be72f3c17f9ea9586f3c8391e6b51295e1ebc4
Josh Coleman <jcoleman@newrelic.com>
2022-03-14 10:28:52 -0700

    Better cleanup logic

--- build/Packaging/AzureSiteExtension/Content/uninstall.cmd
+++ build/Packaging/AzureSiteExtension/Content/uninstall.cmd
@@ -1,12 +1,7 @@
 :: Copyright 2020 New Relic Corporation. All rights reserved.
 :: SPDX-License-Identifier: Apache-2.0
 
-SET NEW_RELIC_FOLDER="%HOME%\NewRelicAgent\newrelic"
-IF EXIST %NEW_RELIC_FOLDER% (
-  rd /S /q %NEW_RELIC_FOLDER%
-)
-
-SET NEW_RELIC_FOLDER="%HOME%\NewRelicAgent\newrelic_core"
+SET NEW_RELIC_FOLDER="%HOME%\NewRelicAgent"
 IF EXIST %NEW_RELIC_FOLDER% (
   rd /S /q %NEW_RELIC_FOLDER%
 )
