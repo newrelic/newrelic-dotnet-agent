93ed599252404540e08819c4f2cb71dac0583b6e
Vu Tran <vuqtran88@gmail.com>
2022-05-05 12:04:34 -0700

    Accidentally committed this. Revert this change.

--- tests/Agent/IntegrationTests/IntegrationTests/Logging/MetricsAndForwardingTests.cs
+++ tests/Agent/IntegrationTests/IntegrationTests/Logging/MetricsAndForwardingTests.cs
@@ -138,7 +138,7 @@ namespace NewRelic.Agent.IntegrationTests.Logging
                     .EnableLogMetrics(metricsEnabled)
                     .EnableLogForwarding(forwardingEnabled)
                     .EnableDistributedTrace()
-                    .SetLogLevel("finest");
+                    .SetLogLevel("debug");
                 }
             );
 
