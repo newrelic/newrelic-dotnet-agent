8f329f1002b7e0223e0a552eb88a3ba1e3ebd559
Michael Goin <michaelgoin@gmail.com>
2022-05-06 16:45:27 -0700

    Adds requirement clarification to Integration test docs and fixes a link.

--- tests/Agent/IntegrationTests/IntegrationTests/AgentFeatures/Rules.cs
+++ tests/Agent/IntegrationTests/IntegrationTests/AgentFeatures/Rules.cs
@@ -15,7 +15,7 @@ namespace NewRelic.Agent.IntegrationTests.AgentFeatures
     /// Tests around transaction renaming using URL rules and transaction segment terms
     /// NOTE: These tests depend on the application named "RulesWebApi" having some specific rules in place.
     ///		with prefix "WebTransaction/WebAPI" and whitelist terms "Values Sleep UrlRule".
-    ///		Can be found at https://[staging|rpm].newrelic.com/account/{accountId}/applications/{applicationId}/segment_terms
+    ///		Can be found at https://[staging|rpm].newrelic.com/accounts/{accountId}/applications/{applicationId}/segment_terms
     /// </summary>
     [NetFrameworkTest]
     public class Rules : NewRelicIntegrationTest<RulesWebApi>
@@ -34,7 +34,10 @@ namespace NewRelic.Agent.IntegrationTests.AgentFeatures
         }
 
         // NOTE: this test depends on the application named "RulesWebApi" having a url rule that matches "WebTransaction/Action/.*/UrlRule" and replaces with "WebTransaction/WebAPI/*/UrlRule"
-        // Can be found at https://[staging|rpm].newrelicc.com/account/{accountId}/applications/{applicationId}/url_rules
+        // Can be found at https://[staging-]one.newrelic.com/nr1-core/metric-normalization-rules/view-rules/{entityGuid}.
+        // | Order | Match                             | Replacement                      | Actions | Target      | Terminate? | Active |
+        // | ----- | --------------------------------- | -------------------------------- | ------- | ----------- | ---------- | ------ |
+        // | 0     | WebTransaction/WebAPI/.\*/UrlRule | WebTransaction/WebAPI/\*/UrlRule | Replace | RulesWebApi | true       | true   |
         [Fact]
         public void Test()
         {
