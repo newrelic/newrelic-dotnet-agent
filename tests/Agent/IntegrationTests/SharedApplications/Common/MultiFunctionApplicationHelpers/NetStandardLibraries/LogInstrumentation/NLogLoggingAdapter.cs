72149a08c8fdd5b4f41c57dea856107994d41ce3
Jacob Affinito <jaffinito@newrelic.com>
2022-05-12 18:36:43 -0700

    Working HSM tests

---
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/LogInstrumentation/NLogLoggingAdapter.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using NLog;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace MultiFunctionApplicationHelpers.NetStandardLibraries.LogInstrumentation
+{
+    class NLogLoggingAdapter : ILoggingAdapter
+    {
+        private static Logger _log;
+
+        public NLogLoggingAdapter()
+        {
+        }
+
+        public void Debug(string message)
+        {
+            _log.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            _log.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _log.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            _log.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            _log.Fatal(message);
+        }
+
+        public void Configure()
+        {
+            var logconsole = new ConsoleTarget();
+        
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(logconsole, LogLevel.Debug);
+            _log = LogManager.GetLogger("LoggingTest");
+        }
+
+        public void ConfigurePatternLayoutAppenderForDecoration()
+        {
+            // The default layout is plain text and NLog appends NR-LINKING to the message automatically.
+            Configure();
+        }
+
+        public void ConfigureJsonLayoutAppenderForDecoration()
+        {
+            var logconsole = new ConsoleTarget();
+            logconsole.Layout = new JsonLayout();
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(logconsole, LogLevel.Debug);
+            _log = LogManager.GetLogger("LoggingTest");
+        }
+    }
+}
