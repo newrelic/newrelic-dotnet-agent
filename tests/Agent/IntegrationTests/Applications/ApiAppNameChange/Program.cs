88973763daa2713565f7a3582a18612a1e6fd344
Alex Hemsath <ahemsath@newrelic.com>
2022-07-21 16:48:09 -0700

    Fix three test apps that weren't actually waiting on their wait handles

--- tests/Agent/IntegrationTests/Applications/ApiAppNameChange/Program.cs
+++ tests/Agent/IntegrationTests/Applications/ApiAppNameChange/Program.cs
@@ -26,13 +26,17 @@ namespace NewRelic.Agent.IntegrationTests.Applications.ApiAppNameChange
                 return;
 
             // Create handle that RemoteApplication expects
-            new EventWaitHandle(false, EventResetMode.ManualReset, "app_server_wait_for_all_request_done_" + program.Port);
+            var eventWaitHandle =
+                new EventWaitHandle(false, EventResetMode.ManualReset, "app_server_wait_for_all_request_done_" + program.Port);
 
             CreatePidFile();
 
             Api.Agent.NewRelic.SetApplicationName("AgentApi");
             Api.Agent.NewRelic.StartAgent();
             Api.Agent.NewRelic.SetApplicationName("AgentApi2");
+
+            // Wait for the test harness to tell us to shut down
+            eventWaitHandle.WaitOne(TimeSpan.FromMinutes(5));
         }
 
         private static void CreatePidFile()
