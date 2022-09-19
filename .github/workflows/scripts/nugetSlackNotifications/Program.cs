30a72dc001f524fdc8ca85133902ee38c697954b
Jack de la Motte <jack.delamotte13@gmail.com>
2022-09-19 15:05:29 +0000

    Dotty improvements (#1245)
    * Dotty: filter prereleases and add links.
    
    * Change days from 10 to 1.
    
    * Change days again for test run.
    
    * Change days back for prod.

--- .github/workflows/scripts/nugetSlackNotifications/Program.cs
+++ .github/workflows/scripts/nugetSlackNotifications/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 #pragma warning disable CS8618
@@ -11,51 +13,49 @@ namespace nugetSlackNotifications
 {
     public class Program
     {
-        private static readonly HttpClient client = new();
+        // the semver2 registration endpoint returns gzip encoded json
+        private static readonly HttpClient client = new(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
 
         static async Task Main(string[] args)
         {
-            List<Tuple<string, string, string>> newVersions = new();
-
+            List<Tuple<string, string, string, string>> newVersions = new();
             foreach (string package in args)
             {
-                string response = await client.GetStringAsync($"https://api.nuget.org/v3/registration5-semver1/{package}/index.json");
-                response = response.Replace("@", ""); // to be able to deserialize the necessary "@id" parameter successfully
+                string response = await client.GetStringAsync($"https://api.nuget.org/v3/registration5-gz-semver2/{package}/index.json");
 
                 SearchResult? searchResult = JsonSerializer.Deserialize<SearchResult>(response);
                 if (searchResult is null) continue;
+                Item item = searchResult.items[^1]; // get the most recent group
 
-                foreach (Item item in searchResult.items)
+                // need to make another request to get the page with individual release listings
+                Page? page = JsonSerializer.Deserialize<Page>(await client.GetStringAsync(item.id));
+                if (page is null) continue;
+
+                // need to get the most recent and previous catalog entries to display previous and new version
+                Catalogentry latestCatalogEntry;
+                Catalogentry previousCatalogEntry;
+
+                // alternative json structure (see mysql.data)
+                if (page.items[^1].catalogEntry is null)
                 {
-                    // need to get the most recent and previous catalog entries
-                    // to display previous and new version
-                    Catalogentry latestCatalogEntry;
-                    Catalogentry previousCatalogEntry;
-
-                    if (item.items is not null) // standard json format from nuget.org
-                    {
-                        latestCatalogEntry = item.items[^1].catalogEntry; // latest release
-                        previousCatalogEntry = item.items[^2].catalogEntry; // next-latest release
-                    }
-                    else // if item.items is null the json structure is weird and we have to use different properties
-                    {
-                        // need to make another request to get the page with individual release listings
-                        Item? page = JsonSerializer.Deserialize<Item>(await client.GetStringAsync(item.id));
-                        if (page is null) continue;
-
-                        latestCatalogEntry = page.items[^1].catalogEntry; // latest release
-                        previousCatalogEntry = page.items[^2].catalogEntry; // next-latest release
-                    }
-                    if (latestCatalogEntry.published > DateTime.Now.AddDays(-1))
-                        newVersions.Add(new Tuple<string, string, string>(latestCatalogEntry.id, previousCatalogEntry.version, latestCatalogEntry.version));
+                    latestCatalogEntry = page.items[^1].items[^1].catalogEntry; // latest release
+                    previousCatalogEntry = page.items[^1].items[^2].catalogEntry; // next-latest release
                 }
+                else // standard structure
+                {
+                    latestCatalogEntry = page.items[^1].catalogEntry;
+                    previousCatalogEntry = page.items[^2].catalogEntry;
+                }
+
+                if (latestCatalogEntry.published > DateTime.Now.AddDays(-1) && !(await latestCatalogEntry.isPrerelease()))
+                    newVersions.Add(new Tuple<string, string, string, string>(package, previousCatalogEntry.version, latestCatalogEntry.version, $"https://www.nuget.org/packages/{package}/"));
             }
 
             if (newVersions.Count > 0) // only message channel if there's package updates to report
             {
                 string msg = "Hi team! Dotty here :technologist::pager:\nThere's some new NuGet releases you should know about :arrow_heading_down::sparkles:";
                 foreach (var t in newVersions)
-                    msg += $"\n\t:package: {char.ToUpper(t.Item1[0]) + t.Item1[1..]} {t.Item2} :point_right: {t.Item3}";
+                    msg += $"\n\t:package: {char.ToUpper(t.Item1[0]) + t.Item1[1..]} {t.Item2} :point_right: <{t.Item4}|{t.Item3}>";
                 msg += $"\nThanks and have a wonderful {DateTime.Now.DayOfWeek}.";
 
                 StringContent jsonContent = new(
@@ -68,7 +68,6 @@ namespace nugetSlackNotifications
 
                 await client.PostAsync(Environment.GetEnvironmentVariable("webhook"), jsonContent);
             }
-
         }
     }
 
@@ -80,25 +79,46 @@ namespace nugetSlackNotifications
 
     public class Item
     {
+        [JsonPropertyName("@id")]
         public string id { get; set; }
         public DateTime commitTimeStamp { get; set; }
-        public int count { get; set; }
+    }
+
+    public class Page
+    {
         public Release[] items { get; set; }
-        // some packages like MySqlConnector and Serilog return json that will have this and not items
-        public string upper { get; set; }
     }
 
     public class Release
     {
+        [JsonPropertyName("@id")]
         public string id { get; set; }
         public Catalogentry catalogEntry { get; set; }
+
+        // only packages with alternative json structure (i.e. mysql.data) will have this
+        public Release[] items { get; set; }
+    }
+
+    public class Version
+    {
+        public bool isPrerelease { get; set; }
     }
 
     public class Catalogentry
     {
+        [JsonPropertyName("@id")]
         public string id { get; set; }
         public DateTime published { get; set; }
         public string version { get; set; }
+
+        public async Task<bool> isPrerelease()
+        {
+            HttpClient client = new();
+            string response = await client.GetStringAsync(id);
+            Version? version = JsonSerializer.Deserialize<Version>(response);
+
+            return version is null ? false : version.isPrerelease;
+        }
     }
 }
 
