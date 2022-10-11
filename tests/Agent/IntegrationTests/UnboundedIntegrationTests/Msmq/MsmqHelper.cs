d4f85f6cadd4e1d23537b2f873533089f041d4c7
chynesNR <chynes@newrelic.com>
2022-10-11 11:19:33 -0700

    Updating MSMQ tests to use ConsoleMF
    Converting MSMQ tests to ConsoleMF and increasing framework coverage

---
+++ tests/Agent/IntegrationTests/UnboundedIntegrationTests/Msmq/MsmqHelper.cs
@@ -0,0 +1,20 @@
+﻿
+namespace NewRelic.Agent.UnboundedIntegrationTests.Msmq
+{
+    internal static class MsmqHelper
+    {
+        private static int _queueNum = 0;
+        private static object _lock = new object();
+
+        // Because the MSMQ tests modify the queue, we can run into concurrency issues if more than one test
+        // is running at the same time. To prevent this, give each test a unique queue name based on a number
+        internal static int GetNextQueueNum()
+        {
+            lock (_lock)
+            {
+                _queueNum++;
+                return _queueNum;
+            }
+        }
+    }
+}
