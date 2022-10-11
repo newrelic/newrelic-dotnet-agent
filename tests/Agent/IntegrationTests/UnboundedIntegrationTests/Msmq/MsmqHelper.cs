007b5688657e76b9ec9d57ad9b59e5f8034a26a6
chynesNR <chynes@newrelic.com>
2022-10-11 13:57:41 -0700

    Added missing header
    StyleCop fix

--- tests/Agent/IntegrationTests/UnboundedIntegrationTests/Msmq/MsmqHelper.cs
+++ tests/Agent/IntegrationTests/UnboundedIntegrationTests/Msmq/MsmqHelper.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 namespace NewRelic.Agent.UnboundedIntegrationTests.Msmq
 {
     internal static class MsmqHelper
