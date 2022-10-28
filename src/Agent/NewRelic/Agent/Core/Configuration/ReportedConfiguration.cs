1eeaa2e4195c7f758b728d48ce3ee0343fa2cba7
Vu Tran <vuqtran88@gmail.com>
2022-10-28 09:56:34 -0700

    Add app logging custom attributes settings (#1291)
    * Add logging custom attributes settings.
    
    * Deserialize new logging settings.
    
    * Add unit tests
    
    * Report new configs.
    
    * fix failing test.
    
    * Update src/Agent/NewRelic/Agent/Core/Config/Configuration.xsd
    
    Co-authored-by: Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
    
    * Update src/Agent/NewRelic/Agent/Core/Config/Configuration.xsd
    
    Co-authored-by: Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
    
    * Fix another failing test.
    
    * Update src/Agent/NewRelic/Agent/Core/Config/Configuration.xsd
    
    Co-authored-by: Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
    
    * Update src/Agent/NewRelic/Agent/Core/Config/Configuration.xsd
    
    Co-authored-by: Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
    
    Co-authored-by: Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>

--- src/Agent/NewRelic/Agent/Core/Configuration/ReportedConfiguration.cs
+++ src/Agent/NewRelic/Agent/Core/Configuration/ReportedConfiguration.cs
@@ -603,6 +603,16 @@ namespace NewRelic.Agent.Core.Configuration
 
         [JsonProperty("agent.app_settings")]
         public IReadOnlyDictionary<string,string> AppSettings => GetAppSettings();
+
+        [JsonProperty("application_logging.forwarding.context_data.enabled")]
+        public bool ContextDataEnabled => _configuration.ContextDataEnabled;
+
+        [JsonProperty("application_logging.forwarding.context_data.include")]
+        public IEnumerable<string> ContextDataInclude => _configuration.ContextDataInclude;
+
+        [JsonProperty("application_logging.forwarding.context_data.exclude")]
+        public IEnumerable<string> ContextDataExclude => _configuration.ContextDataExclude;
+
         public IReadOnlyDictionary<string, string> GetAppSettings()
         {
             return _configuration.GetAppSettings();
