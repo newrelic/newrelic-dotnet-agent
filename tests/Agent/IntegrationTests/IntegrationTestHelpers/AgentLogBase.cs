3b980213b0b722cdb8174085ba9c2cbb34f08451
Josh Coleman <jcoleman@newrelic.com>
2023-02-07 11:42:40 -0800

    update expected log line after merging `feature/grpc-hybrid-solution`

--- tests/Agent/IntegrationTests/IntegrationTestHelpers/AgentLogBase.cs
+++ tests/Agent/IntegrationTests/IntegrationTestHelpers/AgentLogBase.cs
@@ -63,7 +63,7 @@ namespace NewRelic.Agent.IntegrationTestHelpers
 
         // Infinite trace
         public const string SpanStreamingSuccessfullySentLogLineRegex = FinestLogLinePrefixRegex + @"SpanStreamingService: consumer \d+ - Attempting to send (\d+) item\(s\) - Success";
-        public const string SpanStreamingSuccessfullyProcessedByServerResponseLogLineRegex = FinestLogLinePrefixRegex + @"SpanStreamingService: consumer \d+ - Received gRPC Server response: (\d+)";
+        public const string SpanStreamingSuccessfullyProcessedByServerResponseLogLineRegex = FinestLogLinePrefixRegex + @"SpanStreamingService: consumer \d+ - Received gRPC Server response messages: (\d+)";
         public const string SpanStreamingResponseGrpcError = FinestLogLinePrefixRegex + @"ResponseStreamWrapper: consumer \d+ - GRPC RpcException encountered while handling gRPC server responses: (.*)";
         
         public abstract IEnumerable<string> GetFileLines();
