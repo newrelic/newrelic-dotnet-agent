435b4224c881a00255748b99f5f27798ea80912a
Jacob Affinito <jaffinito@newrelic.com>
2023-03-09 15:13:00 -0800

    Add unit tests for ErrorGroupCallback in ReportedConfiguration

--- tests/Agent/UnitTests/Core.UnitTest/DataTransport/ExhaustiveTestConfiguration.cs
+++ tests/Agent/UnitTests/Core.UnitTest/DataTransport/ExhaustiveTestConfiguration.cs
@@ -204,7 +204,7 @@ namespace NewRelic.Agent.Core.DataTransport
             { "eight", new[] { "eight1", "eight2" } },
         };
 
-        public Func<IReadOnlyDictionary<string, object>, string> ErrorGroupCallback => ex => "my error group";
+        public Func<IReadOnlyDictionary<string, object>, string> ErrorGroupCallback => dict => "my error group";
 
         public Dictionary<string, string> RequestHeadersMap => new Dictionary<string, string>
         {
