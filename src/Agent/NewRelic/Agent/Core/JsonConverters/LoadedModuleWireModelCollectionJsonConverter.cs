8d1f4ef42986e52939680895a3ba90862c40bdfb
Jacob Affinito <jaffinito@newrelic.com>
2023-03-22 15:33:09 -0700

    Working PoC for Vuln Mgmt using update_loaded_modules

---
+++ src/Agent/NewRelic/Agent/Core/JsonConverters/LoadedModuleWireModelCollectionJsonConverter.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using NewRelic.Agent.Core.WireModels;
+using Newtonsoft.Json;
+
+namespace NewRelic.Agent.Core.JsonConverters
+{
+    public class LoadedModuleWireModelCollectionJsonConverter : JsonConverter<LoadedModuleWireModelCollection>
+    {
+        private const string JarsName = "Jars";
+
+        public override LoadedModuleWireModelCollection ReadJson(JsonReader reader, Type objectType, LoadedModuleWireModelCollection existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter jsonWriter, LoadedModuleWireModelCollection value, JsonSerializer serializer)
+        {
+            WriteJsonImpl(jsonWriter, value);
+        }
+
+        private static void WriteJsonImpl(JsonWriter jsonWriter, LoadedModuleWireModelCollection value)
+        {
+            jsonWriter.WriteValue(JarsName);
+
+            jsonWriter.WriteStartArray();
+
+            foreach (var loadedModule in value.LoadedModules)
+            {
+                // MODULE
+                jsonWriter.WriteStartArray();
+
+                jsonWriter.WriteValue(loadedModule.Value.AssemblyName);
+                jsonWriter.WriteValue(loadedModule.Value.Version ?? " ");
+
+                // DATA DICTIONARY
+                jsonWriter.WriteStartObject();
+                foreach (var item in loadedModule.Value.Data)
+                {
+                    jsonWriter.WritePropertyName(item.Key);
+                    jsonWriter.WriteValue(item.Value.ToString());
+                }
+
+                jsonWriter.WriteEndObject();
+
+                jsonWriter.WriteEndArray();
+            }
+
+            jsonWriter.WriteEndArray();
+        }
+    }
+}
---
+++ src/Agent/NewRelic/Agent/Core/WireModels/LoadedModuleWireModelCollection.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Collections.Generic;
+using NewRelic.Agent.Core.JsonConverters;
+using Newtonsoft.Json;
+
+namespace NewRelic.Agent.Core.WireModels
+{
+    [JsonConverter(typeof(LoadedModuleWireModelCollectionJsonConverter))]
+    public class LoadedModuleWireModelCollection
+    {
+        public Dictionary<string, LoadedModuleWireModel> LoadedModules { get; }
+
+        public LoadedModuleWireModelCollection()
+        {
+            LoadedModules = new Dictionary<string, LoadedModuleWireModel>();
+        }
+    }
+}
