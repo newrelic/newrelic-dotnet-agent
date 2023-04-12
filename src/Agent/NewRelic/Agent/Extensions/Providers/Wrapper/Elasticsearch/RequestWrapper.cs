573b0edd390b6f9864e6e39b5059204e29cef7a5
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2023-04-12 10:42:21 -0700

    Instrument async Elasticsearch methods (#1535)
    * Working async instrumentation
    
    * Get NEST async working, fix integration test

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/Elasticsearch/RequestWrapper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/Elasticsearch/RequestWrapper.cs
@@ -2,10 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.Threading.Tasks;
 using NewRelic.Agent.Api;
 using NewRelic.Agent.Extensions.Parsing;
 using NewRelic.Agent.Extensions.Providers.Wrapper;
 using NewRelic.Reflection;
+using System.Collections.Concurrent;
 
 namespace NewRelic.Providers.Wrapper.Elasticsearch
 {
@@ -16,8 +18,9 @@ namespace NewRelic.Providers.Wrapper.Elasticsearch
 
         private const string WrapperName = "RequestWrapper";
 
-        private Func<object, object> _apiCallDetailsGetter;
-        private Func<object, Uri> _uriGetter;
+        private static Func<object, object> _apiCallDetailsGetter;
+        private static Func<object, Uri> _uriGetter;
+        private static ConcurrentDictionary<Type, Func<object, object>> _getRequestResponseFromGeneric = new ConcurrentDictionary<Type, Func<object, object>>();
 
         public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
         {
@@ -27,10 +30,22 @@ namespace NewRelic.Providers.Wrapper.Elasticsearch
 
         public AfterWrappedMethodDelegate BeforeWrappedMethod(InstrumentedMethodCall instrumentedMethodCall, IAgent agent, ITransaction transaction)
         {
-            //Elasticsearch.Net.HttpMethod,System.String,Elasticsearch.Net.PostData,Elasticsearch.Net.IRequestParameters
+            var indexOfRequestParams = 3; // unless it's Elasticsearch.Net/NEST and async
+
+            if (instrumentedMethodCall.IsAsync) 
+            {
+                transaction.AttachToAsync();
+
+                var parameterTypeNamesList = instrumentedMethodCall.InstrumentedMethodInfo.Method.ParameterTypeNames.Split(',');
+                if (parameterTypeNamesList[4] == "Elasticsearch.Net.IRequestParameters")
+                {
+                    indexOfRequestParams = 4;
+                }
+            }
+
             var path = (string)instrumentedMethodCall.MethodCall.MethodArguments[1];
             var postData = instrumentedMethodCall.MethodCall.MethodArguments[2];
-            var requestParams = instrumentedMethodCall.MethodCall.MethodArguments[3];
+            var requestParams = instrumentedMethodCall.MethodCall.MethodArguments[indexOfRequestParams];
 
             //  For reference here's Elastic's take on mapping SQL terms/concepts to Elastic's: https://www.elastic.co/guide/en/elasticsearch/reference/current/_mapping_concepts_across_sql_and_elasticsearch.html
             var databaseName = string.Empty; // Per Elastic.co this SQL DB concept most closely maps to "cluster instance name".  TBD how to get this
@@ -55,25 +70,71 @@ namespace NewRelic.Providers.Wrapper.Elasticsearch
                 connectionInfo: endpoint != null ? new ConnectionInfo(endpoint.Host, endpoint.Port.ToString(), databaseName) : new ConnectionInfo(string.Empty, string.Empty, databaseName),
                 isLeaf: true);
 
-            return Delegates.GetDelegateFor<object>(
-                    onSuccess: response =>
+            if (instrumentedMethodCall.IsAsync)
+            {
+                return Delegates.GetAsyncDelegateFor<Task>(agent, segment, true, InvokeTryProcessResponse, TaskContinuationOptions.ExecuteSynchronously);
+
+                void InvokeTryProcessResponse(Task responseTask)
+                {
+                    if (!ValidTaskResponse(responseTask) || (segment == null))
                     {
-                        var ApiCallDetailsGetter = _apiCallDetailsGetter ??= GetApiCallDetailsGetterFromResponse(response);
-                        var apiCallDetails = ApiCallDetailsGetter.Invoke(response);
+                        return;
+                    }
+                    var responseGetter = _getRequestResponseFromGeneric.GetOrAdd(responseTask.GetType(), t => VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(t, "Result"));
+                    var response = responseGetter(responseTask);
+                    TryProcessResponse(agent, response, transaction, segment);
+                }
+            }
+            else
+            {
+                return Delegates.GetDelegateFor<object>(
+                        onSuccess: response =>
+                        {
+                            var uri = GetUriFromResponse(response);
+                            // TODO: need to figure out how to plumb things so that we can set the uri on the segment after it has already been created.
+                            // See comments above about extending the experimental API
 
-                        var UriGetter = _uriGetter ??= GetUriGetterFromResponse(response);
-                        var uri = UriGetter.Invoke(apiCallDetails);
+                            segment.End();
+                        },
+                        onFailure: exception =>
+                        {
+                            // Don't know how valid this is
+                            segment.End(exception);
+                        });
+            }
+        }
 
-                        // TODO: need to figure out how to plumb things so that we can set the uri on the segment after it has already been created.
-                        // See comments above about extending the experimental API
+        private static void TryProcessResponse(IAgent agent, object response, ITransaction transaction, ISegment segment)
+        {
+            try
+            {
+                if (response == null || segment == null)
+                {
+                    return;
+                }
 
-                        segment.End();
-                    },
-                    onFailure: exception =>
-                    {
-                        // Don't know how valid this is
-                        segment.End(exception);
-                    });
+                var uri = GetUriFromResponse(response);
+
+                // TODO: need to figure out how to plumb things so that we can set the uri on the segment after it has already been created.
+                // See comments above about extending the experimental API
+
+                segment.End();
+            }
+            catch (Exception ex)
+            {
+                agent.HandleWrapperException(ex);
+            }
+        }
+
+        private static Uri GetUriFromResponse(object response)
+        {
+            var ApiCallDetailsGetter = _apiCallDetailsGetter ??= GetApiCallDetailsGetterFromResponse(response);
+            var apiCallDetails = ApiCallDetailsGetter.Invoke(response);
+
+            var UriGetter = _uriGetter ??= GetUriGetterFromResponse(response);
+            var uri = UriGetter.Invoke(apiCallDetails);
+
+            return uri;
         }
 
         private string GetOperationFromRequestParams(object requestParams)
@@ -89,7 +150,7 @@ namespace NewRelic.Providers.Wrapper.Elasticsearch
             return requestParamsTypeName.Remove(requestParamsTypeName.Length - "RequestParameters".Length);
         }
 
-        private Func<object, object> GetApiCallDetailsGetterFromResponse(object response)
+        private static Func<object, object> GetApiCallDetailsGetterFromResponse(object response)
         {
             var typeOfResponse = response.GetType();
             var responseAssemblyName = typeOfResponse.Assembly.FullName;
@@ -98,13 +159,18 @@ namespace NewRelic.Providers.Wrapper.Elasticsearch
             return VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(responseAssemblyName, typeOfResponse.FullName, apiCallDetailsPropertyName);
         }
 
-        private Func<object, Uri> GetUriGetterFromResponse(object response)
+        private static Func<object, Uri> GetUriGetterFromResponse(object response)
         {
             var responseAssemblyName = response.GetType().Assembly.FullName;
             var apiCallDetailsAssemblyName = responseAssemblyName.StartsWith("Elastic.Clients.Elasticsearch") ? "Elastic.Transport" : "Elasticsearch.Net";
             var apiCallDetailsType = $"{apiCallDetailsAssemblyName}.ApiCallDetails";
 
             return VisibilityBypasser.Instance.GeneratePropertyAccessor<Uri>(apiCallDetailsAssemblyName, apiCallDetailsType, "Uri");
+        }
+
+        private static bool ValidTaskResponse(Task response)
+        {
+            return response?.Status == TaskStatus.RanToCompletion;
         }
 
     }
