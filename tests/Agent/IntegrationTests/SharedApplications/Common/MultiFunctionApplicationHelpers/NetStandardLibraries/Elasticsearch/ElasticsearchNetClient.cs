40f0ecad2badfbc6196b37380f7e6690dc6f0f0d
chynesNR <chynes@newrelic.com>
2023-04-14 10:56:51 -0700

    Fixed accidental commit

--- tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Elasticsearch/ElasticsearchNetClient.cs
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Elasticsearch/ElasticsearchNetClient.cs
@@ -91,7 +91,6 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Elasticsearch
             }));
             // TODO: Gotta parse the JSON :(
             var json = response.Body;
-            IndexRequestParameters
             return 0;
         }
 
