c20df2c7b279117bdf06d9cd1557f6321f3b431f
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-04-19 10:56:21 -0500

    feat: Move TLS config logging closer to connect.

--- src/Agent/NewRelic/Agent/Core/NewRelic.Agent.Core/AgentManager.cs
+++ src/Agent/NewRelic/Agent/Core/NewRelic.Agent.Core/AgentManager.cs
@@ -184,12 +184,6 @@ namespace NewRelic.Agent.Core
 
             StartServices();
             LogInitialized();
-            LogTlsConfiguration();
-        }
-
-        private void LogTlsConfiguration()
-        {
-            Log.Info($"TLS Configuration (System.Net.ServicePointManager.SecurityProtocol): {System.Net.ServicePointManager.SecurityProtocol}");
         }
 
         private void LogInitialized()
