ab3606d8673e4467cebf0e5fa2d2c0dbaebc9960
Alex Hemsath <ahemsath@newrelic.com>
2023-04-28 14:23:34 -0700

    Remove dependency on SevenZipExtractor
    Only working for the tar.gz archive for now

--- build/ArtifactBuilder/Artifacts/LinuxPackage.cs
+++ build/ArtifactBuilder/Artifacts/LinuxPackage.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Formats;
+using System.Formats.Tar;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using SevenZipExtractor;
 
 namespace ArtifactBuilder.Artifacts
 {
     public class LinuxPackage : Artifact
     {
+        private const string GzipFileExtension = ".gz";
+
         private readonly string _packagePlatform;
         private readonly string _fileExtension;
         private readonly string _buildOutputDirectory;
@@ -22,9 +25,9 @@ namespace ArtifactBuilder.Artifacts
             _packageName = name;
             _packagePlatform = packagePlatform;
             _fileExtension = fileExtension;
-            _buildOutputDirectory = $@"{RepoRootDirectory}\src\_build\CoreArtifacts";
+            _buildOutputDirectory = Path.Join(RepoRootDirectory, "src", "_build", "CoreArtifacts");
             _packagePath = GetPackagePath();
-            OutputDirectory = $@"{RepoRootDirectory}\build\BuildArtifacts\{Name}";
+            OutputDirectory = Path.Join(RepoRootDirectory, "build", "BuildArtifacts", Name);
             ValidateContentAction = ValidateContent;
 
             _coreAgentComponents = AgentComponents.GetAgentComponents(AgentType.Core, "Release", "x64", RepoRootDirectory, HomeRootDirectory);
@@ -36,11 +39,11 @@ namespace ArtifactBuilder.Artifacts
             Directory.CreateDirectory(OutputDirectory);
 
             var fileInfo = new FileInfo(_packagePath);
-            File.Copy(fileInfo.FullName, $@"{OutputDirectory}\{fileInfo.Name}", true);
+            File.Copy(fileInfo.FullName, Path.Join(OutputDirectory, fileInfo.Name), true);
 
             //Generate the checksum file based on the Linux standards for the checksum contents: <checksum_value> <checksum_mode><filename>
             //<checksum_mode> is either a ' ' for text mode or '*' for binary mode.
-            File.WriteAllText($@"{OutputDirectory}\{fileInfo.Name}.sha256", $"{FileHelpers.GetSha256Checksum(_packagePath)} *{fileInfo.Name}");
+            File.WriteAllText(Path.Join(OutputDirectory, $"{fileInfo.Name}.sha256"), $"{FileHelpers.GetSha256Checksum(_packagePath)} *{fileInfo.Name}");
         }
 
         private string GetPackagePath()
@@ -68,59 +71,62 @@ namespace ArtifactBuilder.Artifacts
             switch (_fileExtension)
             {
                 case "deb":
-                    using (ArchiveFile archiveFile = new ArchiveFile(_packagePath))
-                    {
-                        archiveFile.Extract(unpackedDir);
-                    }
+                    //using (ArchiveFile archiveFile = new ArchiveFile(_packagePath))
+                    //{
+                    //    archiveFile.Extract(unpackedDir);
+                    //}
 
-                    using (ArchiveFile archiveFileXz = new ArchiveFile(Path.Join(unpackedDir, "data.tar.xz")))
-                    {
-                        MemoryStream memoryStream = new MemoryStream();
-                        archiveFileXz.Entries[0].Extract(memoryStream);
+                    //using (ArchiveFile archiveFileXz = new ArchiveFile(Path.Join(unpackedDir, "data.tar.xz")))
+                    //{
+                    //    MemoryStream memoryStream = new MemoryStream();
+                    //    archiveFileXz.Entries[0].Extract(memoryStream);
 
-                        using (ArchiveFile archiveFileTar = new ArchiveFile(memoryStream, SevenZipFormat.Tar))
-                        {
-                            archiveFileTar.Extract(unpackedDir);
-                        }
-                    }
+                    //    using (ArchiveFile archiveFileTar = new ArchiveFile(memoryStream, SevenZipFormat.Tar))
+                    //    {
+                    //        archiveFileTar.Extract(unpackedDir);
+                    //    }
+                    //}
                     return Path.Combine(unpackedDir, "usr", "local", "newrelic-dotnet-agent");
                 case "rpm":
-                    using (ArchiveFile archiveFile = new ArchiveFile(_packagePath))
-                    {
-                        archiveFile.Extract(unpackedDir);
-                    }
-                    var cpioFile = packageFileName.Replace(".rpm", "") + ".cpio.xz";
-                    using (ArchiveFile archiveFileXz = new ArchiveFile(Path.Join(unpackedDir, cpioFile)))
-                    {
-                        MemoryStream memoryStream = new MemoryStream();
-                        archiveFileXz.Entries[0].Extract(memoryStream);
+                    //using (ArchiveFile archiveFile = new ArchiveFile(_packagePath))
+                    //{
+                    //    archiveFile.Extract(unpackedDir);
+                    //}
+                    //var cpioFile = packageFileName.Replace(".rpm", "") + ".cpio.xz";
+                    //using (ArchiveFile archiveFileXz = new ArchiveFile(Path.Join(unpackedDir, cpioFile)))
+                    //{
+                    //    MemoryStream memoryStream = new MemoryStream();
+                    //    archiveFileXz.Entries[0].Extract(memoryStream);
 
-                        using (ArchiveFile archiveFileCpio = new ArchiveFile(memoryStream, SevenZipFormat.Cpio))
-                        {
-                            archiveFileCpio.Extract(unpackedDir);
-                        }
-                    }
+                    //    using (ArchiveFile archiveFileCpio = new ArchiveFile(memoryStream, SevenZipFormat.Cpio))
+                    //    {
+                    //        archiveFileCpio.Extract(unpackedDir);
+                    //    }
+                    //}
                     return Path.Combine(unpackedDir, "usr", "local", "newrelic-dotnet-agent");
                 case "tar.gz":
-                    // The SevenZipWrapper is choking on our tar.gz file for some reason
                     Directory.CreateDirectory(unpackedDir);
-                    UnGzip(_packagePath, unpackedDir);
-                    using (ArchiveFile archiveFileTar = new ArchiveFile(Path.Join(unpackedDir, packageFileName.Replace(".gz", ""))))
-                    {
-                        archiveFileTar.Extract(unpackedDir);
-                    }
+                    var tar = UnGzip(_packagePath, unpackedDir);
+                    TarFile.ExtractToDirectory(tar, unpackedDir, true);
                     return Path.Combine(unpackedDir, "newrelic-dotnet-agent");
                 default:
                     throw new PackagingException($"Unknown extension {_fileExtension} for Linux package {_packageName}");
             }
         }
 
-        private void UnGzip(string gzipFile, string path)
+        private string UnGzip(string gzipFile, string outputPath)
         {
+            if (!gzipFile.EndsWith(GzipFileExtension))
+            {
+                throw new ArgumentException($"Input filename {gzipFile} does not end in '{GzipFileExtension}'");
+            }
+            var outputFile = Path.Join(outputPath, new FileInfo(gzipFile).Name.Replace(GzipFileExtension, ""));
+
             using FileStream compressedFileStream = File.Open(gzipFile, FileMode.Open);
-            using FileStream outputFileStream = File.Create(Path.Join(path, new FileInfo(gzipFile).Name.Replace(".gz", "")));
+            using FileStream outputFileStream = File.Create(outputFile);
             using var decompressor = new GZipStream(compressedFileStream, CompressionMode.Decompress);
             decompressor.CopyTo(outputFileStream);
+            return outputFile;
         }
 
         private void ValidateContent()
