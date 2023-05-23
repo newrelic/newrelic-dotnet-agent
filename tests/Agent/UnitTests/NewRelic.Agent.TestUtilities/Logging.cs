d7764e941fc7c247b29a574cbe590580eb17e8fb
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-05-23 15:24:38 -0500

    More cleanup / refactoring

--- tests/Agent/UnitTests/NewRelic.Agent.TestUtilities/Logging.cs
+++ tests/Agent/UnitTests/NewRelic.Agent.TestUtilities/Logging.cs
@@ -20,6 +20,7 @@ namespace NewRelic.Agent.TestUtilities
     public class Logging : IDisposable
     {
         private readonly InMemorySink _inMemorySink = new InMemorySink();
+
         /// <summary>
         /// Initializes serilog to log to an in-memory sink which can then be queried
         /// </summary>
@@ -34,9 +35,6 @@ namespace NewRelic.Agent.TestUtilities
             Log.Initialize(new Logger());
         }
 
-        /// <summary>
-        /// When you dispose of this object the memory appender will be removed from the logging system.
-        /// </summary>
         public void Dispose()
         {
         }
@@ -50,9 +48,6 @@ namespace NewRelic.Agent.TestUtilities
 
             foreach (var logEvent in logEvents)
             {
-                if (logEvent == null)
-                    continue;
-
                 builder.AppendLine(logEvent.RenderMessage());
             }
 
@@ -64,27 +59,14 @@ namespace NewRelic.Agent.TestUtilities
         /// </summary>
         /// <param name="message">The message you want to check for.</param>
         /// <returns>True if the message was logged, false otherwise.</returns>
-        public bool HasMessage(string message)
-        {
-            var events = _inMemorySink.LogEvents;
-            foreach (var item in events)
-            {
-                if (item.RenderMessage() == message)
-                    return true;
-            }
-            return false;
-        }
+        public bool HasMessage(string message) => _inMemorySink.LogEvents.Any(e => e.RenderMessage() == message);
 
         /// <summary>
         /// checks for messages that begins with a segment
         /// </summary>
         /// <param name="segment"></param>
         /// <returns></returns>
-        public bool HasMessageBeginingWith(string segment)
-        {
-            var events = _inMemorySink.LogEvents;
-            return events.Any(item => item.RenderMessage().StartsWith(segment));
-        }
+        public bool HasMessageBeginingWith(string segment) => _inMemorySink.LogEvents.Any(item => item.RenderMessage().StartsWith(segment));
 
         /// <summary>
         /// checks for messages that begins with a segment
@@ -96,7 +78,7 @@ namespace NewRelic.Agent.TestUtilities
         /// <summary>
         /// Counts the number of messages that were logged since the construction of this object.
         /// </summary>
-        public int MessageCount { get { return _inMemorySink.LogEvents.Count(); } }
+        public int MessageCount => _inMemorySink.LogEvents.Count();
 
         /// <summary>
         /// Counts the number of [level] messages that were logged since the construction of this object.
@@ -104,51 +86,39 @@ namespace NewRelic.Agent.TestUtilities
         /// <returns>The number of messages logged at [level] level.</returns>
         private int LevelCount(LogEventLevel level) => _inMemorySink.LogEvents.Count(e => e.Level == level);
 
-        public IEnumerable<string> ErrorMessages
-        {
-            get
-            {
-                return _inMemorySink.LogEvents
-                    .Where(@event => @event.Level == LogEventLevel.Error)
-                    .Select(@event => @event.RenderMessage());
-            }
-        }
+        public IEnumerable<string> ErrorMessages =>
+            _inMemorySink.LogEvents
+                .Where(@event => @event.Level == LogEventLevel.Error)
+                .Select(@event => @event.RenderMessage());
 
         /// <summary>
         /// Counts the number of error level messages that were logged since construction of this object.
         /// </summary>
         /// <returns></returns>
-        public int ErrorCount { get { return LevelCount(LogEventLevel.Error); } }
+        public int ErrorCount => LevelCount(LogEventLevel.Error);
 
         /// <summary>
         /// Counts the number of warn level messages that were logged since construction of this object.
         /// </summary>
         /// <returns></returns>
-        public int WarnCount { get { return LevelCount(LogEventLevel.Warning); } }
+        public int WarnCount => LevelCount(LogEventLevel.Warning);
 
         /// <summary>
         /// Counts the number of info level messages that were logged since construction of this object.
         /// </summary>
         /// <returns></returns>
-        public int InfoCount { get { return LevelCount(LogEventLevel.Information); } }
+        public int InfoCount => LevelCount(LogEventLevel.Information);
 
         /// <summary>
         /// Counts the number of debug level messages that were logged since construction of this object.
         /// </summary>
         /// <returns></returns>
-        public int DebugCount { get { return LevelCount(LogEventLevel.Debug); } }
+        public int DebugCount => LevelCount(LogEventLevel.Debug);
 
         /// <summary>
         /// Counts the number of finest level messages that were logged since construction of this object.
         /// </summary>
         /// <returns></returns>
-        public int FinestCount { get { return LevelCount(LogEventLevel.Verbose); } }
-
-        public bool HasProperty(string propertyId)
-        {
-            return _inMemorySink.LogEvents.Any(e => e.Properties.ContainsKey(propertyId));
-        }
-
-        public string GetProperty(string propertyId) => _inMemorySink.LogEvents.FirstOrDefault(e => e.Properties[propertyId] != null)?.Properties[propertyId].ToString();
+        public int FinestCount => LevelCount(LogEventLevel.Verbose);
     }
 }
