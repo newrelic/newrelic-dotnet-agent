98dfc0dd985912dc7e7e5e3d0907ff3b418aa502
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-05-25 11:02:30 -0500

    log4net to Serilog: Unit tests plus code cleanup for serilog implementation (#1663)

---
+++ tests/Agent/UnitTests/Core.UnitTest/Logging/InMemorySinkTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using NUnit.Framework;
+using Serilog.Events;
+using System;
+using System.Linq;
+using Serilog.Parsing;
+
+namespace NewRelic.Agent.Core.Logging.Tests
+{
+    [TestFixture]
+    public class InMemorySinkTests
+    {
+        private InMemorySink _sink;
+        private LogEvent _logEvent;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sink = new InMemorySink();
+            _logEvent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null, new MessageTemplate("Test", Enumerable.Empty<MessageTemplateToken>()), Enumerable.Empty<LogEventProperty>());
+        }
+
+        [Test]
+        public void Emit_Enqueues_LogEvent()
+        {
+            _sink.Emit(_logEvent);
+            Assert.AreEqual(1, _sink.LogEvents.Count());
+        }
+
+        [Test]
+        public void LogEvents_ReturnsCorrectEvents()
+        {
+            _sink.Emit(_logEvent);
+            var logEvents = _sink.LogEvents;
+
+            Assert.AreEqual(1, logEvents.Count());
+            Assert.AreEqual(_logEvent, logEvents.First());
+        }
+
+        [Test]
+        public void Clear_LogEvents_EmptiesQueue()
+        {
+            _sink.Emit(_logEvent);
+            _sink.Clear();
+            Assert.AreEqual(0, _sink.LogEvents.Count());
+        }
+
+        [Test]
+        public void Dispose_ClearsLogEvents()
+        {
+            _sink.Emit(_logEvent);
+            _sink.Dispose();
+            Assert.AreEqual(0, _sink.LogEvents.Count());
+        }
+    }
+}
