3520f7f5744556f9d17000ef0a10483456170bd1
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-06-07 13:40:20 -0500

    ci: Artifactbuilder parses output from nuget pack (#1699)
    * Modifies Nuget build / validation to capture and parse stdout from the `nuget pack` command, to obtain the actual filename of the NuGet package that was generated and use that in content validation.
    
    * Use regex to parse output from nuget pack

--- build/ArtifactBuilder/NuGetHelpers.cs
+++ build/ArtifactBuilder/NuGetHelpers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ArtifactBuilder
 {
@@ -8,10 +9,20 @@ namespace ArtifactBuilder
     {
         private static readonly string _nugetPath = Path.Combine(FileHelpers.GetRepoRootDirectory(), @"Build\Tools\nuget.exe");
 
-        public static void Pack(string nuspecFilePath, string outputDirectory)
+        public static string Pack(string nuspecFilePath, string outputDirectory)
         {
             var parameters = $@"Pack -NoPackageAnalysis ""{nuspecFilePath}"" -OutputDirectory ""{outputDirectory}""";
-            var process = Process.Start(_nugetPath, parameters);
+            var process = new Process();
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = _nugetPath,
+                Arguments = parameters,
+                RedirectStandardOutput = true
+            };
+            process.StartInfo = startInfo;
+
+            process.Start();
             process.WaitForExit(30000);
             if (!process.HasExited)
             {
@@ -22,6 +33,30 @@ namespace ArtifactBuilder
             {
                 throw new Exception($"Nuget pack failed with exit code {process.ExitCode}.");
             }
+
+            var stdOut = process.StandardOutput;
+            var output = stdOut.ReadToEnd();
+            Console.WriteLine(output);
+
+            // output is expected to look like:
+            // Attempting to build package from 'NewRelic.Azure.WebSites.x64.nuspec'.
+            // Successfully created package 'C:\Source\Repos\newrelic-dotnet-agent\Build\BuildArtifacts\NugetAzureWebSites-x64\NewRelic.Azure.WebSites.x64.10.11.0.nupkg'.
+
+            // capture the full path
+            var regex = ".*Successfully created package '(.*)'.*";
+            var matches = Regex.Match(output, regex);
+            if (matches.Success && matches.Groups.Count > 1)
+            {
+                var packagePath = matches.Groups[1].Value;
+
+                // verify the file exists, then return the filename
+                if (File.Exists(packagePath))
+                {
+                    return Path.GetFileName(packagePath);
+                }
+            }
+
+            throw new PackagingException("Failed to parse NuGet package filename.");
         }
 
         public static void Unpack(string nupkgFile, string outputDirectory)
