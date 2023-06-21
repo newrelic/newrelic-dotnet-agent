0803b595d64e4b31b0f7c37830aaafa361b8fc09
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-06-21 15:04:35 -0500

    fix: Format and log audit-level messages only when audit logging is enabled.

--- src/Agent/NewRelic/Agent/Core/Logging/AuditLog.cs
+++ src/Agent/NewRelic/Agent/Core/Logging/AuditLog.cs
@@ -13,21 +13,23 @@ namespace NewRelic.Agent.Core.Logging
         private static Lazy<ILogger> _lazyAuditLogger = new Lazy<ILogger>(() =>
             Serilog.Log.Logger.ForContext(LogLevelExtensions.AuditLevel, LogLevelExtensions.AuditLevel));
 
+        public static bool IsAuditLogEnabled { get; private set; }
+
         /// <summary>
         /// Logs <paramref name="message"/> at the AUDIT level. This log level should be used only as dictated by the security team to satisfy auditing requirements.
         /// </summary>
         public static void Log(string message)
         {
+            if (IsAuditLogEnabled)
                 // use Fatal log level to ensure audit log messages never get filtered due to level restrictions
                 _lazyAuditLogger.Value.Fatal(message);
         }
 
         internal static LoggerConfiguration IncludeOnlyAuditLog(this LoggerConfiguration loggerConfiguration)
         {
-            return loggerConfiguration.Filter.ByIncludingOnly($"{LogLevelExtensions.AuditLevel} is not null");
+            IsAuditLogEnabled = true; // set a flag so we can short-circuit when audit log is not enabled
 
-            //return loggerConfiguration.Filter.ByIncludingOnly(logEvent =>
-            //    logEvent.Properties.ContainsKey(LogLevelExtensions.AuditLevel));
+            return loggerConfiguration.Filter.ByIncludingOnly($"{LogLevelExtensions.AuditLevel} is not null");
         }
         internal static LoggerConfiguration ExcludeAuditLog(this LoggerConfiguration loggerConfiguration)
         {
