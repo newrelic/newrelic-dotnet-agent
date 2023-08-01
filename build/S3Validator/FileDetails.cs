c9923509e96789162dfa9ad4c6fc89393c27ad56
Jacob Affinito <jaffinito@newrelic.com>
2023-07-31 18:29:16 -0700

    Add S3 Validation tool.

---
+++ build/S3Validator/FileDetails.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace S3Validator
+{
+    public struct FileDetails
+    {
+        [YamlDotNet.Serialization.YamlMember(Alias = "name")]
+        public string Name { get; set; }
+
+        [YamlDotNet.Serialization.YamlMember(Alias = "size")]
+        public long Size { get; set; }
+    }
+}
