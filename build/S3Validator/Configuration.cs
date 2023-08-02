c5248e4fc1d707a58aec1d0f3d2ea5567f6b129d
Jacob Affinito <jaffinito@newrelic.com>
2023-08-02 08:41:18 -0700

    chore: Add S3 Validation tool. (#1811)

---
+++ build/S3Validator/Configuration.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace S3Validator
+{
+    public class Configuration
+    {
+        [YamlDotNet.Serialization.YamlMember(Alias = "base-url")]
+        public string? BaseUrl { get; set; }
+
+        [YamlDotNet.Serialization.YamlMember(Alias = "directory-list")]
+        public List<string>? DirectoryList { get; set; }
+
+        [YamlDotNet.Serialization.YamlMember(Alias = "file-list")]
+        public List<FileDetails>? FileList { get; set; }
+    }
+}
