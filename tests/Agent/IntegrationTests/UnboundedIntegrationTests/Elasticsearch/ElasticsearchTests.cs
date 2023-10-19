d289edffb9669dc28d2c6e4a807d9028711c95c5
Alex Hemsath <ahemsath@newrelic.com>
2023-10-19 08:45:10 -0700

    Sort of working

--- tests/Agent/IntegrationTests/UnboundedIntegrationTests/Elasticsearch/ElasticsearchTests.cs
+++ tests/Agent/IntegrationTests/UnboundedIntegrationTests/Elasticsearch/ElasticsearchTests.cs
@@ -69,6 +69,7 @@ namespace NewRelic.Agent.UnboundedIntegrationTests.Elasticsearch
                     configModifier.ConfigureFasterSpanEventsHarvestCycle(15);
 
                     configModifier.ForceTransactionTraces();
+                    configModifier.SetLogLevel("finest");
                 },
                 exerciseApplication: () =>
                 {
