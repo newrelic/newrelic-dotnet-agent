c8e2339fa90eda4d7c33e2006f2da15bfa009f62
Chris Ventura <45495992+nrcventura@users.noreply.github.com>
2023-10-24 13:10:16 -0700

    chore: method cache cleanup (#2007)

---
+++ src/Agent/NewRelic/Agent/Core/ProfilerAgentMethodCallCache.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace NewRelic.Agent.Core
+{
+    /// <summary>
+    /// Cache of the Agent methods that can be invoked by the byte-code injected by the profiler.
+    /// </summary>
+    public class ProfilerAgentMethodCallCache
+    {
+        private static readonly ConcurrentDictionary<string, MethodInfo> _methodInfoCache = new ConcurrentDictionary<string, MethodInfo>();
+
+        /// <summary>
+        /// This method is invoked once using reflection by the byte-code injected by the profiler. This method is used to get a 
+        /// reference to the method that is used to cache reflection lookups for Agent methods. These methods are either
+        /// Public Agent API methods, or AgentShim methods.
+        /// </summary>
+        /// <returns>
+        /// A reference to the method that can access the method info cache. The result is treated as an object
+        /// to simplify the type definition injected by the profiler to store this reference.
+        /// </returns>
+        public static object GetMethodCacheFunc()
+        {
+            return (Func<string, string, string, Type[], MethodInfo>)GetAgentMethodInfoFromCache;
+        }
+
+        private static MethodInfo GetAgentMethodInfoFromCache(string key, string className, string methodName, Type[] types)
+        {
+            var methodInfoFactory = new MethodInfoCacheItemFactory(className, methodName, types);
+            return _methodInfoCache!.GetOrAdd(key, methodInfoFactory.GetMethodInfo);
+        }
+
+        // Using a struct to try to take advantage of stack allocation to reduce overall allocations.
+        private struct MethodInfoCacheItemFactory
+        {
+            private string _className;
+            private string _methodName;
+            private Type[] _types;
+
+            public MethodInfoCacheItemFactory(string className, string methodName, Type[] types)
+            {
+                _className = className;
+                _methodName = methodName;
+                _types = types;
+            }
+
+            public MethodInfo GetMethodInfo(string _)
+            {
+                var type = Type.GetType(_className);
+
+                var method = _types != null ? type.GetMethod(_methodName, _types) : type.GetMethod(_methodName);
+
+                return method;
+            }
+        }
+    }
+}
