effca3d5e1e6a2aad706e9c7c01dd1c42b474d4d
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-10-24 15:53:26 -0500

    Bug fixes, etc

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AspNetCore6Plus/ResponseStreamWrapper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AspNetCore6Plus/ResponseStreamWrapper.cs
@@ -8,17 +8,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using NewRelic.Agent.Api;
+using NewRelic.Agent.Extensions.Logging;
 
 namespace NewRelic.Providers.Wrapper.AspNetCore6Plus
 {
     /// <summary>
-    /// Wrapper for the response stream, handles checking for response content type and injecting the browser script if appropriate
+    /// Wrapper for the response stream, handles injecting the browser script if appropriate
     /// </summary>
     public class ResponseStreamWrapper : Stream
     {
         private readonly IAgent _agent;
         private Stream _baseStream;
         private HttpContext _context;
+        private bool _isContentLengthSet;
 
 
         public ResponseStreamWrapper(IAgent agent, Stream baseStream, HttpContext context)
@@ -30,57 +32,83 @@ namespace NewRelic.Providers.Wrapper.AspNetCore6Plus
             CanWrite = true;
         }
 
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            if (!_isContentLengthSet && IsHtmlResponse())
+            {
+                _context.Response.Headers.ContentLength = null;
+                _isContentLengthSet = true;
+            }
+
+            return _baseStream.FlushAsync(cancellationToken);
+        }
+
         public override void Flush()
         {
             _baseStream.Flush();
         }
 
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            return _baseStream.Read(buffer, offset, count);
-        }
+        public override int Read(byte[] buffer, int offset, int count) => _baseStream.Read(buffer, offset, count);
 
         public override long Seek(long offset, SeekOrigin origin) => _baseStream.Seek(offset, origin);
 
         public override void SetLength(long value)
         {
             _baseStream.SetLength(value);
+
+            IsHtmlResponse(forceReCheck: true);
         }
 
-        public override void Write(ReadOnlySpan<byte> buffer)
-        {
-            _baseStream.Write(buffer);
-        }
+        public override void Write(ReadOnlySpan<byte> buffer) => _baseStream.Write(buffer);
 
-        public override void WriteByte(byte value)
-        {
-            _baseStream.WriteByte(value);
-        }
+        public override void WriteByte(byte value) => _baseStream.WriteByte(value);
 
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            var curBuf = buffer.AsMemory(offset, count).ToArray();
-            _agent.TryInjectBrowserScriptAsync(_context.Response.ContentType, _context.Request.Path.Value, curBuf, _baseStream)
+            if (IsHtmlResponse())
+            {
+                var curBuf = buffer.AsMemory(offset, count).ToArray();
+                _agent.TryInjectBrowserScriptAsync(_context.Response.ContentType, _context.Request.Path.Value, curBuf,
+                        _baseStream)
                     .GetAwaiter().GetResult();
+            }
+            else
+            {
+                _agent.CurrentTransaction.LogFinest("ResponseStreamWrapper: Not an HTML response so no attempt to inject RUM.");
+                _baseStream?.Write(buffer, offset, count);
+            }
+
         }
 
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            return WriteAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
-        }
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => WriteAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
 
         public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         {
-            await _agent.TryInjectBrowserScriptAsync(_context.Response.ContentType, _context.Request.Path.Value, buffer.ToArray(), _baseStream);
+            if (IsHtmlResponse())
+            {
+                await _agent.TryInjectBrowserScriptAsync(_context.Response.ContentType, _context.Request.Path.Value,
+                    buffer.ToArray(), _baseStream);
+            }
+            else
+            {
+                _agent.Logger.Log(Level.Finest, "ResponseStreamWrapper: Not an HTML response so no attempt to inject RUM.");
+                if (_baseStream != null)
+                    await _baseStream.WriteAsync(buffer, cancellationToken);
+            }
         }
 
-        protected override void Dispose(bool disposing)
+        public override async ValueTask DisposeAsync()
         {
-            _baseStream = null;
+            // TODO: Debugging only
+            _agent.CurrentTransaction.LogFinest($"ResponseStreamWrapper.DisposeAsync starting ");
             _context = null;
 
-            base.Dispose(disposing);
+            await _baseStream.DisposeAsync();
+            _baseStream = null;
+
+            // TODO: Debugging only
+            _agent.CurrentTransaction.LogFinest($"ResponseStreamWrapper.DisposeAsync complete ");
         }
 
         public override bool CanRead { get; }
@@ -88,5 +116,47 @@ namespace NewRelic.Providers.Wrapper.AspNetCore6Plus
         public override bool CanWrite { get; }
         public override long Length { get; }
         public override long Position { get; set; }
+
+        private bool? _isHtmlResponse = null;
+        private bool IsHtmlResponse(bool forceReCheck = false)
+        {
+            if (!forceReCheck && _isHtmlResponse != null)
+                return _isHtmlResponse.Value;
+
+            // we need to check if the active request is still valid
+            // this can fail if we're in the middle of an error response
+            // or url rewrite in which case we can't intercept
+            if (_context?.Response == null)
+                return false;
+
+            // Requirements for script injection:
+            // * has to have result body
+            // * 200 or 500 response
+            // * text/html response
+            // * UTF-8 formatted (explicit or no charset)
+
+            _isHtmlResponse =
+                _context.Response?.Body != null &&
+                (_context.Response.StatusCode == 200 || _context.Response.StatusCode == 500) &&
+                _context.Response.ContentType != null &&
+                _context.Response.ContentType.Contains("text/html", StringComparison.OrdinalIgnoreCase) &&
+                (_context.Response.ContentType.Contains("utf-8", StringComparison.OrdinalIgnoreCase) ||
+                !_context.Response.ContentType.Contains("charset=", StringComparison.OrdinalIgnoreCase));
+
+            if (!_isHtmlResponse.Value)
+                return false;
+
+            // Make sure we force dynamic content type since we're
+            // rewriting the content - static content will set the header explicitly
+            // and fail when it doesn't matchif (_isHtmlResponse.Value)
+            if (!_isContentLengthSet && _context.Response.ContentLength != null)
+            {
+                _context.Response.Headers.ContentLength = null;
+                _isContentLengthSet = true;
+            }
+
+            return _isHtmlResponse.Value;
+        }
+
     }
 }
