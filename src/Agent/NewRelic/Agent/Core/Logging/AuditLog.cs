c6b940d1c51e79acfa918504bf3adbfbb3e379e5
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-11-06 14:54:35 -0600

    fix: Ensure log lines are written to the audit log file when audit logging is enabled. (#2028) (#2029)

--- src/Agent/NewRelic/Agent/Core/Logging/AuditLog.cs
+++ src/Agent/NewRelic/Agent/Core/Logging/AuditLog.cs
@@ -12,7 +12,7 @@ namespace NewRelic.Agent.Core.Logging
         // a lazy ILogger instance that injects an "Audit" property
         private static Lazy<ILogger> _lazyAuditLogger = LazyAuditLogger();
 
-        public static bool IsAuditLogEnabled { get; set; } //setter is public only for unit tests, not expected to be use anywhere else
+        public static bool IsAuditLogEnabled { get; set; } 
 
         // for unit tests only
         public static void ResetLazyLogger()
@@ -38,15 +38,11 @@ namespace NewRelic.Agent.Core.Logging
 
         public static LoggerConfiguration IncludeOnlyAuditLog(this LoggerConfiguration loggerConfiguration)
         {
-            IsAuditLogEnabled = true; // set a flag so Log() can short-circuit when audit log is not enabled
-
             return loggerConfiguration.Filter.ByIncludingOnly($"{LogLevelExtensions.AuditLevel} is not null");
         }
 
         public static LoggerConfiguration ExcludeAuditLog(this LoggerConfiguration loggerConfiguration)
         {
-            IsAuditLogEnabled = false; // set a flag so Log() can short-circuit when audit log is not enabled
-
             return loggerConfiguration.Filter.ByIncludingOnly($"{LogLevelExtensions.AuditLevel} is null");
         }
     }
