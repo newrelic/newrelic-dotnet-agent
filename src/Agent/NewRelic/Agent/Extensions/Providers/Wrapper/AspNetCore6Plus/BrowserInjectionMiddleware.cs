f0cb727d036aa1eab40c0bfc86dc4f44dfed7035
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-11-08 11:42:37 -0600

    Removed ShouldInject check in the middleware - response.ContentType isn't available at that point.

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AspNetCore6Plus/BrowserInjectionMiddleware.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AspNetCore6Plus/BrowserInjectionMiddleware.cs
@@ -23,24 +23,20 @@ namespace NewRelic.Providers.Wrapper.AspNetCore6Plus
 
         public Task Invoke(HttpContext context)
         {
-            // short-circuit if we're not supposed to inject the browser script at all
-            if (_agent.ShouldInjectBrowserScript(context.Response.ContentType, context.Request.Path.Value))
+            var originalResponseBodyFeature = context.Features.Get<IHttpResponseBodyFeature>();
+            try
             {
-                var originalResponseBodyFeature = context.Features.Get<IHttpResponseBodyFeature>();
-                try
-                {
-                    // wrap the response body in our stream wrapper which will inject the RUM script if appropriate
-                    using var injectedResponse = new BrowserInjectingStreamWrapper(_agent, context.Response.Body, context);
-                    context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(injectedResponse));
-                }
-                catch (Exception e) // don't let any exceptions in our code escape to user code
-                {
-                    // put the original response body feature back, log a message
-                    _agent.Logger.Log(Level.Error, $"Exception in BrowserInjectionMiddleware: {e}");
+                // wrap the response body in our stream wrapper which will inject the RUM script if appropriate
+                using var injectedResponse = new BrowserInjectingStreamWrapper(_agent, context.Response.Body, context);
+                context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(injectedResponse));
+            }
+            catch (Exception e) // don't let any exceptions in our code escape to user code
+            {
+                // put the original response body feature back, log a message
+                _agent.Logger.Log(Level.Error, $"Exception in BrowserInjectionMiddleware: {e}");
 
-                    if (originalResponseBodyFeature != null)
-                        context.Features.Set<IHttpResponseBodyFeature>(originalResponseBodyFeature);
-                }
+                if (originalResponseBodyFeature != null)
+                    context.Features.Set<IHttpResponseBodyFeature>(originalResponseBodyFeature);
             }
 
             return _next(context);
