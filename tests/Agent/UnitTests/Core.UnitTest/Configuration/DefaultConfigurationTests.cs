887061a2fd577ee976c59de0e863c32f974ae42e
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-11-09 16:25:16 -0600

    chore: Disable ASP.NET Core 6+ browser injection by default. (#2052)

--- tests/Agent/UnitTests/Core.UnitTest/Configuration/DefaultConfigurationTests.cs
+++ tests/Agent/UnitTests/Core.UnitTest/Configuration/DefaultConfigurationTests.cs
@@ -3241,8 +3241,8 @@ namespace NewRelic.Agent.Core.Configuration.UnitTest
             return defaultConfig.ForceSynchronousTimingCalculationHttpClient;
         }
 
-        [TestCase(null, ExpectedResult = true)]
-        [TestCase("not a bool", ExpectedResult = true)]
+        [TestCase(null, ExpectedResult = false)]
+        [TestCase("not a bool", ExpectedResult = false)]
         [TestCase("false", ExpectedResult = false)]
         [TestCase("true", ExpectedResult = true)]
         public bool AspNetCore6PlusBrowserInjectionTests(string localConfigValue)
