baad68a97828c9a9a873a0dd8c24f826efec34fd
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2023-11-29 14:59:53 -0600

    Minor cleanup

--- tests/Agent/IntegrationTests/ContainerIntegrationTests/LinuxKafkaTests.cs
+++ tests/Agent/IntegrationTests/ContainerIntegrationTests/LinuxKafkaTests.cs
@@ -18,7 +18,7 @@ public abstract class LinuxKafkaTest<T> : NewRelicIntegrationTest<T> where T : L
 {
     private const int TopicNameLength = 15;
 
-    internal string _topicName;
+    private readonly string _topicName;
     private readonly T _fixture;
 
     protected LinuxKafkaTest(T fixture, ITestOutputHelper output) : base(fixture)
@@ -28,7 +28,6 @@ public abstract class LinuxKafkaTest<T> : NewRelicIntegrationTest<T> where T : L
 
         _topicName = GenerateTopic();
         var brokerName = "kafka-broker";
-        //((ContainerApplication)_fixture.RemoteApplication).DockerDependencies.Add(brokerName);
 
         _fixture.Actions(setupConfiguration: () =>
             {
@@ -113,7 +112,6 @@ public abstract class LinuxKafkaTest<T> : NewRelicIntegrationTest<T> where T : L
     }
 }
 
-//[Collection("Sequential")]
 public class UbuntuX64Kafka1Test : LinuxKafkaTest<UbuntuX64Kafka1TestFixture>
 {
     public UbuntuX64Kafka1Test(UbuntuX64Kafka1TestFixture fixture, ITestOutputHelper output) : base(fixture, output)
@@ -121,7 +119,6 @@ public class UbuntuX64Kafka1Test : LinuxKafkaTest<UbuntuX64Kafka1TestFixture>
     }
 }
 
-//[Collection("Sequential")]
 public class UbuntuX64Kafka2Test : LinuxKafkaTest<UbuntuX64Kafka2TestFixture>
 {
     public UbuntuX64Kafka2Test(UbuntuX64Kafka2TestFixture fixture, ITestOutputHelper output) : base(fixture, output)
