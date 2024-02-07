ab43c8d77d0f65dc0a283bcca53891fa90fb7f36
Alex Hemsath <ahemsath@newrelic.com>
2024-02-07 14:39:06 -0800

    Refactor

--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AwsLambda/HandlerMethodWrapper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AwsLambda/HandlerMethodWrapper.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using Amazon.Lambda.SQSEvents;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using NewRelic.Agent.Api;
 using NewRelic.Agent.Api.Experimental;
@@ -9,14 +10,36 @@ using NewRelic.Agent.Extensions.Providers.Wrapper;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
-//using Newtonsoft.Json;
 
 namespace NewRelic.Providers.Wrapper.AwsLambda
 {
     public class HandlerMethodWrapper : IWrapper
     {
         public List<string> WebInputEventTypes = ["APIGatewayProxyRequest", "ALBTargetGroupRequest"];
+        public Dictionary<string, string> eventTypes = new Dictionary<string, string>()
+        {
+            { "APIGatewayProxyRequest", "apiGateway" },
+            { "ApplicationLoadBalancerRequest", "alb" },
+            { "CloudWatchEvent", "cloudWatch_scheduled" },
+            { "KinesisEvent", "kinesis" },
+            { "SNSEvent", "sns" },
+            { "S3Event", "s3" },
+            { "SimpleEmailEvent", "ses" },
+            { "SQSEvent", "sqs" },
+        };
+
         public bool IsTransactionRequired => false;
+
+        private bool _coldStart = true;
+        private bool IsColdstart()
+        {
+            if (_coldStart)
+            {
+                _coldStart = false;
+                return true;
+            }
+            return false;
+        }
 
         public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
         {
@@ -29,7 +52,7 @@ namespace NewRelic.Providers.Wrapper.AwsLambda
             var inputObject = instrumentedMethodCall.MethodCall.MethodArguments[0];
             var lambdaContext = (ILambdaContext) instrumentedMethodCall.MethodCall.MethodArguments[1];
 
-            var eventTypeName = inputObject.GetType().FullName.Split('.').Last(); // e.g. SQSEvents.SQSEvent
+            var eventTypeName = inputObject.GetType().FullName.Split('.').Last(); // e.g. SQSEvent
 
             var xapi = agent.GetExperimentalApi();
 
@@ -37,31 +60,77 @@ namespace NewRelic.Providers.Wrapper.AwsLambda
 
             transaction = agent.CreateTransaction(
                 isWeb: WebInputEventTypes.Any(s => s == eventTypeName),
-                category: EnumNameCache<WebTransactionType>.GetName(WebTransactionType.ASP),
+                category: "Lambda", // TODO: is this is correct/useful?
                 transactionDisplayName: lambdaContext.FunctionName,
-                doNotTrackAsUnitOfWork: true); 
+                doNotTrackAsUnitOfWork: true);
 
-            // eventually make this a big switch statement
-            if (eventTypeName == "SQSEvent")
-            {
-                var sqsEvent = (SQSEvent)inputObject;
-                transaction.AddCustomAttribute("aws.lambda.eventSource.eventType", "sqs");
-                transaction.AddCustomAttribute("aws.lambda.eventSource.arn", sqsEvent.Records[0].EventSourceArn);
-                xapi.LogFromWrapper($"event source arn={sqsEvent.Records[0].EventSourceArn}");
-                transaction.AddCustomAttribute("aws.lambda.eventSource.length", sqsEvent.Records.Count.ToString());
-                xapi.LogFromWrapper($"event source length={sqsEvent.Records.Count}");
-            } else if (eventTypeName == "ApiGatewayProxyRequest")
-            {
-                // get other attributes
+            var attributes = new Dictionary<string, string>();
 
+            var eventType = eventTypes[eventTypeName];
+
+            attributes.AddEventSourceAttribute("eventType", eventType);
+            attributes.Add("aws.ReqeustId", lambdaContext.AwsRequestId);
+            attributes.Add("aws.lambda.arn", lambdaContext.InvokedFunctionArn);
+            attributes.Add("aws.coldStart", IsColdstart().ToString());
+
+            switch (eventType)
+            {
+                case "apiGateway":
+                    var apiReqEvent = (APIGatewayProxyRequest)inputObject;
+                    //HTTP headers
+                    transaction.SetRequestHeaders(apiReqEvent.Headers, agent.Configuration.AllowAllRequestHeaders ? apiReqEvent.Headers.Keys : Statics.DefaultCaptureHeaders, (headers, key) => headers[key]);
+                    //HTTP method
+                    transaction.SetRequestMethod(apiReqEvent.HttpMethod);
+                    //HTTP uri
+                    transaction.SetUri(apiReqEvent.Path); // TODO: not sure if this is correct
+                    //HTTP query parameters
+                    transaction.SetRequestParameters(apiReqEvent.QueryStringParameters);
+
+                    //aws.lambda.eventSource.accountId    string  event requestContext.accountId Identifier of the API account
+                    attributes.AddEventSourceAttribute("accountId", apiReqEvent.RequestContext.AccountId);
+                    //aws.lambda.eventSource.apiId string event requestContext.apiId Identifier of the API gateway
+                    attributes.AddEventSourceAttribute("apiId", apiReqEvent.RequestContext.ApiId);
+                    //aws.lambda.eventSource.resourceId string event requestContext.resourceId Identifier of the API resource
+                    attributes.AddEventSourceAttribute("resourceId", apiReqEvent.RequestContext.ResourceId);
+                    //aws.lambda.eventSource.resourcePath string event requestContext.resourcePath Path of the API resource
+                    attributes.AddEventSourceAttribute("resourcePath", apiReqEvent.RequestContext.ResourcePath);
+                    //aws.lambda.eventSource.stage string event requestContext.stage Stage of the API resource
+                    attributes.AddEventSourceAttribute("stage", apiReqEvent.RequestContext.Stage);
+
+                    // TODO: insert distributed tracing headers if they're not already there
+                    break;
+                case "sqs":
+                    var sqsEvent = (SQSEvent)inputObject;
+                    attributes.AddEventSourceAttribute("arn", sqsEvent.Records[0].EventSourceArn);
+                    attributes.AddEventSourceAttribute("length", sqsEvent.Records.Count.ToString());
+                    break;
+                default:
+                    break;
             }
 
-
+            transaction.AddLambdaAttributes(attributes, xapi);
 
             var segment = transaction.StartTransactionSegment(instrumentedMethodCall.MethodCall, lambdaContext.FunctionName);
 
 
             return Delegates.GetAsyncDelegateFor<Task>(agent, segment, TaskContinueWithOption.None);
+        }
+    }
+
+    public static class LambdaAttributeExtensions
+    {
+        public static void AddEventSourceAttribute(this Dictionary<string, string> dict, string suffix, string value)
+        {
+            dict.Add($"aws.labmda.eventSource.{suffix}", value);
+        }
+
+        public static void AddLambdaAttributes(this ITransaction transaction, Dictionary<string, string> attributes, IAgentExperimental xapi)
+        {
+            foreach (var attribute in attributes)
+            {
+                xapi.LogFromWrapper($"Lambda Attribute: {attribute.Key}={attribute.Value}");
+                transaction.AddCustomAttribute(attribute.Key, attribute.Value); // TODO: figure out if custom attributes are correct
+            }
         }
 
     }
