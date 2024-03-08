26a9a94ebb461e2c24fcced29e3078a47199af3b
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2024-03-08 10:44:46 -0600

    finished eventhelper tests

--- tests/Agent/UnitTests/NewRelic.Agent.Extensions.Tests/Llm/EventHelperTests.cs
+++ tests/Agent/UnitTests/NewRelic.Agent.Extensions.Tests/Llm/EventHelperTests.cs
@@ -212,26 +212,47 @@ namespace Agent.Extensions.Tests.Llm
             var sequence = 1;
             var completionId = "456";
             var tokenCount = 10;
-            var isResponse = false;
+            var isResponse = true;
+
+            Mock.Arrange(() => _agent.GetLinkingMetadata()).Returns(
+            new Dictionary<string, string>
+            {
+                    { "trace.id", "trace_id"}
+            });
+
+            // replace the RecordLlmEvent method with a mock so we can capture the attributes passed to it
+            Dictionary<string, object> llmAttributes = null;
+            Mock.Arrange(() => _agent.RecordLlmEvent("LlmChatCompletionMessage", Arg.IsAny<Dictionary<string, object>>()))
+                .DoInstead((string eventType, Dictionary<string, object> attributes) =>
+                {
+                    llmAttributes = attributes;
+                });
 
             // Act
             EventHelper.CreateChatMessageEvent(_agent, _segment, requestId, responseModel, content, role, sequence, completionId, tokenCount, isResponse);
 
             // Assert
-            //Assert.That(MockAgent.LlmEventRecorded, Is.True);
-            //Assert.That(MockAgent.LlmEventName, Is.EqualTo("LlmChatCompletionMessage"));
-            //Assert.That(MockAgent.LlmEventAttributes["id"], Is.EqualTo(requestId + "-" + sequence));
-            //Assert.That(MockAgent.LlmEventAttributes["request_id"], Is.EqualTo(requestId));
-            //Assert.That(MockAgent.LlmEventAttributes["span_id"], Is.EqualTo(_segment.SpanId));
-            //Assert.That(MockAgent.LlmEventAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
-            //Assert.That(MockAgent.LlmEventAttributes["response.model"], Is.EqualTo(responseModel));
-            //Assert.That(MockAgent.LlmEventAttributes["vendor"], Is.EqualTo("bedrock"));
-            //Assert.That(MockAgent.LlmEventAttributes["ingest_source"], Is.EqualTo("DotNet"));
-            //Assert.That(MockAgent.LlmEventAttributes["content"], Is.EqualTo(content));
-            //Assert.That(MockAgent.LlmEventAttributes["role"], Is.EqualTo(role));
-            //Assert.That(MockAgent.LlmEventAttributes["sequence"], Is.EqualTo(sequence));
-            //Assert.That(MockAgent.LlmEventAttributes["completion_id"], Is.EqualTo(completionId));
-            //Assert.That(MockAgent.LlmEventAttributes["token_count"], Is.EqualTo(tokenCount));
+            Mock.Assert(() => _agent.RecordLlmEvent("LlmChatCompletionMessage", Arg.IsAny<Dictionary<string, object>>()), Occurs.Once());
+
+            Assert.Multiple(() =>
+            {
+                // assert that the attributes passed to _agent.RecordLlmEvent are correct
+                Assert.That(llmAttributes, Is.Not.Null);
+                Assert.That(llmAttributes.Count, Is.EqualTo(13));
+                Assert.That(llmAttributes["id"], Is.EqualTo(requestId + "-" + sequence));
+                Assert.That(llmAttributes["request_id"], Is.EqualTo(requestId));
+                Assert.That(llmAttributes["span_id"], Is.EqualTo(_segment.SpanId));
+                Assert.That(llmAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
+                Assert.That(llmAttributes["response.model"], Is.EqualTo(responseModel));
+                Assert.That(llmAttributes["vendor"], Is.EqualTo("bedrock"));
+                Assert.That(llmAttributes["ingest_source"], Is.EqualTo("DotNet"));
+                Assert.That(llmAttributes["content"], Is.EqualTo(content));
+                Assert.That(llmAttributes["role"], Is.EqualTo(role));
+                Assert.That(llmAttributes["sequence"], Is.EqualTo(sequence));
+                Assert.That(llmAttributes["completion_id"], Is.EqualTo(completionId));
+                Assert.That(llmAttributes["token_count"], Is.EqualTo(tokenCount));
+                Assert.That(llmAttributes["is_response"], Is.True);
+            });
         }
 
         [Test]
@@ -244,27 +265,75 @@ namespace Agent.Extensions.Tests.Llm
             var responseModel = "model2";
             var vendor = "vendor1";
             var tokenCount = 10;
-            var isError = false;
-            var headers = new Dictionary<string, string>();
-            var errorData = new LlmErrorData();
+
+            var headers = new Dictionary<string, string>
+            {
+                {"llmVersion", "1.0"},
+                {"ratelimitLimitRequests", "99"},
+                {"ratelimitLimitTokens", "99"},
+                {"ratelimitResetTokens", "ratelimitResetTokens"},
+                {"ratelimitResetRequests", "ratelimitResetRequests" },
+                {"ratelimitRemainingTokens", "99"},
+                {"ratelimitRemainingRequests", "99"},
+                {"ratelimitLimitTokensUsageBased", "99"},
+                {"ratelimitResetTokensUsageBased", "ratelimitResetTokensUsageBased"},
+                {"ratelimitRemainingTokensUsageBased", "99"},
+                {"header1", "value1"},
+                {"header2", "value2"}
+            };
+
+            Mock.Arrange(() => _agent.GetLinkingMetadata()).Returns(
+                new Dictionary<string, string>
+                {
+                    { "trace.id", "trace_id"}
+                });
+
+            // replace the RecordLlmEvent method with a mock so we can capture the attributes passed to it
+            Dictionary<string, object> llmAttributes = null;
+            Mock.Arrange(() => _agent.RecordLlmEvent("LlmEmbedding", Arg.IsAny<Dictionary<string, object>>()))
+                .DoInstead((string eventType, Dictionary<string, object> attributes) =>
+                {
+                    llmAttributes = attributes;
+                });
 
             // Act
-            EventHelper.CreateEmbeddingEvent(_agent, _segment, requestId, input, requestModel, responseModel, vendor, tokenCount, isError, headers, errorData);
+            EventHelper.CreateEmbeddingEvent(_agent, _segment, requestId, input, requestModel, responseModel, vendor, tokenCount, false, headers, null);
 
             // Assert
-            //Assert.That(MockAgent.LlmEventRecorded, Is.True);
-            //Assert.That(MockAgent.LlmEventName, Is.EqualTo("LlmEmbedding"));
-            //Assert.That(MockAgent.LlmEventAttributes["id"], Is.EqualTo(MockAgent.LlmEventAttributes["embedding_id"]));
-            //Assert.That(MockAgent.LlmEventAttributes["request_id"], Is.EqualTo(requestId));
-            //Assert.That(MockAgent.LlmEventAttributes["span_id"], Is.EqualTo(_segment.SpanId));
-            //Assert.That(MockAgent.LlmEventAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
-            //Assert.That(MockAgent.LlmEventAttributes["input"], Is.EqualTo(input));
-            //Assert.That(MockAgent.LlmEventAttributes["request.model"], Is.EqualTo(requestModel));
-            //Assert.That(MockAgent.LlmEventAttributes["response.model"], Is.EqualTo(responseModel));
-            //Assert.That(MockAgent.LlmEventAttributes["vendor"], Is.EqualTo(vendor));
-            //Assert.That(MockAgent.LlmEventAttributes["ingest_source"], Is.EqualTo("DotNet"));
-            //Assert.That(MockAgent.LlmEventAttributes["duration"], Is.EqualTo((float)_segment.DurationOrZero.TotalMilliseconds));
-            //Assert.That(MockAgent.LlmEventAttributes["token_count"], Is.EqualTo(tokenCount));
+            Mock.Assert(() => _agent.RecordLlmEvent("LlmEmbedding", Arg.IsAny<Dictionary<string, object>>()), Occurs.Once());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(llmAttributes, Is.Not.Null);
+                Assert.That(llmAttributes.Count, Is.EqualTo(21));
+                Assert.That(llmAttributes.ContainsKey("id"));
+                Assert.That(llmAttributes["request_id"], Is.EqualTo(requestId));
+                Assert.That(llmAttributes["span_id"], Is.EqualTo(_segment.SpanId));
+                Assert.That(llmAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
+                Assert.That(llmAttributes["input"], Is.EqualTo(input));
+                Assert.That(llmAttributes["request.model"], Is.EqualTo(requestModel));
+                Assert.That(llmAttributes["response.model"], Is.EqualTo(responseModel));
+                Assert.That(llmAttributes["vendor"], Is.EqualTo(vendor));
+                Assert.That(llmAttributes["ingest_source"], Is.EqualTo("DotNet"));
+                Assert.That(llmAttributes["duration"], Is.EqualTo((float)_segment.DurationOrZero.TotalMilliseconds));
+                Assert.That(llmAttributes["token_count"], Is.EqualTo(tokenCount));
+                Assert.That(llmAttributes["llmVersion"], Is.EqualTo("1.0"));
+
+                Assert.That(llmAttributes["ratelimitLimitRequests"], Is.EqualTo(99));
+                Assert.That(llmAttributes["ratelimitLimitTokens"], Is.EqualTo(99));
+                Assert.That(llmAttributes["ratelimitResetTokens"], Is.EqualTo("ratelimitResetTokens"));
+                Assert.That(llmAttributes["ratelimitResetRequests"], Is.EqualTo("ratelimitResetRequests"));
+                Assert.That(llmAttributes["ratelimitRemainingTokens"], Is.EqualTo(99));
+                Assert.That(llmAttributes["ratelimitRemainingRequests"], Is.EqualTo(99));
+                Assert.That(llmAttributes["ratelimitLimitTokensUsageBased"], Is.EqualTo(99));
+                Assert.That(llmAttributes["ratelimitResetTokensUsageBased"], Is.EqualTo("ratelimitResetTokensUsageBased"));
+                Assert.That(llmAttributes["ratelimitRemainingTokensUsageBased"], Is.EqualTo(99));
+
+                Assert.That(llmAttributes, Does.Not.ContainKey("error"));
+                Assert.That(llmAttributes, Does.Not.ContainKey("header1"));
+                Assert.That(llmAttributes, Does.Not.ContainKey("header2"));
+
+            });
         }
 
         [Test]
@@ -277,32 +346,80 @@ namespace Agent.Extensions.Tests.Llm
             var responseModel = "model2";
             var vendor = "vendor1";
             var tokenCount = 10;
-            var isError = true;
-            var headers = new Dictionary<string, string>();
-            var errorData = new LlmErrorData();
+
+            var errorData = new LlmErrorData
+            {
+                ErrorMessage = "error_message",
+                ErrorCode = "error_code",
+                ErrorParam = "error_param",
+                HttpStatusCode = "500"
+            };
+
+            Mock.Arrange(() => _agent.GetLinkingMetadata()).Returns(
+                new Dictionary<string, string>
+                {
+                    { "trace.id", "trace_id"}
+                });
+
+            // replace the RecordLlmEvent method with a mock so we can capture the attributes passed to it
+            Dictionary<string, object> llmAttributes = null;
+            Mock.Arrange(() => _agent.RecordLlmEvent("LlmEmbedding", Arg.IsAny<Dictionary<string, object>>()))
+                .DoInstead((string eventType, Dictionary<string, object> attributes) =>
+                {
+                    llmAttributes = attributes;
+                });
+
+            // create a mock agent api implementation
+            var agentApiMock = Mock.Create<IAgentApi>();
+
+            // replace NoticeError method with a mock so we can capture the error message and attributes
+            var noticedError = false;
+            string errorMessage = null;
+            Dictionary<string, object> errorAttributes = null;
+
+            Mock.Arrange(() => agentApiMock.NoticeError(Arg.IsAny<string>(), Arg.IsAny<Dictionary<string, object>>()))
+                .DoInstead((string message, Dictionary<string, object> attributes) =>
+                {
+                    noticedError = true;
+                    errorMessage = message;
+                    errorAttributes = attributes;
+                });
+
+            InternalApi.SetAgentApiImplementation(agentApiMock);
+            
 
             // Act
-            EventHelper.CreateEmbeddingEvent(_agent, _segment, requestId, input, requestModel, responseModel, vendor, tokenCount, isError, headers, errorData);
+            EventHelper.CreateEmbeddingEvent(_agent, _segment, requestId, input, requestModel, responseModel, vendor, tokenCount, true, null, errorData);
 
             // Assert
-            //Assert.That(MockAgent.LlmEventRecorded, Is.True);
-            //Assert.That(MockAgent.LlmEventName, Is.EqualTo("LlmEmbedding"));
-            //Assert.That(MockAgent.LlmEventAttributes["id"], Is.EqualTo(MockAgent.LlmEventAttributes["embedding_id"]));
-            //Assert.That(MockAgent.LlmEventAttributes["request_id"], Is.EqualTo(requestId));
-            //Assert.That(MockAgent.LlmEventAttributes["span_id"], Is.EqualTo(_segment.SpanId));
-            //Assert.That(MockAgent.LlmEventAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
-            //Assert.That(MockAgent.LlmEventAttributes["input"], Is.EqualTo(input));
-            //Assert.That(MockAgent.LlmEventAttributes["request.model"], Is.EqualTo(requestModel));
-            //Assert.That(MockAgent.LlmEventAttributes["response.model"], Is.EqualTo(responseModel));
-            //Assert.That(MockAgent.LlmEventAttributes["vendor"], Is.EqualTo(vendor));
-            //Assert.That(MockAgent.LlmEventAttributes["ingest_source"], Is.EqualTo("DotNet"));
-            //Assert.That(MockAgent.LlmEventAttributes["duration"], Is.EqualTo((float)_segment.DurationOrZero.TotalMilliseconds));
-            //Assert.That(MockAgent.LlmEventAttributes["token_count"], Is.EqualTo(tokenCount));
-            //Assert.That(MockAgent.NoticedError, Is.True);
-            //Assert.That(MockAgent.ErrorMessage, Is.EqualTo(errorData.ErrorMessage));
-            //Assert.That(MockAgent.ErrorAttributes["error.code"], Is.EqualTo(errorData.ErrorCode));
-            //Assert.That(MockAgent.ErrorAttributes["error.param"], Is.EqualTo(errorData.ErrorParam));
-            //Assert.That(MockAgent.ErrorAttributes["http.statusCode"], Is.EqualTo(errorData.HttpStatusCode));
+            Mock.Assert(() => _agent.RecordLlmEvent("LlmEmbedding", Arg.IsAny<Dictionary<string, object>>()), Occurs.Once());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(llmAttributes, Is.Not.Null);
+                Assert.That(llmAttributes.Count, Is.EqualTo(12));
+                Assert.That(llmAttributes.ContainsKey("id"));
+                Assert.That(llmAttributes["request_id"], Is.EqualTo(requestId));
+                Assert.That(llmAttributes["span_id"], Is.EqualTo(_segment.SpanId));
+                Assert.That(llmAttributes["trace_id"], Is.EqualTo(_agent.GetLinkingMetadata()["trace.id"]));
+                Assert.That(llmAttributes["input"], Is.EqualTo(input));
+                Assert.That(llmAttributes["request.model"], Is.EqualTo(requestModel));
+                Assert.That(llmAttributes["response.model"], Is.EqualTo(responseModel));
+                Assert.That(llmAttributes["vendor"], Is.EqualTo(vendor));
+                Assert.That(llmAttributes["ingest_source"], Is.EqualTo("DotNet"));
+                Assert.That(llmAttributes["duration"], Is.EqualTo((float)_segment.DurationOrZero.TotalMilliseconds));
+                Assert.That(llmAttributes["token_count"], Is.EqualTo(tokenCount));
+
+                Assert.That(llmAttributes, Does.Not.ContainKey("llmVersion"));
+
+                Assert.That(llmAttributes["error"], Is.True);
+
+                Assert.That(noticedError, Is.True);
+                Assert.That(errorMessage, Is.EqualTo(errorData.ErrorMessage));
+                Assert.That(errorAttributes["error.code"], Is.EqualTo(errorData.ErrorCode));
+                Assert.That(errorAttributes["error.param"], Is.EqualTo(errorData.ErrorParam));
+                Assert.That(errorAttributes["http.statusCode"], Is.EqualTo(errorData.HttpStatusCode));
+            });
         }
     }
 }
