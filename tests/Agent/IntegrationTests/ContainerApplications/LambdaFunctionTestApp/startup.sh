4959c6e1f2dd3c0382ef8a022188d9a7d8e8b2c0
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2024-05-06 16:19:11 -0500

    Removed some unused code from the ContainerIntegrationTests solution

--- tests/Agent/IntegrationTests/ContainerApplications/LambdaFunctionTestApp/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create the pid file that tells the container application that we're up and running
-touch /app/logs/containerizedapp.pid
-
-# launch the "real" entrypoint script
-source /lambda-entrypoint.sh "$@"
