0dc031d580cd78ddcdbf2c6101579c572919717e
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2024-05-24 11:51:54 -0500

    Reverted changes to NewRelic.NuGetHelper. It's used by some Powershell scripts. Who knew??

---
+++ build/NewRelic.NuGetHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NewRelic.NuGetHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("New Relic")]
+[assembly: AssemblyProduct("NewRelic.NuGetHelper")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94bf8d27-2122-4573-aa79-90b977b40ef3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ build/NewRelic.NuGetHelper/Utils.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet;
+
+namespace NewRelic.NuGetHelper
+{
+    public static class Utils
+    {
+        public static IPackage FindPackage(string packageId, string maxVersion, string repoUrl)
+        {
+            var repo = PackageRepositoryFactory.Default.CreateRepository(repoUrl);
+
+            var versionSpec = new VersionSpec();
+            if (!string.IsNullOrEmpty(maxVersion))
+            {
+                versionSpec.MaxVersion = SemanticVersion.Parse(maxVersion);
+            }
+            var package = repo.FindPackage(packageId, versionSpec, false, false);
+
+            return package;
+        }
+    }
+}
