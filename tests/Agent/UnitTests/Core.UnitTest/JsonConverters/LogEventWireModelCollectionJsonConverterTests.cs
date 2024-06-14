f3b45b732a8be42dd21345d1bb4420e58c1d548a
chynesNR <chynes@newrelic.com>
2024-06-14 16:06:05 -0700

    test: Refactor tests using large single-line JSON strings

--- tests/Agent/UnitTests/Core.UnitTest/JsonConverters/LogEventWireModelCollectionJsonConverterTests.cs
+++ tests/Agent/UnitTests/Core.UnitTest/JsonConverters/LogEventWireModelCollectionJsonConverterTests.cs
@@ -7,6 +7,7 @@ using NewRelic.Agent.Core.JsonConverters;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using NewRelic.Agent.Core.WireModels;
+using NewRelic.Agent.TestUtilities;
 
 namespace NewRelic.Agent.Core.Utilities
 {
@@ -16,7 +17,62 @@ namespace NewRelic.Agent.Core.Utilities
         [Test]
         public void LogEventWireModelCollectionIsJsonSerializable()
         {
-            var expected = @"{""common"":{""attributes"":{""entity.name"":""myApplicationName"",""entity.guid"":""guid"",""hostname"":""hostname""}},""logs"":[{""timestamp"":1,""message"":""TestMessage1"",""level"":""TestLevel"",""span.id"":""TestSpanId1"",""trace.id"":""TestTraceId1"",""attributes"":{""context.key1"":""value1"",""context.key2"":1,""context.key3"":{""Foo"":1,""Bar"":2}}},{""timestamp"":1,""message"":""TestMessage2"",""level"":""TestLevel"",""span.id"":""TestSpanId2"",""trace.id"":""TestTraceId2""},{""timestamp"":1,""message"":""TestMessage3"",""level"":""TestLevel"",""span.id"":""TestSpanId3"",""trace.id"":""TestTraceId3""},{""timestamp"":1,""message"":""TestMessage4"",""level"":""TestLevel"",""error.stack"":""foo \nbar"",""error.message"":""errorMessage"",""error.class"":""errorClass"",""span.id"":""TestSpanId4"",""trace.id"":""TestTracedId4"",""attributes"":{""context.key1"":""value1"",""context.key2"":1,""context.key3"":{""Foo"":1,""Bar"":2}}}]}";
+            var expected = """
+                {
+                    "common": {
+                        "attributes": {
+                            "entity.name": "myApplicationName",
+                            "entity.guid": "guid",
+                            "hostname": "hostname"
+                        }
+                    },
+                    "logs": [{
+                            "timestamp": 1,
+                            "message": "TestMessage1",
+                            "level": "TestLevel",
+                            "span.id": "TestSpanId1",
+                            "trace.id": "TestTraceId1",
+                            "attributes": {
+                                "context.key1": "value1",
+                                "context.key2": 1,
+                                "context.key3": {
+                                    "Foo": 1,
+                                    "Bar": 2
+                                }
+                            }
+                        }, {
+                            "timestamp": 1,
+                            "message": "TestMessage2",
+                            "level": "TestLevel",
+                            "span.id": "TestSpanId2",
+                            "trace.id": "TestTraceId2"
+                        }, {
+                            "timestamp": 1,
+                            "message": "TestMessage3",
+                            "level": "TestLevel",
+                            "span.id": "TestSpanId3",
+                            "trace.id": "TestTraceId3"
+                        }, {
+                            "timestamp": 1,
+                            "message": "TestMessage4",
+                            "level": "TestLevel",
+                            "error.stack": "foo \nbar",
+                            "error.message": "errorMessage",
+                            "error.class": "errorClass",
+                            "span.id": "TestSpanId4",
+                            "trace.id": "TestTracedId4",
+                            "attributes": {
+                                "context.key1": "value1",
+                                "context.key2": 1,
+                                "context.key3": {
+                                    "Foo": 1,
+                                    "Bar": 2
+                                }
+                            }
+                        }
+                    ]
+                }
+                """;
 
             var _contextData = new Dictionary<string, object>() { { "key1", "value1" }, { "key2", 1 }, {"key3", new { Foo = 1, Bar = 2 } } };
             var sourceObject = new LogEventWireModelCollection(
@@ -32,7 +88,7 @@ namespace NewRelic.Agent.Core.Utilities
             });
 
             var serialized = JsonConvert.SerializeObject(sourceObject, Formatting.None);
-            Assert.That(serialized, Is.EqualTo(expected));
+            Assert.That(serialized, Is.EqualTo(expected.Condense()));
         }
 
         [Test]
