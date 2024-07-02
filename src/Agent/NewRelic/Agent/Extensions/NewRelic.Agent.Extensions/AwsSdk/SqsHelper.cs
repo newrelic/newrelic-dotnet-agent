4a6c869d437f35f409e8d3e3a07b72c4385338ad
Marty T <120425148+tippmar-nr@users.noreply.github.com>
2024-07-02 16:25:15 -0500

    SQS: Distributed Tracing support (#2591)
    ---------
    
    Co-authored-by: Alex Hemsath <ahemsath@newrelic.com>

--- src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/AwsSdk/SqsHelper.cs
+++ src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/AwsSdk/SqsHelper.cs
@@ -2,14 +2,20 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using NewRelic.Agent.Api;
+using NewRelic.Agent.Api.Experimental;
 using NewRelic.Agent.Extensions.Providers.Wrapper;
+using NewRelic.Reflection;
 
 namespace NewRelic.Agent.Extensions.AwsSdk
 {
     public static class SqsHelper
     {
+        private static Func<object, IDictionary> _getMessageAttributes;
+        private static Func<object> _messageAttributeValueTypeFactory;
+
         public const string VendorName = "SQS";
 
         private class SqsAttributes
@@ -48,25 +54,57 @@ namespace NewRelic.Agent.Extensions.AwsSdk
         public static ISegment GenerateSegment(ITransaction transaction, MethodCall methodCall, string url, MessageBrokerAction action)
         {
             var attr = new SqsAttributes(url);
-            return transaction.StartMessageBrokerSegment(methodCall, MessageBrokerDestinationType.Queue, action, VendorName, attr.QueueName);
+            var segment = transaction.StartMessageBrokerSegment(methodCall, MessageBrokerDestinationType.Queue, action, VendorName, attr.QueueName);
+            segment.GetExperimentalApi().MakeLeaf();
+
+            return segment;
         }
 
-        public static void InsertDistributedTraceHeaders(ITransaction transaction, dynamic webRequest)
+        public static void InsertDistributedTraceHeaders(ITransaction transaction, object sendMessageRequest)
         {
-            var setHeaders = new Action<dynamic, string, string>((wr, key, value) =>
+            var headersInserted = 0;
+
+            var setHeaders = new Action<object, string, string>((smr, key, value) =>
             {
-                var headers = wr.Headers as IDictionary<string, object>;
+                var getMessageAttributes = _getMessageAttributes ??=
+                    VisibilityBypasser.Instance.GeneratePropertyAccessor<IDictionary>(
+                        smr.GetType(), "MessageAttributes");
 
-                if (headers == null)
-                {
-                    headers = new Dictionary<string, object>();
-                    wr.Headers = headers;
-                }
+                var messageAttributes = getMessageAttributes(smr);
 
-                headers[key] = value;
+                // SQS is limited to no more than 10 attributes; if we can't add up to 3 attributes, don't add any
+                if ((messageAttributes.Count + 3 - headersInserted) > 10)
+                    return;
+
+                // create a new MessageAttributeValue instance
+                var messageAttributeValueTypeFactory = _messageAttributeValueTypeFactory ??= VisibilityBypasser.Instance.GenerateTypeFactory(smr.GetType().Assembly.FullName, "Amazon.SQS.Model.MessageAttributeValue");
+                object newMessageAttributeValue = messageAttributeValueTypeFactory.Invoke();
+
+                var dataTypePropertySetter = VisibilityBypasser.Instance.GeneratePropertySetter<string>(newMessageAttributeValue, "DataType");
+                dataTypePropertySetter("String");
+
+                var stringValuePropertySetter = VisibilityBypasser.Instance.GeneratePropertySetter<string>(newMessageAttributeValue, "StringValue");
+                stringValuePropertySetter(value);
+
+                messageAttributes.Add(key, newMessageAttributeValue);
+
+                ++headersInserted;
             });
 
-            transaction.InsertDistributedTraceHeaders(webRequest, setHeaders);
+            transaction.InsertDistributedTraceHeaders(sendMessageRequest, setHeaders);
+
+        }
+        public static void AcceptDistributedTraceHeaders(ITransaction transaction, dynamic messageAttributes)
+        {
+            var getHeaders = new Func<IDictionary, string, IEnumerable<string>>((maDict, key) =>
+            {
+                if (!maDict.Contains(key))
+                    return [];
+
+                return [(string)((dynamic)maDict[key]).StringValue];
+            });
+
+            transaction.AcceptDistributedTraceHeaders((IDictionary)messageAttributes, getHeaders, TransportType.Queue);
 
         }
     }
--- src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AwsSdk/AwsSdkPipelineWrapper.cs
+++ src/Agent/NewRelic/Agent/Extensions/Providers/Wrapper/AwsSdk/AwsSdkPipelineWrapper.cs
@@ -1,9 +1,14 @@
 // Copyright 2020 New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Collections.Concurrent;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using NewRelic.Agent.Api;
 using NewRelic.Agent.Extensions.AwsSdk;
 using NewRelic.Agent.Extensions.Providers.Wrapper;
+using NewRelic.Reflection;
 
 namespace NewRelic.Providers.Wrapper.AwsSdk
 {
@@ -12,6 +17,12 @@ namespace NewRelic.Providers.Wrapper.AwsSdk
         public bool IsTransactionRequired => true;
 
         private const string WrapperName = "AwsSdkPipelineWrapper";
+        private static readonly ConcurrentDictionary<Type, Func<object, object>> _getRequestResponseFromGeneric = new();
+
+        private const string NEWRELIC_TRACE_HEADER = "newrelic";
+        private const string W3C_TRACEPARENT_HEADER = "traceparent";
+        private const string W3C_TRACESTATE_HEADER = "tracestate";
+
 
         public CanWrapResponse CanWrap(InstrumentedMethodInfo methodInfo)
         {
@@ -23,6 +34,15 @@ namespace NewRelic.Providers.Wrapper.AwsSdk
             // Get the IExecutionContext (the only parameter)
             dynamic executionContext = instrumentedMethodCall.MethodCall.MethodArguments[0];
 
+            var isAsync = instrumentedMethodCall.IsAsync ||
+                          instrumentedMethodCall.InstrumentedMethodInfo.Method.MethodName == "InvokeAsync";
+
+            if (isAsync)
+            {
+                transaction.AttachToAsync();
+                transaction.DetachFromPrimary(); //Remove from thread-local type storage
+            }
+
             // Get the IRequestContext
             if (executionContext.RequestContext == null)
             {
@@ -30,16 +50,6 @@ namespace NewRelic.Providers.Wrapper.AwsSdk
                 return Delegates.NoOp;
             }
             dynamic requestContext = executionContext.RequestContext;
-
-            if (requestContext.ServiceMetaData == null)
-            {
-                agent.Logger.Debug("AwsSdkPipelineWrapper: requestContext.ServiceMetaData is null. Returning NoOp delegate.");
-                return Delegates.NoOp;
-            }
-            dynamic metadata = requestContext.ServiceMetaData;
-
-            // check for null first if we decide to use this property
-            // string requestId = metadata.ServiceId; // SQS?
 
             // Get the AmazonWebServiceRequest being invoked. The name will tell us the type of request
             if (requestContext.OriginalRequest == null)
@@ -53,13 +63,11 @@ namespace NewRelic.Providers.Wrapper.AwsSdk
             agent.Logger.Finest("AwsSdkPipelineWrapper: Request type is " + requestType);
 
             MessageBrokerAction action;
-            var insertDistributedTraceHeaders = false;
             switch (requestType)
             {
                 case "SendMessageRequest":
                 case "SendMessageBatchRequest":
                     action = MessageBrokerAction.Produce;
-                    insertDistributedTraceHeaders = true;
                     break;
                 case "ReceiveMessageRequest":
                     action = MessageBrokerAction.Consume;
@@ -74,19 +82,67 @@ namespace NewRelic.Providers.Wrapper.AwsSdk
 
             string requestQueueUrl = request.QueueUrl;
             ISegment segment = SqsHelper.GenerateSegment(transaction, instrumentedMethodCall.MethodCall, requestQueueUrl, action);
-            if (insertDistributedTraceHeaders)
+            if (action == MessageBrokerAction.Produce)
             {
-                // This needs to happen at the end
-                if (requestContext.Request == null)
-                    agent.Logger.Finest("AwsSdkPipelineWrapper: requestContext.Request is null, unable to insert distributed trace headers.");
+                if (request.MessageAttributes == null)
+                {
+                    agent.Logger.Debug("AwsSdkPipelineWrapper: requestContext.OriginalRequest.MessageAttributes is null, unable to insert distributed trace headers.");
+                }
                 else
                 {
-                    dynamic webRequest = requestContext.Request;
-                    SqsHelper.InsertDistributedTraceHeaders(transaction, webRequest);
+                    SqsHelper.InsertDistributedTraceHeaders(transaction, request);
                 }
             }
 
-            return Delegates.GetDelegateFor(segment);
+            // modify the request to ask for DT headers in the response message attributes
+            if (action == MessageBrokerAction.Consume)
+            {
+                if (request.MessageAttributeNames == null)
+                    request.MessageAttributeNames = new List<string>();
+
+                request.MessageAttributeNames.Add(NEWRELIC_TRACE_HEADER);
+                request.MessageAttributeNames.Add(W3C_TRACESTATE_HEADER);
+                request.MessageAttributeNames.Add(W3C_TRACEPARENT_HEADER);
+            }
+
+
+            if (isAsync)
+            {
+                return Delegates.GetAsyncDelegateFor<Task>(agent, segment, true, ProcessResponse, TaskContinuationOptions.ExecuteSynchronously);
+
+                void ProcessResponse(Task responseTask)
+                {
+                    if (!ValidTaskResponse(responseTask) || (segment == null) || action != MessageBrokerAction.Consume)
+                        return;
+
+                    // taskResult is a ReceiveMessageResponse
+                    var taskResultGetter = _getRequestResponseFromGeneric.GetOrAdd(responseTask.GetType(), t => VisibilityBypasser.Instance.GeneratePropertyAccessor<object>(t, "Result"));
+                    dynamic receiveMessageResponse = taskResultGetter(responseTask);
+
+                    // accept distributed trace headers from the first message in the response
+                    SqsHelper.AcceptDistributedTraceHeaders(transaction, receiveMessageResponse.Messages[0].MessageAttributes);
+                }
+            }
+
+            return Delegates.GetDelegateFor(
+                onComplete: segment.End,
+                onSuccess: () =>
+                {
+                    if (action != MessageBrokerAction.Consume)
+                        return;
+
+                    var ec = executionContext;
+                    var response = ec.ResponseContext.Response; // response is a ReceiveMessageResponse
+
+                    // accept distributed trace headers from the first message in the response
+                    SqsHelper.AcceptDistributedTraceHeaders(transaction, response.Messages[0].MessageAttributes);
+                }
+            );
+        }
+
+        private static bool ValidTaskResponse(Task response)
+        {
+            return response?.Status == TaskStatus.RanToCompletion;
         }
     }
 }
