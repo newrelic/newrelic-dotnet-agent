7631d1b9bbd7a0db6099abf4d55668b5c84f5a99
Jacob Affinito <jaffinito@newrelic.com>
2024-07-03 10:55:28 -0700

    Fix some missed unit tests.

--- tests/Agent/UnitTests/Core.UnitTest/DataTransport/AgentSettingsTests.cs
+++ tests/Agent/UnitTests/Core.UnitTest/DataTransport/AgentSettingsTests.cs
@@ -338,7 +338,7 @@ namespace NewRelic.Agent.Core.Configuration
                         }
                     ],
                     "agent.disable_file_system_watcher": false,
-                    "agent.ai_monitoring.enabled": true,
+                    "ai_monitoring.enabled": true,
                     "ai_monitoring.streaming.enabled": true,
                     "ai_monitoring.record_content.enabled": true
                 }
