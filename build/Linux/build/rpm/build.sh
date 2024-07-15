3217e1c1267d1916707d8052b1844f4ccff5a9c2
Marty T <120425148+tippmar-nr@users.noreply.github.com>
2024-07-15 17:25:44 -0500

    ci: Build RPM on Ubuntu (#2627)
    Modifies the RPM build process to use Ubuntu instead of CentOS.
    Surprisingly, only a few minor changes were required.
    
    Successful CI run:
    https://github.com/newrelic/newrelic-dotnet-agent/actions/runs/9943833300/job/27468784640?pr=2627
    
    Verified by pulling latest Fedora docker image, copying the .rpm file
    from the build artifact to the container and running `rpm -i` on it.

--- build/Linux/build/rpm/build.sh
+++ build/Linux/build/rpm/build.sh
@@ -5,7 +5,7 @@ set -e # Halt the build script on any error
 function sign_rpm {
     rpm_file="$1"
     # unpack the tarball, rename it and set perms
-    tar -jxvf "$GPG_KEYS"
+    dtrx --one here "$GPG_KEYS"
     mv gpg-conf "$HOME/.gnupg"
     chown root:root "$HOME/.gnupg"
     chmod 0700 "$HOME/.gnupg"
@@ -18,20 +18,7 @@ function sign_rpm {
 %_gpgbin /usr/bin/gpg
 MACROS
 
-    # create an expect script to run the sign command
-    # this is necessary because the sign tool asks for a passphrase
-    cat << EXPECT | tee sign.expect
-#!/usr/bin/expect -f
-set timeout -1
-spawn rpm --addsign $rpm_file
-expect "Enter pass phrase:"
-send -- "\r"
-expect eof
-EXPECT
-
-    # run the expect script
-    chmod a+x sign.expect
-    ./sign.expect
+    rpm --addsign $rpm_file
 }
 
 PACKAGE_NAME='newrelic-dotnet-agent'
