b21b93420a21009d10a245c335a0fd202a767301
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2024-08-29 11:48:46 -0500

    minor cleanup

--- tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Oracle/OracleExerciser.cs
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Oracle/OracleExerciser.cs
@@ -24,9 +24,11 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Oracle
         private const string InsertHotelOracleSql = "INSERT INTO {0} (HOTEL_ID, BOOKING_DATE) VALUES (1, SYSDATE)";
         private const string DeleteHotelOracleSql = "DELETE FROM {0} WHERE HOTEL_ID = 1";
         private const string CountHotelOracleSql = "SELECT COUNT(*) FROM {0}";
+        private const string SelectDegreeFromUserTablesWhereRownum = "SELECT DEGREE FROM user_tables WHERE ROWNUM <= 1";
 
 
         private string _tableName;
+        private string _storedProcedureName;
 
         [LibraryMethod]
         public void InitializeTable(string tableName)
@@ -55,7 +57,7 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Oracle
             using var connection = new OracleConnection(connectionString);
             connection.Open();
 
-            using (var command = new OracleCommand("SELECT DEGREE FROM user_tables WHERE ROWNUM <= 1", connection))
+            using (var command = new OracleCommand(SelectDegreeFromUserTablesWhereRownum, connection))
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())
@@ -97,7 +99,7 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Oracle
             using var connection = new OracleConnection(connectionString);
             await connection.OpenAsync();
 
-            using (var command = new OracleCommand("SELECT DEGREE FROM user_tables WHERE ROWNUM <= 1", connection))
+            using (var command = new OracleCommand(SelectDegreeFromUserTablesWhereRownum, connection))
             using (var reader = await command.ExecuteReaderAsync())
             {
                 while (await reader.ReadAsync())
@@ -186,14 +188,13 @@ namespace MultiFunctionApplicationHelpers.NetStandardLibraries.Oracle
             _storedProcedureName = null;
         }
 
-        private readonly string createProcedureStatment = @"CREATE PROCEDURE {0} ({1}) IS BEGIN NULL; END {0};";
+        private readonly string createProcedureStatement = @"CREATE PROCEDURE {0} ({1}) IS BEGIN NULL; END {0};";
         private readonly string dropProcedureStatement = @"DROP PROCEDURE {0}";
-        private string _storedProcedureName;
 
         private void CreateProcedure()
         {
             var parameters = string.Join(", ", DbParameterData.OracleParameters.Select(x => $"{x.ParameterName} IN {x.DbTypeName}"));
-            var statement = string.Format(createProcedureStatment, _storedProcedureName, parameters);
+            var statement = string.Format(createProcedureStatement, _storedProcedureName, parameters);
             using (var connection = new OracleConnection(OracleConfiguration.OracleConnectionString))
             using (var command = new OracleCommand(statement, connection))
             {
