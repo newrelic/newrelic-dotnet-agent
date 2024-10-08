3d213e6ed5ce3b0c37a46663a4bd0dd38f867661
chynesNR <chynes@newrelic.com>
2024-10-08 08:32:18 -0700

    Update ARN parsing logic

--- src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Helpers/AwsSdk.cs
+++ src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Helpers/AwsSdk.cs
@@ -1,58 +1,95 @@
 // Copyright 2020 New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using NewRelic.Agent.Api;
 
 namespace NewRelic.Agent.Extensions.Helpers
 {
     public static class AwsSdkHelpers
     {
+        private static Regex RegionRegex = new Regex(@"^[a-z]{2}((-gov)|(-iso([a-z]?)))?-[a-z]+-\d{1}$", RegexOptions.Compiled);
+        private static bool LooksLikeARegion(string text) => RegionRegex.IsMatch(text);
+        private static bool LooksLikeAnAccountId(string text) => (text.Length == 12) && text.All(c => c >= '0' && c <= '9');
+        // Only log ARNs we can't parse once
+        private static HashSet<string> BadInvocations = new HashSet<string>();
+
+        // This is the full regex pattern for an ARN:
+        // (arn:(aws[a-zA-Z-]*)?:lambda:)?([a-z]{2}((-gov)|(-iso([a-z]?)))?-[a-z]+-\d{1}:)?(\d{12}:)?(function:)?([a-zA-Z0-9-_\.]+)(:(\$LATEST|[a-zA-Z0-9-_]+))?
+
+        // If it's a full ARN, it has to start with 'arn:'
+        // A partial ARN can contain up to 5 segments separated by ':'
+        // 1. Region
+        // 2. Account ID
+        // 3. 'function' (fixed string)
+        // 4. Function name
+        // 5. Alias or version
+        // Only the function name is required, the reset are all optional. e.g. you could have region and function name and nothing else
+
+        // Note that this will not catch functions where the name also looks like a region or account ID
         public static string ConstructArn(IAgent agent, string invocationName, string region, string accountId)
         {
             if (invocationName.StartsWith("arn:"))
             {
-                if (invocationName.StartsWith("arn:aws:lambda:"))
-                {
-                    return invocationName;
-                }
-                agent?.Logger.Debug($"Unable to parse function name '{invocationName}'");
-                return null;
+                return invocationName;
             }
             var segments = invocationName.Split(':');
-            string functionName;
+            string functionName = null;
+            string alias = null;
 
-            if ((segments.Length == 1) || (segments.Length == 2))
+            foreach (var segment in segments)
             {
-                // 'myfunction' or 'myfunction:alias'
-                // Need account ID to reconstruct ARN
-                if (string.IsNullOrEmpty(accountId))
+                if (LooksLikeARegion(segment) && string.IsNullOrEmpty(region))
                 {
-                    agent?.Logger.Debug($"Need account ID in order to resolve function '{invocationName}'");
+                    region = segment;
+                }
+                else if (LooksLikeAnAccountId(segment) && string.IsNullOrEmpty(accountId))
+                {
+                    accountId = segment;
+                }
+                else if (segment == "function")
+                {
+                    continue;
+                }
+                else if (functionName == null)
+                {
+                    functionName = segment;
+                }
+                else if (alias == null)
+                {
+                    alias = segment;
+                }
+                else
+                {
+                    if (BadInvocations.Add(invocationName))
+                    {
+                        agent?.Logger.Debug($"Unable to parse function name '{invocationName}'");
+                    }
                     return null;
                 }
-                functionName = invocationName;
             }
-            else if (segments.Length == 3)
+
+            if (string.IsNullOrEmpty(accountId))
             {
-                // 123456789012:function:my-function'
-                accountId = segments[0];
-                functionName = segments[2];
-            }
-            else if (segments.Length == 4)
-            {
-                // 123456789012:function:my-function:myalias
-                accountId = segments[0];
-                functionName = $"{segments[2]}:{segments[3]}";
-            }
-            else
-            {
-                agent?.Logger.Debug($"Unable to parse function name '{invocationName}'.");
+                if (BadInvocations.Add(invocationName))
+                {
+                    agent?.Logger.Debug($"Need account ID in order to resolve function '{invocationName}'");
+                }
                 return null;
             }
             if (string.IsNullOrEmpty(region))
             {
-                agent?.Logger.Debug($"Need region in order to resolve function '{invocationName}'");
+                if (BadInvocations.Add(invocationName))
+                {
+                    agent?.Logger.Debug($"Need region in order to resolve function '{invocationName}'");
+                }
                 return null;
+            }
+            if (!string.IsNullOrEmpty(alias))
+            {
+                return $"arn:aws:lambda:{region}:{accountId}:function:{functionName}:{alias}";
             }
             return $"arn:aws:lambda:{region}:{accountId}:function:{functionName}";
         }
