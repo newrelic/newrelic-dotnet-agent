e12dba253cdfdef6d54705da679ad8b6a3606da0
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2024-10-21 10:06:02 -0700

    Clean up POC (#2839)
    * Remove datastore vendor name from ConnectionInfo in MemcachedHelpers
    
    * Get operation name from request type by converting PascalCaseRequest to snake_case

--- src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Parsing/StringsHelper.cs
+++ src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Parsing/StringsHelper.cs
@@ -119,5 +119,34 @@ namespace NewRelic.Agent.Extensions.Parsing
 
             return value;
         }
+
+        public static string ToSnakeCase(this string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+            if (text.Length < 2)
+            {
+                return text.ToLowerInvariant();
+            }
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(text[0]));
+            for (int i = 1; i < text.Length; ++i)
+            {
+                char c = text[i];
+                if (char.IsUpper(c))
+                {
+                    sb.Append('_');
+                    sb.Append(char.ToLowerInvariant(c));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
     }
 }
