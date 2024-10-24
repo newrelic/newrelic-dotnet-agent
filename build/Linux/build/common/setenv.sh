bc82d1f17c746038fbedbec7b5afc3fb9210d938
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2024-10-24 14:33:35 -0500

    A couple more fixes

--- build/Linux/build/common/setenv.sh
+++ build/Linux/build/common/setenv.sh
@@ -3,13 +3,9 @@
 if [ -z "$CORECLR_NEWRELIC_HOME" ]; && [ -z "$CORECLR_NEW_RELIC_HOME" ]; then
     echo "CORECLR_NEWRELIC_HOME is undefined"
 else
-    if [ -n "$CORECLR_NEWRELIC_HOME" ]; then
-        NRHOME=$CORECLR_NEWRELIC_HOME
-    else
-        NRHOME=$CORECLR_NEW_RELIC_HOME
-    fi
+    NRHOME=${CORECLR_NEWRELIC_HOME:-${CORECLR_NEW_RELIC_HOME}}
     
     export CORECLR_ENABLE_PROFILING=1
     export CORECLR_PROFILER={36032161-FFC0-4B61-B559-F6C5D41BAE5A}
-    export CORECLR_PROFILER_PATH=$NR_HOME/libNewRelicProfiler.so
+    export CORECLR_PROFILER_PATH=$NRHOME/libNewRelicProfiler.so
 fi
