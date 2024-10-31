newrelic/newrelic-dotnet-agent

commit e77683b7e60afa502b6e700a51945c757530a47b

fix: Revert environment variable name change in installers and scripts (#2852)

build/Linux/build/common/setenv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -z "$CORECLR_NEW_RELIC_HOME" ]; then
-    echo "CORECLR_NEW_RELIC_HOME is undefined"
+if [ -z "$CORECLR_NEWRELIC_HOME" ]; && [ -z "$CORECLR_NEW_RELIC_HOME" ]; then
+    echo "CORECLR_NEWRELIC_HOME is undefined"
 else
+    NRHOME=${CORECLR_NEWRELIC_HOME:-${CORECLR_NEW_RELIC_HOME}}
+    
     export CORECLR_ENABLE_PROFILING=1
     export CORECLR_PROFILER={36032161-FFC0-4B61-B559-F6C5D41BAE5A}
-    export CORECLR_PROFILER_PATH=$CORECLR_NEW_RELIC_HOME/libNewRelicProfiler.so
+    export CORECLR_PROFILER_PATH=$NRHOME/libNewRelicProfiler.so
 fi

build/ArtifactBuilder/Artifacts/MsiInstaller.cs
@@ -17,7 +17,7 @@ namespace ArtifactBuilder.Artifacts
         private readonly string[] _coreIISRegistryValues = new string[] {
                     "CORECLR_ENABLE_PROFILING=1",
                     "CORECLR_PROFILER={36032161-FFC0-4B61-B559-F6C5D41BAE5A}",
-                    "CORECLR_NEW_RELIC_HOME=[NETAGENTCOMMONFOLDER]" };
+                    "CORECLR_NEWRELIC_HOME=[NETAGENTCOMMONFOLDER]" };
 
         private readonly AgentComponents _frameworkAgentComponents;
         private readonly AgentComponents _coreAgentComponents;

build/Linux/build/common/run.sh
@@ -2,4 +2,4 @@
 
 # This script can be used to run a dotnet application with New Relic monitoring.
 
-CORECLR_NEW_RELIC_HOME=${CORECLR_NEW_RELIC_HOME:-/usr/local/newrelic-dotnet-agent} CORECLR_ENABLE_PROFILING=1 CORECLR_PROFILER={36032161-FFC0-4B61-B559-F6C5D41BAE5A} CORECLR_PROFILER_PATH=$CORECLR_NEW_RELIC_HOME/libNewRelicProfiler.so $@
+NRHOME=${CORECLR_NEWRELIC_HOME:-${CORECLR_NEW_RELIC_HOME:-/usr/local/newrelic-dotnet-agent}} CORECLR_ENABLE_PROFILING=1 CORECLR_PROFILER={36032161-FFC0-4B61-B559-F6C5D41BAE5A} CORECLR_PROFILER_PATH=$NRHOME/libNewRelicProfiler.so $@

build/Linux/build/deb/postinst
@@ -45,7 +45,7 @@ fi
 rm -f $NEWRELIC_HOME/extensions/NewRelic.Providers.Wrapper.Logging.Instrumentation.xml 2> /dev/null
 rm -f $NEWRELIC_HOME/extensions/NewRelic.Providers.Wrapper.Logging.dll 2> /dev/null
 
-echo "export CORECLR_NEW_RELIC_HOME=${NEWRELIC_HOME}" > /etc/profile.d/${PACKAGE_NAME}-path.sh
+echo "export CORECLR_NEWRELIC_HOME=${NEWRELIC_HOME}" > /etc/profile.d/${PACKAGE_NAME}-path.sh
 source /etc/profile.d/${PACKAGE_NAME}-path.sh
 
 chmod o+w $NEWRELIC_HOME/logs
@@ -53,4 +53,4 @@ chmod +x $NEWRELIC_HOME/*.sh 2> /dev/null
 
 printf "Initialize the New Relic .NET Agent environment variables by running:\n"
 printf "\t\033[1msource /etc/profile.d/${PACKAGE_NAME}-path.sh\033[0m\n"
-printf "\t\033[1msource $CORECLR_NEW_RELIC_HOME/setenv.sh\033[0m\n"
+printf "\t\033[1msource $CORECLR_NEWRELIC_HOME/setenv.sh\033[0m\n"

build/Linux/build/rpm/newrelic-dotnet-agent.spec
@@ -88,7 +88,7 @@ fi
 rm -f $NEWRELIC_HOME/extensions/NewRelic.Providers.Wrapper.Logging.Instrumentation.xml 2> /dev/null
 rm -f $NEWRELIC_HOME/extensions/NewRelic.Providers.Wrapper.Logging.dll 2> /dev/null
 
-echo "export CORECLR_NEW_RELIC_HOME=${NEWRELIC_HOME}" > /etc/profile.d/%{name}-path.sh
+echo "export CORECLR_NEWRELIC_HOME=${NEWRELIC_HOME}" > /etc/profile.d/%{name}-path.sh
 source /etc/profile.d/%{name}-path.sh
 
 chmod o+w $NEWRELIC_HOME/logs
@@ -96,4 +96,4 @@ chmod +x $NEWRELIC_HOME/*.sh 2> /dev/null
 
 printf "Initialize the New Relic .NET Agent environment variables by running:\n"
 printf "\t\033[1msource /etc/profile.d/%{name}-path.sh\033[0m\n"
-printf "\t\033[1msource $CORECLR_NEW_RELIC_HOME/setenv.sh\033[0m\n"
+printf "\t\033[1msource $CORECLR_NEWRELIC_HOME/setenv.sh\033[0m\n"

build/Packaging/AzureSiteExtension/Content/applicationHost.xdt
@@ -8,12 +8,12 @@
         <add name="COR_PROFILER_PATH" value="%HOME%\NewRelicAgent\Framework\NewRelic.Profiler.dll" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="COR_PROFILER_PATH_32" value="%HOME%\NewRelicAgent\Framework\x86\NewRelic.Profiler.dll" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="COR_PROFILER_PATH_64" value="%HOME%\NewRelicAgent\Framework\NewRelic.Profiler.dll" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
-        <add name="NEW_RELIC_HOME" value="%HOME%\NewRelicAgent\Framework" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
+        <add name="NEWRELIC_HOME" value="%HOME%\NewRelicAgent\Framework" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="CORECLR_ENABLE_PROFILING" value="1" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="CORECLR_PROFILER" value="{36032161-FFC0-4B61-B559-F6C5D41BAE5A}" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="CORECLR_PROFILER_PATH_32" value="%HOME%\NewRelicAgent\Core\x86\NewRelic.Profiler.dll" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="CORECLR_PROFILER_PATH_64" value="%HOME%\NewRelicAgent\Core\NewRelic.Profiler.dll" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
-        <add name="CORECLR_NEW_RELIC_HOME" value="%HOME%\NewRelicAgent\Core" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
+        <add name="CORECLR_NEWRELIC_HOME" value="%HOME%\NewRelicAgent\Core" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
         <add name="NEW_RELIC_METADATA_AZURE_APP_SERVICE_NAME" value="%WEBSITE_SITE_NAME%" xdt:Locator="Match(name)" xdt:Transform="InsertIfMissing"/>
       </environmentVariables>
     </runtime>

build/Packaging/AzureSiteExtension/Content/install.ps1
@@ -320,7 +320,7 @@ try
 
 	if ($env:NEWRELIC_LICENSEKEY -eq $null -and $env:NEW_RELIC_LICENSE_KEY -eq $null)
 	{
-		WriteToInstallLog "The environment variable NEW_RELIC_LICENSE_KEY must be set. Please make sure to add it."
+		WriteToInstallLog "The environment variable NEWRELIC_LICENSE_KEY must be set. Please make sure to add it."
 	}
 
 	RemoveNewRelicInstallArtifacts "."
@@ -337,7 +337,7 @@ try
 					"/configuration/system.webServer/runtime/environmentVariables/add[@name='CORECLR_PROFILER']",
 					"/configuration/system.webServer/runtime/environmentVariables/add[@name='CORECLR_PROFILER_PATH_32']",
 					"/configuration/system.webServer/runtime/environmentVariables/add[@name='CORECLR_PROFILER_PATH_64']",
-					"/configuration/system.webServer/runtime/environmentVariables/add[@name='CORECLR_NEW_RELIC_HOME']")
+					"/configuration/system.webServer/runtime/environmentVariables/add[@name='CORECLR_NEWRELIC_HOME']")
 		$file = resolve-path(".\applicationHost.xdt")
 		RemoveXmlElements $file $xPaths
 	}

build/Packaging/NugetAzureCloudServices/tools/NewRelicHelper.psm1
@@ -244,12 +244,12 @@ function update_azure_service_definition([System.__ComObject] $project){
             	
             	#Helps Azure Workers find the newrelic.config
             	$variableNHNode = $xml.CreateElement('Variable','http://schemas.microsoft.com/ServiceHosting/2008/10/ServiceDefinition')
-            	$variableNHNode.SetAttribute('name','NEW_RELIC_HOME')
+            	$variableNHNode.SetAttribute('name','NEWRELIC_HOME')
                 $variableNHNode.SetAttribute('value','D:\ProgramData\New Relic\.NET Agent\')
                 
                 #Helps Azure Workers find the NewRelic.Agent.Core.dll
             	$variableNIPNode = $xml.CreateElement('Variable','http://schemas.microsoft.com/ServiceHosting/2008/10/ServiceDefinition')
-            	$variableNIPNode.SetAttribute('name','NEW_RELIC_INSTALL_PATH')
+            	$variableNIPNode.SetAttribute('name','NEWRELIC_INSTALL_PATH')
                 $variableNIPNode.SetAttribute('value','D:\Program Files\New Relic\.NET Agent\')
                 
             	$runtimeEnvironmentNode.AppendChild($variableCEPNode)
@@ -390,7 +390,7 @@ function cleanup_azure_service_definition([System.__ComObject] $project){
 		
 		$runtimeNode = $modified.Runtime
         if($runtimeNode -ne $null -and $runtimeNode.ChildNodes.Count -gt 0){
-        	$variableNodes = $runtimeNode.Environment.Variable | where { $_.name -eq "COR_ENABLE_PROFILING" -or $_.name -eq "COR_PROFILER" -or $_.name -eq "NEW_RELIC_HOME" -or $_.name -eq "NEW_RELIC_INSTALL_PATH" }
+			$variableNodes = $runtimeNode.Environment.Variable | where { $_.name -eq "COR_ENABLE_PROFILING" -or $_.name -eq "COR_PROFILER" -or $_.name -eq "NEW_RELIC_HOME" -or $_.name -eq "NEW_RELIC_INSTALL_PATH" -or $_.name -eq "NEWRELIC_HOME" -or $_.name -eq "NEWRELIC_INSTALL_PATH" }
         	if($variableNodes -ne $null -and $variableNodes.Count -gt 0){
         		foreach($varNode in $variableNodes){
         			[Void]$varNode.ParentNode.RemoveChild($varNode)

commit bb77c5c7a27594252846a2f908b53d543b54974c

fix: Fix syntax error in `setenv.sh`
Fixes a syntax error that was introduced in #2852. 

Resolves #2863

build/Linux/build/common/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$CORECLR_NEWRELIC_HOME" ]; && [ -z "$CORECLR_NEW_RELIC_HOME" ]; then
+if [ -z "$CORECLR_NEWRELIC_HOME" ] && [ -z "$CORECLR_NEW_RELIC_HOME" ]; then
     echo "CORECLR_NEWRELIC_HOME is undefined"
 else
     NRHOME=${CORECLR_NEWRELIC_HOME:-${CORECLR_NEW_RELIC_HOME}}