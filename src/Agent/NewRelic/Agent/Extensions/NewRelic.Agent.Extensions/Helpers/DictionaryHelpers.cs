f214a5300423c5d240a9d82c86fce78128a6ed32
Jacob Affinito <jaffinito@newrelic.com>
2024-11-05 09:52:19 -0800

    Add unit test for DictionaryHelpers.

--- src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Helpers/DictionaryHelpers.cs
+++ src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Helpers/DictionaryHelpers.cs
@@ -8,9 +8,26 @@ namespace NewRelic.Agent.Extensions.Helpers
 {
     public static class DictionaryHelpers
     {
+        /// <summary>
+        /// Converts a JSON string to a dictionary.  Will always return a dictionary, even if the JSON is invalid.
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns>IReadOnlyDictionary<string, object></returns>
         public static IReadOnlyDictionary<string, object> FromJson(string json)
         {
-            return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            if (string.IsNullOrEmpty(json))
+            {
+                return new Dictionary<string, object>();
+            }
+
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            }
+            catch
+            {
+                return new Dictionary<string, object>();
+            }
         }
     }
 }
