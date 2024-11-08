8b8cb52d53bbbb8c0820072ce30a77ed0733d0c4
Jacob Affinito <jaffinito@newrelic.com>
2024-11-08 13:07:21 -0800

    Fix merge issue and add additional tests.

--- tests/Agent/IntegrationTests/IntegrationTestHelpers/NewRelicConfigModifier.cs
+++ tests/Agent/IntegrationTests/IntegrationTestHelpers/NewRelicConfigModifier.cs
@@ -107,7 +107,7 @@ namespace NewRelic.Agent.IntegrationTestHelpers
 
         public NewRelicConfigModifier AddExpectedStatusCodes(string statusCodes)
         {
-            CommonUtils.AddXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration", "errorCollector"}, "expectedStatusCodes", statusCodes );
+            CommonUtils.AddXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration", "errorCollector" }, "expectedStatusCodes", statusCodes);
             return this;
         }
 
@@ -386,7 +386,7 @@ namespace NewRelic.Agent.IntegrationTestHelpers
         public NewRelicConfigModifier EnableAgentTiming(bool enable = true)
         {
             CommonUtils.ModifyOrCreateXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration" }, "diagnostics", string.Empty);
-            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "diagnostics"}, "captureAgentTiming", enable.ToString().ToLower());
+            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "diagnostics" }, "captureAgentTiming", enable.ToString().ToLower());
             return this;
         }
 
@@ -436,8 +436,8 @@ namespace NewRelic.Agent.IntegrationTestHelpers
         {
             CommonUtils.ModifyOrCreateXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration" }, "appSettings", string.Empty);
             CommonUtils.ModifyOrCreateXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration", "appSettings" }, "add", string.Empty);
-            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "appSettings", "add"}, "key", "EnableAspNetCore6PlusBrowserInjection");
-            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "appSettings", "add"}, "value", $"{enableBrowserInjection}");
+            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "appSettings", "add" }, "key", "EnableAspNetCore6PlusBrowserInjection");
+            CommonUtils.ModifyOrCreateXmlAttributeInNewRelicConfig(_configFilePath, new[] { "configuration", "appSettings", "add" }, "value", $"{enableBrowserInjection}");
             return this;
         }
 
@@ -527,6 +527,8 @@ namespace NewRelic.Agent.IntegrationTestHelpers
         {
             var labelsString = string.Join(";", labels.Select(x => x.Key + ":" + x.Value).ToArray());
             CommonUtils.AddXmlNodeInNewRelicConfig(_configFilePath, new[] { "configuration" }, "labels", labelsString);
+            return this;
+        }
 
         public NewRelicConfigModifier EnableGCSamplerV2(bool enabled)
         {
