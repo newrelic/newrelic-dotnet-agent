fa079bd77120903159f80b76b35061c2e44931df
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-01-30 13:07:45 -0600

    fix: Custom attribute values of type float and decimal were not serialized correctly.

--- tests/Agent/UnitTests/Core.UnitTest/CustomEvents/CustomEventWireModelTests.cs
+++ tests/Agent/UnitTests/Core.UnitTest/CustomEvents/CustomEventWireModelTests.cs
@@ -39,7 +39,7 @@ namespace NewRelic.Agent.Core.CustomEvents.Tests
             var customEvents = new CustomEventWireModel[countEvents];
             var expectedSerializations = new List<Dictionary<string, object>[]>();
 
-            for(var i = 0; i < countEvents; i++)
+            for (var i = 0; i < countEvents; i++)
             {
                 var timestampVal = DateTime.UtcNow;
                 var typeVal = $"CustomEvent{i}";
@@ -87,7 +87,64 @@ namespace NewRelic.Agent.Core.CustomEvents.Tests
             AttributeComparer.CompareDictionaries(expectedSerializations[0], deserialized[0]);
             AttributeComparer.CompareDictionaries(expectedSerializations[1], deserialized[1]);
         }
-    }
 
+        [Test]
+        public void CustomEvents_UserAttributes_AllAttributeTypesSerializeCorrectly()
+        {
+            var dateTime = DateTime.UtcNow;
+            var timestampVal = dateTime;
+            var typeVal = $"CustomEvent";
+
+            var guid = Guid.NewGuid();
+            var expectedSerialization = new Dictionary<string, object>[]
+            {
+                    new Dictionary<string, object>()
+                    {
+                        {_attribDefs.Timestamp.Name, timestampVal.ToUnixTimeMilliseconds() },
+                        {_attribDefs.CustomEventType.Name, typeVal }
+                    },
+
+                    new Dictionary<string, object>()
+                    {
+                        { "boolVal", true},
+                        { "dateTimeVal", dateTime},
+                        { "decimalVal", 0.2M},
+                        { "doubleVal", 0.2D},
+                        { "enumVal", AttributeDestinations.CustomEvent},
+                        { "floatVal", 0.2f},
+                        { "guidVal", guid},
+                        { "intVal", 2},
+                        { "longVal", 2L},
+                        { "stringVal", "string"},
+                    },
+
+                    new Dictionary<string, object>()
+            };
+
+            var attribVals = new AttributeValueCollection(AttributeDestinations.CustomEvent);
+
+            _attribDefs.Timestamp.TrySetValue(attribVals, timestampVal);
+            _attribDefs.CustomEventType.TrySetValue(attribVals, typeVal);
+
+            _attribDefs.GetCustomAttributeForCustomEvent("boolVal").TrySetValue(attribVals, true);
+            _attribDefs.GetCustomAttributeForCustomEvent("dateTimeVal").TrySetValue(attribVals, dateTime);
+            _attribDefs.GetCustomAttributeForCustomEvent("decimalVal").TrySetValue(attribVals, 0.2M);
+            _attribDefs.GetCustomAttributeForCustomEvent("doubleVal").TrySetValue(attribVals, 0.2D);
+            _attribDefs.GetCustomAttributeForCustomEvent("enumVal").TrySetValue(attribVals, AttributeDestinations.CustomEvent);
+            _attribDefs.GetCustomAttributeForCustomEvent("floatVal").TrySetValue(attribVals, 0.2f);
+            _attribDefs.GetCustomAttributeForCustomEvent("guidVal").TrySetValue(attribVals, guid);
+            _attribDefs.GetCustomAttributeForCustomEvent("intVal").TrySetValue(attribVals, 2);
+            _attribDefs.GetCustomAttributeForCustomEvent("longVal").TrySetValue(attribVals, 2L);
+            _attribDefs.GetCustomAttributeForCustomEvent("stringVal").TrySetValue(attribVals, "string");
+
+            var customEvent = new CustomEventWireModel(.5f, attribVals);
+
+            var serialized = JsonConvert.SerializeObject(customEvent);
+            var expectedSerialized = JsonConvert.SerializeObject(expectedSerialization);
+
+            Assert.That(serialized, Is.Not.Null);
+            Assert.That(serialized, Is.EqualTo(expectedSerialized));
+        }
+    }
 }
 
