5974bcced328b2ec6c22b7c9532be9b072c1332c
Alex Hemsath <ahemsath@newrelic.com>
2025-03-12 14:00:36 -0700

    More unit tests

--- tests/Agent/UnitTests/NewRelic.Agent.Extensions.Tests/Helpers/KinesisHelperTests.cs
+++ tests/Agent/UnitTests/NewRelic.Agent.Extensions.Tests/Helpers/KinesisHelperTests.cs
@@ -1,16 +1,10 @@
 // Copyright 2020 New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using NewRelic.Agent.Api;
+using Aws.Kinesis.Models;
 using NewRelic.Agent.Extensions.AwsSdk;
-using NewRelic.Agent.Extensions.Providers.Wrapper;
 using NewRelic.Testing.Assertions;
 using NUnit.Framework;
-using Telerik.JustMock;
 
 namespace Agent.Extensions.Tests.Helpers
 {
@@ -31,5 +25,89 @@ namespace Agent.Extensions.Tests.Helpers
             // Assert
             Assert.That(streamName.IsEqualTo(expectedStreamName));
         }
+
+        [Test]
+        [TestCase("KinesisStreamName", "arn:aws:kinesis:us-west-2:111111111111:stream/KinesisStreamName", "KinesisStreamName")]
+        [TestCase("KinesisStreamName", null, "KinesisStreamName")]
+        [TestCase(null, "arn:aws:kinesis:us-west-2:111111111111:stream/KinesisStreamName", "KinesisStreamName")]
+        [TestCase(null, null, null)]
+        public void GetStreamNameFromRequest(string streamName, string streamArn, string expectedStreamName)
+        {
+            dynamic request = new MockKinesisDataStreamRequest();
+            request.StreamName = streamName;
+            request.StreamArn = streamArn;
+
+            // Act
+            var streamNameFromHelper = KinesisHelper.GetStreamNameFromRequest(request) as string;
+
+            // Assert
+            Assert.That(streamNameFromHelper.IsEqualTo(expectedStreamName));
+        }
+
+        [Test]
+        [TestCase("FirehoseStreamName", "arn:aws:kinesis:us-west-2:111111111111:deliverystream/FirehoseStreamName", "FirehoseStreamName")]
+        [TestCase("FirehoseStreamName", null, "FirehoseStreamName")]
+        [TestCase(null, "arn:aws:kinesis:us-west-2:111111111111:stream/FirehoseStreamName", "FirehoseStreamName")]
+        [TestCase(null, null, null)]
+        public void GetDeliveryStreamNameFromRequest(string streamName, string streamArn, string expectedStreamName)
+        {
+            dynamic request = new MockKinesisFirehoseRequest();
+            request.DeliveryStreamName = streamName;
+            request.DeliveryStreamArn = streamArn;
+
+            // Act
+            var streamNameFromHelper = KinesisHelper.GetDeliveryStreamNameFromRequest(request) as string;
+
+            // Assert
+            Assert.That(streamNameFromHelper.IsEqualTo(expectedStreamName));
+        }
+
+        [Test]
+        public void GetStreamNameFromRequest_UnknownRequestType_ReturnsNull()
+        {
+            dynamic request = new MockUnknownRequest();
+            request.NotTheDroidsYoureLookingFor = "Daleks";
+
+            // Act
+            var streamNameFromHelper = KinesisHelper.GetStreamNameFromRequest(request) as string;
+
+            // Assert
+            Assert.That(streamNameFromHelper.IsEqualTo(null));
+        }
+
+        [Test]
+        public void GetDeliveryStreamNameFromRequest_UnknownRequestType_ReturnsNull()
+        {
+            dynamic request = new MockUnknownRequest();
+            request.NotTheDroidsYoureLookingFor = "Lore";
+
+            // Act
+            var streamNameFromHelper = KinesisHelper.GetDeliveryStreamNameFromRequest(request) as string;
+
+            // Assert
+            Assert.That(streamNameFromHelper.IsEqualTo(null));
+        }
+
     }
 }
+
+namespace Aws.Kinesis.Models
+{
+    public class MockKinesisDataStreamRequest
+    {
+        public string StreamName { get; set; }
+        public string StreamArn { get; set; }
+    }
+
+    public class MockKinesisFirehoseRequest
+    {
+        public string DeliveryStreamName { get; set; }
+        public string DeliveryStreamArn { get; set; }
+    }
+
+    public class MockUnknownRequest
+    {
+        public string NotTheDroidsYoureLookingFor { get; set; }
+    }
+}
+
