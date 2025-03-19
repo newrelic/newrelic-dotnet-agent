b5ad43d29ef78a50481438abd01488d464de85e5
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-03-19 10:56:24 -0500

    Integration tests

--- tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Couchbase/Couchbase3Exerciser.cs
+++ tests/Agent/IntegrationTests/SharedApplications/Common/MultiFunctionApplicationHelpers/NetStandardLibraries/Couchbase/Couchbase3Exerciser.cs
@@ -30,7 +30,7 @@ class Couchbase3Exerciser
     // - IncrementAsync
     // - DecrementAsync
 
-    private async Task<(ICluster, IBucket)> InitializeAsync()
+    private async Task<(ICluster Cluster, IBucket Bucket)> InitializeAsync()
     {
         // Initialize the Couchbase cluster
         var clusterOptions = new ClusterOptions
@@ -53,7 +53,9 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Get()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
 
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
@@ -69,7 +71,6 @@ class Couchbase3Exerciser
             var result = await t;
             result.Dispose();
         }
-        using var getResult3 = await collection.GetAndTouchAsync("0", TimeSpan.FromSeconds(10));
     }
 
     [LibraryMethod]
@@ -77,7 +78,10 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task GetAndLockAndUnlock()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
@@ -94,7 +98,10 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Exists()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
@@ -107,12 +114,15 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task InsertUpsertReplaceAndRemove()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
 
-        var key = "my-document-key";
+        var key = Guid.NewGuid().ToString();
 
         // insert a document
         await collection.InsertAsync(key, new { Name = "Ted", Age = 31 });
@@ -132,10 +142,14 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Mutate()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         var inventoryScope = await bucket.ScopeAsync("inventory");
         var hotelCollection = await inventoryScope.CollectionAsync("hotel");
-        var mutateInResult = await hotelCollection.MutateInAsync("hotel_10025",
+
+        using var mutateInResult = await hotelCollection.MutateInAsync("hotel_10025",
             specs => specs.Upsert("pets_ok", true)
         );
     }
@@ -145,18 +159,21 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Lookup()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
 
         // lookup a document
-        await collection.LookupInAsync("0", [LookupInSpec.Get("credit_cards")]);
-        await collection.LookupInAnyReplicaAsync("0", [LookupInSpec.Get("credit_cards")]);
+        using var result1 = await collection.LookupInAsync("0", [LookupInSpec.Get("credit_cards")]);
+        using var result2 = await collection.LookupInAnyReplicaAsync("0", [LookupInSpec.Get("credit_cards")]);
         var results = collection.LookupInAllReplicasAsync("0", [LookupInSpec.Get("credit_cards")]);
         await foreach (var result in results)
         {
-            // process the result
+            result.Dispose();
         }
     }
 
@@ -165,10 +182,14 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Scan()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
+
         // scan the collection
         var results = collection.ScanAsync(new RangeScan());
         await foreach (var result in results)
@@ -182,13 +203,22 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task Touch()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
+
         // get a user-defined collection reference
         var scope = await bucket.ScopeAsync("tenant_agent_00");
         var collection = await scope.CollectionAsync("users");
+
+        // insert a new document so we can touch it and let it expire
+        var key = Guid.NewGuid().ToString();
+        await collection.InsertAsync(key, new { Name = "Ted", Age = 32 });
+
         // update the expiry of a document
-        await collection.TouchAsync("0", TimeSpan.FromSeconds(10));
-        await collection.TouchWithCasAsync("0", TimeSpan.FromSeconds(10));
+        using var getResult3 = await collection.GetAndTouchAsync(key, TimeSpan.FromSeconds(10));
+        await collection.TouchAsync(key, TimeSpan.FromSeconds(5));
+        await collection.TouchWithCasAsync(key, TimeSpan.FromSeconds(2));
     }
 
     [LibraryMethod]
@@ -196,10 +226,12 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task ScopeQuery()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
 
         var inventoryScope = await bucket.ScopeAsync("inventory");
-        var queryResult = await inventoryScope.QueryAsync<dynamic>("SELECT * FROM airline WHERE id = 10");
+        using var queryResult = await inventoryScope.QueryAsync<dynamic>("SELECT * FROM airline WHERE id = 10");
 
     }
 
@@ -208,7 +240,9 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task ClusterQuery()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
 
         try
         {
@@ -216,12 +250,12 @@ class Couchbase3Exerciser
             options.IgnoreIfExists(true);
             await cluster.QueryIndexes.CreatePrimaryIndexAsync("`travel-sample`", options);
         }
-        catch (IndexExistsException) // ignore
+        catch (IndexExistsException) // ignore, it might already exist
         {
         }
 
         // requires a primary index
-        var clusterQueryResult = await cluster.QueryAsync<dynamic>(
+        using var clusterQueryResult = await cluster.QueryAsync<dynamic>(
             "SELECT t.* FROM `travel-sample` t WHERE t.type=$type",
             options => options.Parameter("type", "landmark")
         );
@@ -232,7 +266,9 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task ScopeSearch()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
 
         var inventoryScope = await bucket.ScopeAsync("inventory");
         var searchResult = await inventoryScope.SearchAsync("index-hotel-description", SearchRequest.Create(new MatchQuery("swanky")), new SearchOptions().Limit(2));
@@ -243,7 +279,9 @@ class Couchbase3Exerciser
     [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
     public async Task ClusterSearch()
     {
-        var (cluster, bucket) = await InitializeAsync();
+        var initResponse = await InitializeAsync();
+        await using var cluster = initResponse.Cluster;
+        await using var bucket = initResponse.Bucket;
 
         var clusterSearchResult = await cluster.SearchAsync("hotels", SearchRequest.Create(new MatchQuery("swanky")), new SearchOptions().Limit(2));
     }
---
+++ tests/Agent/IntegrationTests/UnboundedIntegrationTests/Couchbase3/Couchbase3Tests.cs
@@ -0,0 +1,166 @@
+// Copyright 2020 New Relic, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NewRelic.Agent.IntegrationTestHelpers;
+using NewRelic.Agent.IntegrationTestHelpers.RemoteServiceFixtures;
+using NewRelic.Agent.IntegrationTests.Shared;
+using NewRelic.Agent.Tests.TestSerializationHelpers.Models;
+using NewRelic.Testing.Assertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NewRelic.Agent.UnboundedIntegrationTests.Couchbase3;
+
+public abstract class Couchbase3TestsBase<TFixture> : NewRelicIntegrationTest<TFixture> where TFixture : ConsoleDynamicMethodFixture
+{
+    protected readonly ConsoleDynamicMethodFixture _fixture;
+
+    protected Couchbase3TestsBase(TFixture fixture, ITestOutputHelper output) : base(fixture)
+    {
+        _fixture = fixture;
+        _fixture.TestLogger = output;
+
+        _fixture.SetTimeout(TimeSpan.FromMinutes(2));
+
+        _fixture.AddCommand("Couchbase3Exerciser Get");
+        _fixture.AddCommand("Couchbase3Exerciser GetAndLockAndUnlock");
+        _fixture.AddCommand("Couchbase3Exerciser Exists");
+        _fixture.AddCommand("Couchbase3Exerciser InsertUpsertReplaceAndRemove");
+        _fixture.AddCommand("Couchbase3Exerciser Mutate");
+        _fixture.AddCommand("Couchbase3Exerciser Lookup");
+        _fixture.AddCommand("Couchbase3Exerciser Scan");
+        _fixture.AddCommand("Couchbase3Exerciser Touch");
+        _fixture.AddCommand("Couchbase3Exerciser ScopeQuery");
+        _fixture.AddCommand("Couchbase3Exerciser ClusterQuery");
+        _fixture.AddCommand("Couchbase3Exerciser ScopeSearch");
+        _fixture.AddCommand("Couchbase3Exerciser ClusterSearch");
+
+        _fixture.AddActions
+        (
+            setupConfiguration: () =>
+            {
+                var configPath = fixture.DestinationNewRelicConfigFilePath;
+                var configModifier = new NewRelicConfigModifier(configPath);
+                configModifier.ConfigureFasterMetricsHarvestCycle(15);
+                configModifier.ConfigureFasterErrorTracesHarvestCycle(15);
+                configModifier.ConfigureFasterSpanEventsHarvestCycle(15);
+
+                configModifier.ForceTransactionTraces();
+                configModifier.ForceSqlTraces();
+            },
+            exerciseApplication: () =>
+            {
+                _fixture.AgentLog.WaitForLogLine(AgentLogBase.AgentConnectedLogLineRegex, TimeSpan.FromMinutes(1));
+                _fixture.AgentLog.WaitForLogLine(AgentLogBase.MetricDataLogLineRegex, TimeSpan.FromMinutes(1));
+                _fixture.AgentLog.WaitForLogLine(AgentLogBase.SpanEventDataLogLineRegex, TimeSpan.FromMinutes(1));
+            }
+        );
+
+        _fixture.Initialize();
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var expectedMetrics = new List<Assertions.ExpectedMetric>()
+        {
+            new() { metricName = "Datastore/all", CallCountAllHarvests = 48},
+            new() { metricName = "Datastore/allOther", CallCountAllHarvests = 48},
+            new() { metricName = "Datastore/Couchbase/all", CallCountAllHarvests = 48},
+            new() { metricName = "Datastore/Couchbase/allOther", CallCountAllHarvests = 48},
+
+            new() { metricName = "Datastore/operation/Couchbase/ExistsAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/GetAllReplicasAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/GetAndLockAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/GetAndTouchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/GetAnyReplicaAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/GetAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/InsertAsync", CallCountAllHarvests = 2},
+            new() { metricName = "Datastore/operation/Couchbase/LookupInAllReplicasAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/LookupInAnyReplicaAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/LookupInAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/MutateInAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/QueryAsync", CallCountAllHarvests = 15},
+            new() { metricName = "Datastore/operation/Couchbase/RemoveAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/ReplaceAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/ScanAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/SearchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/SearchQueryAsync", CallCountAllHarvests = 12},
+            new() { metricName = "Datastore/operation/Couchbase/TouchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/TouchWithCasAsync", CallCountAllHarvests = 2},
+            new() { metricName = "Datastore/operation/Couchbase/UnlockAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/operation/Couchbase/UpsertAsync", CallCountAllHarvests = 1},
+
+            new() { metricName = "Datastore/statement/Couchbase/hotel/MutateInAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/travel-sample/QueryAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/travel-sample/SearchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/ExistsAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/GetAllReplicasAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/GetAndLockAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/GetAndTouchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/GetAnyReplicaAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/GetAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/InsertAsync", CallCountAllHarvests = 2},
+            new() { metricName = "Datastore/statement/Couchbase/users/LookupInAllReplicasAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/LookupInAnyReplicaAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/LookupInAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/RemoveAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/ReplaceAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/ScanAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/TouchAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/TouchWithCasAsync", CallCountAllHarvests = 2},
+            new() { metricName = "Datastore/statement/Couchbase/users/UnlockAsync", CallCountAllHarvests = 1},
+            new() { metricName = "Datastore/statement/Couchbase/users/UpsertAsync", CallCountAllHarvests = 1},
+
+        };
+
+        var expectedSqlTraces = new List<Assertions.ExpectedSqlTrace>
+        {
+            new Assertions.ExpectedSqlTrace
+            {
+                TransactionName = "OtherTransaction/Custom/MultiFunctionApplicationHelpers.NetStandardLibraries.Couchbase.Couchbase3Exerciser/Get",
+                Sql = "SELECT ?;",
+                DatastoreMetricName = $"Datastore/operation/Couchbase/QueryAsync"
+            }
+        };
+
+
+        var expectedTransactionEventIntrinsicAttributes = new List<string>
+        {
+            "databaseCallCount",
+            "databaseDuration"
+        };
+
+        var expectedTransactionTraceSegmentParameters = new List<Assertions.ExpectedSegmentParameter>
+        {
+            new Assertions.ExpectedSegmentParameter { segmentName = $"Datastore/operation/Couchbase/QueryAsync", parameterName = "sql", parameterValue = "SELECT ?;"}
+        };
+
+
+        var metrics = _fixture.AgentLog.GetMetrics().ToList();
+        var transactionEvents = _fixture.AgentLog.GetTransactionEvents().ToList();
+        var sqlTraces = _fixture.AgentLog.GetSqlTraces().ToList();
+        var transactionSample = _fixture.AgentLog.TryGetTransactionSample("OtherTransaction/Custom/MultiFunctionApplicationHelpers.NetStandardLibraries.Couchbase.Couchbase3Exerciser/Get");
+
+        Assert.Multiple(
+            () => Assertions.MetricsExist(expectedMetrics, metrics),
+            () => Assertions.SqlTraceExists(expectedSqlTraces, sqlTraces),
+            () => Assert.All(transactionEvents, (t) => Assertions.TransactionEventHasAttributes(expectedTransactionEventIntrinsicAttributes, TransactionEventAttributeType.Intrinsic, t)),
+            () => Assertions.TransactionTraceSegmentParametersExist(expectedTransactionTraceSegmentParameters, transactionSample)
+        );
+    }
+
+}
+
+[NetCoreTest]
+public class Couchbase3TestsCoreLatest : Couchbase3TestsBase<ConsoleDynamicMethodFixtureCoreLatest>
+{
+    public Couchbase3TestsCoreLatest(ConsoleDynamicMethodFixtureCoreLatest fixture, ITestOutputHelper output)
+        : base(fixture, output)
+    {
+    }
+}
