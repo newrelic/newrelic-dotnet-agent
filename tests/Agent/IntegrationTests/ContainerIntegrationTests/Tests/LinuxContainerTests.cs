dbcb9c5d3e7accf76d8b271c48c9540cfcf76c67
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-03-27 11:16:38 -0500

    Update container integration tests for xunit v3

--- tests/Agent/IntegrationTests/ContainerIntegrationTests/Tests/LinuxContainerTests.cs
+++ tests/Agent/IntegrationTests/ContainerIntegrationTests/Tests/LinuxContainerTests.cs
@@ -5,7 +5,6 @@ using System;
 using NewRelic.Agent.ContainerIntegrationTests.Fixtures;
 using NewRelic.Agent.IntegrationTestHelpers;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace NewRelic.Agent.ContainerIntegrationTests.Tests;
 
