780cdfb9389f1aa45abbf494dbf7984555dab259
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-05-22 15:38:43 -0500

    Use loadbalancer and public IP everywhere

--- tests/Agent/IntegrationTests/UnboundedServices/build_and_push_acr.sh
+++ tests/Agent/IntegrationTests/UnboundedServices/build_and_push_acr.sh
@@ -1,21 +1,19 @@
 # Standard License Header
 # Bash script to build and push Docker images to Azure Container Registry (ACR)
-# Usage: ./build_and_push_acr.sh <ACR_NAME> <RESOURCE_GROUP> <AZURE_SUBSCRIPTION_ID>
+# Usage: ./build_and_push_acr.sh <ACR_NAME> <RESOURCE_GROUP>
 
 set -e
 
 ACR_NAME=$1
 RESOURCE_GROUP=$2
-SUBSCRIPTION_ID=$3
 
-az account set --subscription "$SUBSCRIPTION_ID"
 LOGIN_SERVER=$(az acr show --name "$ACR_NAME" --resource-group "$RESOURCE_GROUP" --query loginServer --output tsv)
 az acr login --name "$ACR_NAME"
 
-services=(rabbitmq mongodb32 mongodb60 redis couchbase postgres mssql mysql)
+services=(rabbitmq mongodb32 mongodb60 redis couchbase postgres mssql mysql elastic elastic7 oracle)
 
 for service in "${services[@]}"; do
-    imageName="$LOGIN_SERVER/unbounded-$service:latest"
-    docker build -t "$imageName" "./$service"
+    imageName="$LOGIN_SERVER/$service:latest"
+    docker build -t "$imageName" "$(dirname "$0")/$service"
     docker push "$imageName"
 done
