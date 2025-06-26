5882348ee398b9d5035a9f1ecaa52b021f72fb46
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-06-26 13:03:04 -0500

    mssql fixes, change manifests to only import necessary secrets from the secrets manifest

--- tests/Agent/IntegrationTests/UnboundedServices/mssql/setup.sh
+++ tests/Agent/IntegrationTests/UnboundedServices/mssql/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Starts a loop to check if SQL is ready before running setup script.
-STATUS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -No -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+STATUS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -No -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
 while [ "$STATUS" != 1 ]
 do
 sleep 1s
-STATUS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -No -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+STATUS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -No -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
 done
 
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD  -No -i /var/restore.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -No -i /var/restore.sql
