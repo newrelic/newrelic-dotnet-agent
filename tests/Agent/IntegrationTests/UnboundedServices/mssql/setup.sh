d72c2db117d2d6346d16de1f3f3fee76a0db2dfb
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-06-26 12:49:42 -0500

    More mssql image fixes

--- tests/Agent/IntegrationTests/UnboundedServices/mssql/setup.sh
+++ tests/Agent/IntegrationTests/UnboundedServices/mssql/setup.sh
@@ -8,4 +8,4 @@ sleep 1s
 STATUS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -No -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
 done
 
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -No -i /var/restore.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD  -No -i /var/restore.sql
