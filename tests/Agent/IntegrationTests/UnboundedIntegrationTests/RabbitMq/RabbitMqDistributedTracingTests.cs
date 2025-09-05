c69c25deb10ba87e933d451b45cf4a76115cf98f
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-09-05 10:34:40 -0500

    Comment out Rabbit DT tests for hybrid agent

--- tests/Agent/IntegrationTests/UnboundedIntegrationTests/RabbitMq/RabbitMqDistributedTracingTests.cs
+++ tests/Agent/IntegrationTests/UnboundedIntegrationTests/RabbitMq/RabbitMqDistributedTracingTests.cs
@@ -1,79 +1,81 @@
-// Copyright 2020 New Relic, Inc. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
+// TODO: Distributed tracing in RabbitMQ with OpenTelemetryBridge does not work. Save in case we figure it out in the future.
+//
+//// Copyright 2020 New Relic, Inc. All rights reserved.
+//// SPDX-License-Identifier: Apache-2.0
 
 
-using NewRelic.Agent.IntegrationTestHelpers;
-using NewRelic.Agent.IntegrationTestHelpers.RemoteServiceFixtures;
-using System;
-using System.Collections.Generic;
-using Xunit;
+//using NewRelic.Agent.IntegrationTestHelpers;
+//using NewRelic.Agent.IntegrationTestHelpers.RemoteServiceFixtures;
+//using System;
+//using System.Collections.Generic;
+//using Xunit;
 
 
 
-namespace NewRelic.Agent.UnboundedIntegrationTests.RabbitMq
-{
-    public abstract class RabbitMqDistributedTracingTestsBase<TFixture> : NewRelicIntegrationTest<TFixture>
-        where TFixture : ConsoleDynamicMethodFixture
-    {
-        private readonly string _sendReceiveQueue = $"integrationTestQueue-{Guid.NewGuid()}";
-        private ConsoleDynamicMethodFixture _fixture;
+//namespace NewRelic.Agent.UnboundedIntegrationTests.RabbitMq
+//{
+//    public abstract class RabbitMqDistributedTracingTestsBase<TFixture> : NewRelicIntegrationTest<TFixture>
+//        where TFixture : ConsoleDynamicMethodFixture
+//    {
+//        private readonly string _sendReceiveQueue = $"integrationTestQueue-{Guid.NewGuid()}";
+//        private ConsoleDynamicMethodFixture _fixture;
 
-        public RabbitMqDistributedTracingTestsBase(TFixture fixture, ITestOutputHelper output)  : base(fixture)
-        {
-            _fixture = fixture;
-            fixture.TestLogger = output;
+//        public RabbitMqDistributedTracingTestsBase(TFixture fixture, ITestOutputHelper output)  : base(fixture)
+//        {
+//            _fixture = fixture;
+//            fixture.TestLogger = output;
 
-            _fixture.AddCommand("RabbitMQModernExerciser ConnectAsync");
+//            _fixture.AddCommand("RabbitMQModernExerciser ConnectAsync");
 
-            // RabbitMQ SendRecieve uses the BasicGet method to receive, which does not process incoming tracing payloads
-            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveAsync {_sendReceiveQueue} TestMessage");
-            // RabbitMQ SendRecieveWithEventingConsumer uses the HandleBasicDeliverWrapper on the receiving side, which does process incoming tracing headers
-            // We execute the method twice to make sure this issue stays fixed: https://github.com/newrelic/newrelic-dotnet-agent/issues/464
-            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveWithEventingConsumerAsync {_sendReceiveQueue} EventingConsumerTestMessageOne");
-            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveWithEventingConsumerAsync {_sendReceiveQueue} EventingConsumerTestMessageTwo");
+//            // RabbitMQ SendRecieve uses the BasicGet method to receive, which does not process incoming tracing payloads
+//            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveAsync {_sendReceiveQueue} TestMessage");
+//            // RabbitMQ SendRecieveWithEventingConsumer uses the HandleBasicDeliverWrapper on the receiving side, which does process incoming tracing headers
+//            // We execute the method twice to make sure this issue stays fixed: https://github.com/newrelic/newrelic-dotnet-agent/issues/464
+//            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveWithEventingConsumerAsync {_sendReceiveQueue} EventingConsumerTestMessageOne");
+//            _fixture.AddCommand($"RabbitMQModernExerciser SendReceiveWithEventingConsumerAsync {_sendReceiveQueue} EventingConsumerTestMessageTwo");
 
-            // This is needed to avoid a hang on shutdown in the test app
-            _fixture.AddCommand("RabbitMQModernExerciser ShutdownAsync");
+//            // This is needed to avoid a hang on shutdown in the test app
+//            _fixture.AddCommand("RabbitMQModernExerciser ShutdownAsync");
 
-            fixture.Actions
-            (
-                setupConfiguration: () =>
-                {
-                    var configModifier = new NewRelicConfigModifier(fixture.DestinationNewRelicConfigFilePath);
-                    configModifier.ForceTransactionTraces();
+//            fixture.Actions
+//            (
+//                setupConfiguration: () =>
+//                {
+//                    var configModifier = new NewRelicConfigModifier(fixture.DestinationNewRelicConfigFilePath);
+//                    configModifier.ForceTransactionTraces();
 
-                    configModifier.SetOrDeleteDistributedTraceEnabled(true);
-                    configModifier.SetOrDeleteSpanEventsEnabled(true);
+//                    configModifier.SetOrDeleteDistributedTraceEnabled(true);
+//                    configModifier.SetOrDeleteSpanEventsEnabled(true);
 
-                    configModifier.EnableOTelBridge(true);
-                }
-            );
-            fixture.Initialize();
-        }
+//                    configModifier.EnableOTelBridge(true);
+//                }
+//            );
+//            fixture.Initialize();
+//        }
 
-        [Fact]
-        public void Test()
-        {
-            var expectedMetrics = new List<Assertions.ExpectedMetric>
-            {
-                new Assertions.ExpectedMetric { metricName = "Supportability/DistributedTrace/CreatePayload/Success", callCount = 3 },
-                new Assertions.ExpectedMetric { metricName = "Supportability/TraceContext/Create/Success", callCount = 3 },
-                new Assertions.ExpectedMetric { metricName = "Supportability/TraceContext/Accept/Success", callCount = 2 },
-                new Assertions.ExpectedMetric { metricName = "DotNet/MultiFunctionApplicationHelpers.NetStandardLibraries.RabbitMQ.RabbitMQModernExerciser/InstrumentedChildMethod"} ,
-                new Assertions.ExpectedMetric { metricName = "DotNet/MultiFunctionApplicationHelpers.NetStandardLibraries.RabbitMQ.RabbitMQModernExerciser/InstrumentedChildMethod", metricScope = "OtherTransaction/Message/RabbitMQ/Queue/Named/integrationTestQueue.*", IsRegexScope = true}
-            };
+//        [Fact]
+//        public void Test()
+//        {
+//            var expectedMetrics = new List<Assertions.ExpectedMetric>
+//            {
+//                new Assertions.ExpectedMetric { metricName = "Supportability/DistributedTrace/CreatePayload/Success", callCount = 3 },
+//                new Assertions.ExpectedMetric { metricName = "Supportability/TraceContext/Create/Success", callCount = 3 },
+//                new Assertions.ExpectedMetric { metricName = "Supportability/TraceContext/Accept/Success", callCount = 2 },
+//                new Assertions.ExpectedMetric { metricName = "DotNet/MultiFunctionApplicationHelpers.NetStandardLibraries.RabbitMQ.RabbitMQModernExerciser/InstrumentedChildMethod"} ,
+//                new Assertions.ExpectedMetric { metricName = "DotNet/MultiFunctionApplicationHelpers.NetStandardLibraries.RabbitMQ.RabbitMQModernExerciser/InstrumentedChildMethod", metricScope = "OtherTransaction/Message/RabbitMQ/Queue/Named/integrationTestQueue.*", IsRegexScope = true}
+//            };
 
-            var metrics = _fixture.AgentLog.GetMetrics();
+//            var metrics = _fixture.AgentLog.GetMetrics();
 
-            Assertions.MetricsExist(expectedMetrics, metrics);
-        }
-    }
+//            Assertions.MetricsExist(expectedMetrics, metrics);
+//        }
+//    }
 
-    public class RabbitMqDistributedTracingTestsCoreLatest : RabbitMqDistributedTracingTestsBase<ConsoleDynamicMethodFixtureCoreLatest>
-    {
-        public RabbitMqDistributedTracingTestsCoreLatest(ConsoleDynamicMethodFixtureCoreLatest fixture, ITestOutputHelper output)
-            : base(fixture, output)
-        {
-        }
-    }
-}
+//    public class RabbitMqDistributedTracingTestsCoreLatest : RabbitMqDistributedTracingTestsBase<ConsoleDynamicMethodFixtureCoreLatest>
+//    {
+//        public RabbitMqDistributedTracingTestsCoreLatest(ConsoleDynamicMethodFixtureCoreLatest fixture, ITestOutputHelper output)
+//            : base(fixture, output)
+//        {
+//        }
+//    }
+//}
