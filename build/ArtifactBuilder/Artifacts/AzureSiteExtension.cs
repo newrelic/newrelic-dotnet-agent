cf0a2c78280d16e609e3de451448952160d8f6ac
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-10-13 12:17:36 -0500

    Revert site extension to target net462

--- build/ArtifactBuilder/Artifacts/AzureSiteExtension.cs
+++ build/ArtifactBuilder/Artifacts/AzureSiteExtension.cs
@@ -23,9 +23,9 @@ namespace ArtifactBuilder.Artifacts
             _version = ReadVersionFromFile();
             var package = new NugetPackage(StagingDirectory, OutputDirectory);
             package.CopyAll($@"{PackageDirectory}");
-            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net481\{NuGetHelperLibraryName}");
-            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net481\{NuGetLibraryName}");
-            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net481\{XmlLibraryName}");
+            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net462\{NuGetHelperLibraryName}");
+            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net462\{NuGetLibraryName}");
+            package.CopyToContent($@"{RepoRootDirectory}\build\NewRelic.NuGetHelper\bin\Release\net462\{XmlLibraryName}");
             package.SetVersion(_version);
             _nuGetPackageName = package.Pack();
         }
