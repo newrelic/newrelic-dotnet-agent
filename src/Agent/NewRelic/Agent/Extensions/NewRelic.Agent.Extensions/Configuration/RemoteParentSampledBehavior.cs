e97274582cdc4e8f6638b941ece8926116d0c04b
Marty T <120425148+tippmar-nr@users.noreply.github.com>
2025-11-05 11:30:15 -0600

    fix: Update ReportedConfiguration to serialize certain enums as strings instead of integers for better clarity when viewing settings in the UI (#3296)

--- src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Configuration/RemoteParentSampledBehavior.cs
+++ src/Agent/NewRelic/Agent/Extensions/NewRelic.Agent.Extensions/Configuration/RemoteParentSampledBehavior.cs
@@ -1,36 +1,20 @@
 // Copyright 2020 New Relic, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 namespace NewRelic.Agent.Configuration
 {
-    [JsonConverter(typeof(StringEnumConverter))]
     public enum SamplerType
     {
-        [EnumMember(Value = "adaptive")]
         Adaptive,
-
-        [EnumMember(Value = "always_on")]
         AlwaysOn,
-
-        [EnumMember(Value = "always_off")]
         AlwaysOff,
-
-        [EnumMember(Value = "trace_id_ratio_based")]
         TraceIdRatioBased
     }
 
-    [JsonConverter(typeof(StringEnumConverter))]
     public enum SamplerLevel
     {
-        [EnumMember(Value = "root")]
         Root,
-        [EnumMember(Value = "remote_parent_sampled")]
         RemoteParentSampled,
-        [EnumMember(Value = "remote_parent_not_sampled")]
         RemoteParentNotSampled,
     }
 
