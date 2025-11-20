e2a81097d94a12e9dd06992f32302699073ad870
Marty T <120425148+tippmar-nr@users.noreply.github.com>
2025-11-20 14:25:43 -0600

    fix: Cache all environment variable access to reduce possible thread contention (#3322)

--- src/Agent/NewRelic/Agent/Core/SharedInterfaces/IEnvironment.cs
+++ src/Agent/NewRelic/Agent/Core/SharedInterfaces/IEnvironment.cs
@@ -25,16 +25,6 @@ namespace NewRelic.Agent.Core.SharedInterfaces
         string GetEnvironmentVariable(string variable);
 
         /// <summary>
-        /// Retrieves the value of an environment variable from the current process located in the specified target.
-        /// </summary>
-        /// <param name="variable">The name of the environment variable.</param>
-        /// <param name="environmentVariableTarget">The environment variable location to use.</param>
-        /// <returns>The value of the environment variable specified by variable, or null if the environment variable is not found.</returns>
-        /// <exception cref="ArgumentNullException">variable is null.</exception>
-        /// <exception cref="SecurityException">The caller does not have the required permission to perform this operation.</exception>
-        string GetEnvironmentVariable(string variable, EnvironmentVariableTarget environmentVariableTarget);
-
-        /// <summary>
         /// Retrieves the value of an environment variable with specified prefix from the current process located in the specified target.
         /// </summary>
         /// <param name="prefix">prefix.</param>
