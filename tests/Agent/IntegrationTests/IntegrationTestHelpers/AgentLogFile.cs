ae6c9f54e1fe1e4b50307969a567ea6770c33cb7
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2025-12-17 15:05:20 -0600

    Rewverting an unneeded change

--- tests/Agent/IntegrationTests/IntegrationTestHelpers/AgentLogFile.cs
+++ tests/Agent/IntegrationTests/IntegrationTestHelpers/AgentLogFile.cs
@@ -72,10 +72,6 @@ namespace NewRelic.Agent.IntegrationTestHelpers
 
         public string GetFullLogAsString()
         {
-            // verify file exists - return empty string if it does not
-            if (!Found)
-                return "Agent Log File Not Found";
-
             using (var fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (var streamReader = new StreamReader(fileStream))
             {
