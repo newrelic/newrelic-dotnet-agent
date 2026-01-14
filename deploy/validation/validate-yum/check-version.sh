2d6aa2d3b3e74c226542b64e1f1a05193c60e85e
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2026-01-14 11:34:21 -0800

    ci: Update post-deploy YUM repo validation to use the same process in user documentation (#3397)
    Fix post-deploy YUM check

--- deploy/validation/validate-yum/check-version.sh
+++ deploy/validation/validate-yum/check-version.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-cat << REPO | tee "/etc/yum.repos.d/newrelic-dotnet-agent.repo"
-[newrelic-dotnet-agent-repo]
-name=New Relic .NET Core packages for Enterprise Linux
-baseurl=https://yum.newrelic.com/pub/newrelic/el7/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-NewRelic
-REPO
-
 yum install newrelic-dotnet-agent -y
 
 rpm -q --queryformat '%{VERSION}\n' newrelic-dotnet-agent
