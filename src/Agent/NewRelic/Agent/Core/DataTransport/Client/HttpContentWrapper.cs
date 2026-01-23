10c30f9d831737c38aa0791c6d0fe3b6926afafc
Marty Tippin <120425148+tippmar-nr@users.noreply.github.com>
2026-01-23 10:22:44 -0600

    formatting

--- src/Agent/NewRelic/Agent/Core/DataTransport/Client/HttpContentWrapper.cs
+++ src/Agent/NewRelic/Agent/Core/DataTransport/Client/HttpContentWrapper.cs
@@ -8,23 +8,23 @@ using NewRelic.Agent.Core.DataTransport.Client.Interfaces;
 
 namespace NewRelic.Agent.Core.DataTransport.Client;
 
-    /// <summary>
-    /// HttpContent wrapper to enable mocking in unit tests
-    /// </summary>
-    public class HttpContentWrapper : IHttpContentWrapper
+/// <summary>
+/// HttpContent wrapper to enable mocking in unit tests
+/// </summary>
+public class HttpContentWrapper : IHttpContentWrapper
+{
+    private readonly HttpContent _httpContent;
+
+    public HttpContentWrapper(HttpContent httpContent)
     {
-        private readonly HttpContent _httpContent;
-
-        public HttpContentWrapper(HttpContent httpContent)
-        {
-            _httpContent = httpContent;
-        }
-
-        public Stream ReadAsStream()
-        {
-            return _httpContent.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-        }
-
-        public IHttpContentHeadersWrapper Headers => new HttpContentHeadersWrapper(_httpContent.Headers);
+        _httpContent = httpContent;
     }
+
+    public Stream ReadAsStream()
+    {
+        return _httpContent.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+    }
+
+    public IHttpContentHeadersWrapper Headers => new HttpContentHeadersWrapper(_httpContent.Headers);
+}
 #endif
