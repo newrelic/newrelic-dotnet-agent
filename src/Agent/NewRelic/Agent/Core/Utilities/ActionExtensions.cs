134854b7df517e43e3e37c9ce0b235fdce1bf129
Alex Hemsath <57361211+nr-ahemsath@users.noreply.github.com>
2026-05-01 13:40:16 -0700

    throw instead of throw ex to preserve call stack history

--- src/Agent/NewRelic/Agent/Core/Utilities/ActionExtensions.cs
+++ src/Agent/NewRelic/Agent/Core/Utilities/ActionExtensions.cs
@@ -19,12 +19,12 @@ public static class ActionExtensions
             try
             {
                 Log.Error(ex, $"An exception occurred while doing some background work");
-                throw ex;
+                throw;
             }
             catch
             {
             }
-            throw ex;
+            throw;
         }
     }
 }
